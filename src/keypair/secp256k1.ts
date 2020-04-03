e184cf677c087b7e4c4935cf5009393237bc9ffa
Cayman <caymannava@gmail.com>
2020-04-02 19:56:17 -0500

    Add keypair verify on init

--- src/keypair/secp256k1.ts
+++ src/keypair/secp256k1.ts
@@ -2,18 +2,53 @@ import secp256k1 = require("bcrypto/lib/secp256k1");
 import { AbstractKeypair, IKeypair, IKeypairClass, KeypairType } from "./types";
 import { ERR_INVALID_KEYPAIR_TYPE } from "./constants";
 
+export function secp256k1PublicKeyToCompressed(publicKey: Buffer): Buffer {
+  if (publicKey.length === 64) {
+    publicKey = Buffer.concat([Buffer.from([4]), publicKey]);
+  }
+  return secp256k1.publicKeyConvert(publicKey, true);
+}
+
+export function secp256k1PublicKeyToFull(publicKey: Buffer): Buffer {
+  if (publicKey.length === 64) {
+    return Buffer.concat([Buffer.from([4]), publicKey]);
+  }
+  return secp256k1.publicKeyConvert(publicKey, false);
+}
+
+export function secp256k1PublicKeyToRaw(publicKey: Buffer): Buffer {
+  return secp256k1.publicKeyConvert(publicKey, false).slice(1);
+}
+
 export const Secp256k1Keypair: IKeypairClass = class Secp256k1Keypair extends AbstractKeypair implements IKeypair {
-  readonly type = KeypairType.secp256k1;
+  readonly type:  KeypairType;
+
+  constructor(privateKey?: Buffer, publicKey?: Buffer) {
+    let pub = publicKey;
+    if (pub) {
+      pub = secp256k1PublicKeyToCompressed(pub);
+    }
+    super(privateKey, pub);
+    this.type = KeypairType.secp256k1;
+  }
+
   static generate(): Secp256k1Keypair {
     const privateKey = secp256k1.privateKeyGenerate();
     const publicKey = secp256k1.publicKeyCreate(privateKey);
     return new Secp256k1Keypair(privateKey, publicKey);
   }
-  privateKeyVerify(): boolean {
-    return secp256k1.privateKeyVerify(this.privateKey);
+
+  privateKeyVerify(key = this._privateKey): boolean {
+    if (key) {
+      return secp256k1.privateKeyVerify(key);
+    }
+    return true;
   }
-  publicKeyVerify(): boolean {
-    return secp256k1.publicKeyVerify(this.publicKey);
+  publicKeyVerify(key = this._publicKey): boolean {
+    if (key) {
+      return secp256k1.publicKeyVerify(key);
+    }
+    return true;
   }
   sign(msg: Buffer): Buffer {
     return secp256k1.sign(msg, this.privateKey);
--- src/keypair/types.ts
+++ src/keypair/types.ts
@@ -21,11 +21,21 @@ export interface IKeypairClass {
 }
 
 export abstract class AbstractKeypair {
-  readonly _privateKey: Buffer | null;
-  readonly _publicKey: Buffer | null;
+  readonly _privateKey?: Buffer;
+  readonly _publicKey?: Buffer;
   constructor(privateKey?: Buffer, publicKey?: Buffer) {
-    this._privateKey = privateKey || null;
-    this._publicKey = publicKey || null;
+    if (
+      (this._privateKey = privateKey) &&
+      !this.privateKeyVerify()
+    ) {
+      throw new Error("Invalid private key");
+    }
+    if (
+      (this._publicKey = publicKey) &&
+      !this.publicKeyVerify()
+    ) {
+      throw new Error("Invalid private key");
+    }
   }
   get privateKey(): Buffer {
     if (!this._privateKey) {
@@ -38,5 +48,11 @@ export abstract class AbstractKeypair {
       throw new Error();
     }
     return this._publicKey;
+  }
+  privateKeyVerify(): boolean {
+    return true;
+  }
+  publicKeyVerify(): boolean {
+    return true;
   }
 }
