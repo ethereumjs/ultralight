25340515a28bd6e4bbb91304cf34a20a4964ac0f
Cayman <caymannava@gmail.com>
2020-08-06 16:06:08 -0500

    Add ENR getters/setters

--- types/multiaddr/index.d.ts
+++ types/multiaddr/index.d.ts
@@ -1,3 +1,10 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 declare module "multiaddr/src/ip" {
   export function toString(ip: Buffer): string;
+}
+
+declare module "multiaddr/src/convert" {
+  export function toString(protocol: any, buf: Buffer): string | number;
+  export function toBuffer(protocol: any, str: string | number): Buffer;
 }
