f5f5bea98cf22b7f25834678b908ea5ae4309e22
Cayman <caymannava@gmail.com>
2020-08-13 18:59:10 -0500

    Add lookupTimeout configuration

--- src/kademlia/types.ts
+++ src/kademlia/types.ts
@@ -54,6 +54,12 @@ export interface ILookupConfig {
    * Defaults to the maximum number of entries in a single kbucket.
    */
   lookupNumResults: number;
+  /**
+   * Maximum amount of time to spend on a single lookup
+   *
+   * Declared in milliseconds
+   */
+  lookupTimeout: number;
 }
 
 export interface ILookupEvents {
