c3f493f891012b0f14641178578f125ff5681a55
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-12-13 09:27:10 -0500

    Remove merged discv5

--- packages/discv5/src/session/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./types";
-export * from "./crypto";
-export * from "./session";
-export * from "./service";
--- packages/discv5/src/util/hexString.ts
+++
@@ -1,7 +0,0 @@
-export function toHex(buf: Buffer): string {
-  return buf.toString("hex");
-}
-
-export function fromHex(str: string): Buffer {
-  return Buffer.from(str, "hex");
-}
