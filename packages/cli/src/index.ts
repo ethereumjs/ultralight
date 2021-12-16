706a2a70098dc9e1ce0c365309fb5902d94da55d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-12-16 10:49:23 -0500

    Cli improvements (#23)
    * Spawn proxy when running cli
    * Add proxy logging and error handling

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,6 +4,7 @@ import PeerId from "peer-id";
 import { Multiaddr } from "multiaddr";
 import yargs from 'yargs'
 import { hideBin } from 'yargs/helpers'
+import { ChildProcessWithoutNullStreams, spawn } from 'child_process'
 const readline = require('readline')
 
 readline.emitKeypressEvents(process.stdin)
@@ -11,11 +12,34 @@ process.stdin.setRawMode(true)
 
 const args: any = yargs(hideBin(process.argv))
     .option('bootnode', {
-        describe: 'Bootnode',
+        describe: 'ENR of Bootnode',
         default: '',
+    })
+    .option('proxy', {
+        describe: 'Start proxy service',
+        boolean: true,
+        default: true
+    })
+    .option('extip', {
+        describe: "External IP Address",
+        default: '127.0.0.1',
     }).argv
 
+let child: ChildProcessWithoutNullStreams
 const main = async () => {
+    if (args.proxy === true) {
+        //Spawn a child process that runs the proxy 
+        const file = require.resolve('../../proxy/dist/index.js')
+        child = spawn(process.execPath, [file, args.extip])
+        child.stdout.on('data', async (data) => {
+            // Prints all proxy logs to the console
+            console.log(data.toString())
+        })
+        child.stderr.on('data', (data) => {
+            // Prints all proxy errors to the console
+            console.log(data.toString())
+        })
+    }
     const id = await PeerId.create({ keyType: "secp256k1" });
     const enr = ENR.createFromPeerId(id);
     enr.setLocationMultiaddr(new Multiaddr("/ip4/127.0.0.1/udp/0"));
@@ -31,29 +55,43 @@ const main = async () => {
     );
     portal.enableLog();
     await portal.start();
-    portal.client.addEnr(args.bootnode)
-    const bootnodeId = ENR.decodeTxt(args.bootnode).nodeId
-    console.log('Press p to ping bootnode')
-    console.log('Press n to send FINDNODES to bootnode')
+    let bootnodeId: string
+    if (args.bootnode) {
+        portal.client.addEnr(args.bootnode)
+        bootnodeId = ENR.decodeTxt(args.bootnode).nodeId
+        console.log(`Press p to ping ${bootnodeId}`)
+        console.log(`Press n to send FINDNODES to ${bootnodeId}`)
+    }
     process.stdin.on('keypress', async (str, key) => {
         switch (key.name) {
             case 'p': {
+                if (!bootnodeId) return;
                 console.log('Sending PING to', bootnodeId);
                 const res = await portal.sendPing(bootnodeId, SubNetworkIds.HistoryNetworkId)
                 console.log('Received PONG response', res);
                 break;
             }
             case 'n': {
+                if (!bootnodeId) return;
                 console.log('Sending FINDNODES to ', bootnodeId)
                 const res = await portal.sendFindNodes(bootnodeId, Uint16Array.from([0, 1, 2]), SubNetworkIds.HistoryNetworkId)
                 console.log(res)
             }
+            case 'e': {
+                console.log('Current ENR is:', portal.client.enr.encodeTxt())
+                break;
+            }
             case 'c': if (key.ctrl) {
                 console.log('Exiting')
+                child?.kill(0)
                 process.exit(0);
             }
         }
     })
 }
 
-main()
+main().catch((err) => {
+    console.log('Encountered an error', err.message)
+    console.log('Shutting down...')
+    child?.kill(0)
+})
