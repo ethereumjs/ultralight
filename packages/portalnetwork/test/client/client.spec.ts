613e5e0a64f2ab762ce94a1acdfe787a69bf8002
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-12-16 21:13:31 -0500

    Add negative sendPing test

--- packages/portalnetwork/test/client/client.spec.ts
+++ packages/portalnetwork/test/client/client.spec.ts
@@ -13,10 +13,13 @@ tape('Client unit tests', async (t) => {
 
     t.throws(() => node.radius = 257, 'should not be able to set radius greater than 256')
     t.throws(() => node.radius = -1, 'radius cannot be negative');
+
     const pongResponse = Uint8Array.from([1, 5, 0, 0, 0, 0, 0, 0, 0, 12, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     node.sendPortalNetworkMessage = td.func<any>()
-    td.when(node.sendPortalNetworkMessage(td.matchers.anything(), td.matchers.anything(), td.matchers.anything())).thenResolve(pongResponse)
-    const res = await node.sendPing('abc', SubNetworkIds.HistoryNetworkId)
+    td.when(node.sendPortalNetworkMessage(td.matchers.anything(), td.matchers.anything(), td.matchers.anything())).thenResolve(pongResponse, null)
+    let res = await node.sendPing('abc', SubNetworkIds.HistoryNetworkId)
     t.ok(res.enrSeq === 5n && res.customPayload[0] === 1, 'received expected PONG response')
+    res = await node.sendPing('abc', SubNetworkIds.HistoryNetworkId)
+    t.ok(res === undefined, 'received undefined when no valid PONG message received')
     t.end()
 })
