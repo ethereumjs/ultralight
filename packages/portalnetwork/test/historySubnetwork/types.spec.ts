734af5f7609dfcbefa5bd2a7f98342e272f6f553
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2021-12-20 09:33:48 -0500

    Fix test

--- packages/portalnetwork/test/historySubnetwork/types.spec.ts
+++ packages/portalnetwork/test/historySubnetwork/types.spec.ts
@@ -13,10 +13,11 @@ tape('History Subnetwork contentKey serialization/deserialization', async t => {
     blockHash = "0xd1c390624d3bd4e409a61a858e5dcc5517729a9170d014a6c96530d64dd8621d"
     encodedKey = HistoryNetworkContentKeyUnionType.serialize({ selector: 2, value: { chainId, blockHash: fromHexString(blockHash) } })
     t.equals(toHexString(encodedKey), "0x021400d1c390624d3bd4e409a61a858e5dcc5517729a9170d014a6c96530d64dd8621d", 'blockbody content key equals expected output')
-    t.equals(toHexString(SHA256.digest(encodedKey)), "0xfccf5551a4b704080d4a7c89fc87b3e88a232683c88c2100240e05b6cf2fe441", 'block header content ID matches')
+    t.equals(toHexString(SHA256.digest(encodedKey)), "0xfccf5551a4b704080d4a7c89fc87b3e88a232683c88c2100240e05b6cf2fe441", 'block body content ID matches')
     chainId = 4
     blockHash = "0xd1c390624d3bd4e409a61a858e5dcc5517729a9170d014a6c96530d64dd8621d"
     encodedKey = HistoryNetworkContentKeyUnionType.serialize({ selector: 3, value: { chainId, blockHash: fromHexString(blockHash) } })
-    t.equals(toHexString(encodedKey), "0x42a3a15338bbd23dd373f831eddbb48d781120c2ea052cc06ff839875e5682e9", 'receipt content key equals expected output')
+    t.equals(toHexString(encodedKey), "0x030400d1c390624d3bd4e409a61a858e5dcc5517729a9170d014a6c96530d64dd8621d", 'receipt content key equals expected output')
+    t.equals(toHexString(SHA256.digest(encodedKey)), "0x42a3a15338bbd23dd373f831eddbb48d781120c2ea052cc06ff839875e5682e9", 'receipt content ID matches')
     t.end()
 })  
