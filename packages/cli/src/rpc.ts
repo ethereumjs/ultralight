5448d841302699f9b3cb49a76bbdd5933588fc61
Emerson Macro <emersonmacro@gmail.com>
2021-12-29 08:54:13 -1000

    rpc: add eth_getBlockByHash

--- packages/cli/src/rpc.ts
+++ packages/cli/src/rpc.ts
@@ -1,9 +1,15 @@
 import debug from 'debug'
 
 const log = debug('RPC')
+
 export const methods = {
     'discv5_nodeInfo': async () => {
         log('discv5_nodeInfo request received')
         return 'Ultralight-CLI: v0.0.1'
     },
+    'eth_getBlockByHash': async (params: [string, boolean]) => {
+        const [blockHash, includeTransactions] = params
+        log(`eth_getBlockByHash request received. blockHash: ${ blockHash } includeTransactions: ${ includeTransactions }`)
+        return 'Ultralight-CLI: v0.0.1'
+    }
 }
