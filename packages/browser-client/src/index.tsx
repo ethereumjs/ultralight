f1aaf2f1160a347c653fb6b7ffefa009e861223a
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-01-05 09:28:44 -0500

    Remove more boilerplate

--- packages/browser-client/src/index.tsx
+++ packages/browser-client/src/index.tsx
@@ -2,7 +2,6 @@ import { ColorModeScript } from '@chakra-ui/react'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from './App'
-import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,8 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 )
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister()
