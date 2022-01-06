784863512073838d715263716cce8b8fbd93d548
ScottyPoi <kyzambo@gmail.com>
2022-01-05 19:02:02 -0700

    disable prettier/prettier

--- config/eslint.js
+++ config/eslint.js
@@ -41,7 +41,7 @@ module.exports = {
     '@typescript-eslint/no-unnecessary-condition': 'off',
     'no-dupe-class-members': 'off',
     'no-extra-semi': 'off',
-    'prettier/prettier': 'error',
+    // 'prettier/prettier': 'error',
     'no-redeclare': 'off',
     '@typescript-eslint/no-redeclare': ['error'],
     'no-case-declarations': 'warn',
