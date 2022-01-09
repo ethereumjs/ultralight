ab052127b092f2a01f967d29108700ffea454b2f
Emerson Macro <emersonmacro@gmail.com>
2022-01-08 23:14:21 -0800

    lint fixes

--- packages/cli/src/rpc.ts
+++ packages/cli/src/rpc.ts
@@ -6,15 +6,17 @@ const log = debug('RPC')
 export class RPCManager {
   public _client: PortalNetwork
   private _methods: { [key: string]: Function } = {
-    'discv5_nodeInfo': async () => {
+    discv5_nodeInfo: async () => {
       log('discv5_nodeInfo request received')
       return 'Ultralight-CLI: v0.0.1'
     },
-    'eth_getBlockByHash': async (params: [string, boolean]) => {
+    eth_getBlockByHash: async (params: [string, boolean]) => {
       const [blockHash, includeTransactions] = params
-      log(`eth_getBlockByHash request received. blockHash: ${blockHash} includeTransactions: ${includeTransactions}`)
+      log(
+        `eth_getBlockByHash request received. blockHash: ${blockHash} includeTransactions: ${includeTransactions}`
+      )
       return 'Ultralight-CLI: v0.0.1'
-    }
+    },
   }
 
   constructor(client: PortalNetwork) {
