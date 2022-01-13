5ce601ec81c02433b455a86186c119e8dc578384
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-01-13 10:55:49 -0500

    Proxy updates, client adjustments, config

--- packages/browser-client/src/App.tsx
+++ packages/browser-client/src/App.tsx
@@ -12,6 +12,8 @@ import {
   RadioGroup,
   Radio,
   Stack,
+  Input,
+  Button,
 } from '@chakra-ui/react'
 import { ColorModeSwitcher } from './ColorModeSwitcher'
 import { ENR } from '@chainsafe/discv5'
@@ -23,23 +25,26 @@ import AddressBookManager from './Components/AddressBookManager'
 
 import { ContentManager } from './Components/ContentManager'
 export const App = () => {
-  const [portal, setDiscv5] = React.useState<PortalNetwork>()
+  const [portal, setPortal] = React.useState<PortalNetwork>()
   const [enr, setENR] = React.useState<string>('')
   const [network, setNetwork] = React.useState<SubNetworkIds>(SubNetworkIds.HistoryNetwork)
-
+  const [proxy, setProxy] = React.useState('127.0.0.1')
   const { onCopy } = useClipboard(enr) // eslint-disable-line
 
   const init = async () => {
+    if (portal?.client.isStarted()) {
+      await portal.stop()
+    }
     const id = await PeerId.create({ keyType: 'secp256k1' })
     const enr = ENR.createFromPeerId(id)
     enr.setLocationMultiaddr(new Multiaddr('/ip4/127.0.0.1/udp/0'))
-    const portal = new PortalNetwork(
+    const node = new PortalNetwork(
       {
         enr: enr,
         peerId: id,
         multiaddr: new Multiaddr('/ip4/127.0.0.1/udp/0'),
         transport: 'wss',
-        proxyAddress: 'ws://127.0.0.1:5050',
+        proxyAddress: `ws://${proxy}:5050`,
       },
       1
     )
@@ -49,19 +54,19 @@ export const App = () => {
     ;(window as any).Multiaddr = Multiaddr
     // eslint-disable-next-line no-undef
     ;(window as any).ENR = ENR
-    setDiscv5(portal)
-    portal.client.on('multiaddrUpdated', () =>
-      setENR(portal.client.enr.encodeTxt(portal.client.keypair.privateKey))
+    setPortal(node)
+    node.client.on('multiaddrUpdated', () =>
+      setENR(node.client.enr.encodeTxt(node.client.keypair.privateKey))
     )
-    await portal.start()
+    await node.start()
 
-    portal.enableLog('portalnetwork*, <uTP>*')
+    node.enableLog('portalnetwork*, <uTP>*,discv5*')
   }
 
-  React.useEffect(() => {
-    init()
-  }, [])
-
+  const stopNode = async () => {
+    await portal?.stop()
+    setPortal(undefined)
+  }
   const copy = async () => {
     await setENR(portal?.client.enr.encodeTxt(portal.client.keypair.privateKey) ?? '')
     onCopy()
@@ -96,6 +101,19 @@ export const App = () => {
             <Heading paddingBottom={2} size="lg">
               Local Node Management
             </Heading>
+            <Stack direction="row">
+              <Input
+                onChange={(evt) => {
+                  setProxy(evt.target.value)
+                }}
+                placeholder="Proxy IP Address"
+              />
+              {portal ? (
+                <Button onClick={stopNode}>Stop Node</Button>
+              ) : (
+                <Button onClick={init}>Start Node</Button>
+              )}
+            </Stack>
             <RadioGroup onChange={updateNetwork} value={network} spacing={1}>
               <Stack direction="row">
                 <Radio value={SubNetworkIds.StateNetwork}>State Network</Radio>
