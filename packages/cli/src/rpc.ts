ec793de8edd46584161c68abd887c66c98656e8e
emersonmacro <77563348+emersonmacro@users.noreply.github.com>
2022-01-18 05:40:14 -0800

    Rpc manager (#66)
    * cli: add RPCManager class and return block RLP for content stored locally
    
    
    Co-authored-by: acolytec3 <17355484+acolytec3@users.noreply.github.com>

--- packages/cli/src/rpc.ts
+++ packages/cli/src/rpc.ts
@@ -1,17 +1,37 @@
 import debug from 'debug'
+import { PortalNetwork, getContentId } from 'portalnetwork'
 
 const log = debug('RPC')
 
-export const methods = {
-  discv5_nodeInfo: async () => {
-    log('discv5_nodeInfo request received')
-    return 'Ultralight-CLI: v0.0.1'
-  },
-  eth_getBlockByHash: async (params: [string, boolean]) => {
-    const [blockHash, includeTransactions] = params
-    log(
-      `eth_getBlockByHash request received. blockHash: ${blockHash} includeTransactions: ${includeTransactions}`
-    )
-    return 'Ultralight-CLI: v0.0.1'
-  },
+export class RPCManager {
+  public _client: PortalNetwork
+  private _methods: { [key: string]: Function } = {
+    discv5_nodeInfo: async () => {
+      log('discv5_nodeInfo request received')
+      return 'Ultralight-CLI: v0.0.1'
+    },
+    eth_getBlockByHash: async (params: [string, boolean]) => {
+      const [blockHash, includeTransactions] = params
+      log(
+        `eth_getBlockByHash request received. blockHash: ${blockHash} includeTransactions: ${includeTransactions}`
+      )
+      const lookupKey = getContentId(1, blockHash, 0)
+      const value = await this._client.db.get(lookupKey)
+      if (value) {
+        return value
+      }
+      // TODO: Add sendFindContent to this to retrieve from network if not found in local DB
+      // TODO: Add logic to compile block header and block body into one response since network
+      // will return them separately
+      return 'Block not found in DB'
+    },
+  }
+
+  constructor(client: PortalNetwork) {
+    this._client = client
+  }
+
+  public getMethods() {
+    return this._methods
+  }
 }
