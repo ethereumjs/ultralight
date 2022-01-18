231a8a0dca890928dd68e75833222514bc72b115
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-01-18 08:55:14 -0500

    add new addBootNode method to rpc

--- packages/cli/src/rpc.ts
+++ packages/cli/src/rpc.ts
@@ -1,5 +1,5 @@
 import debug from 'debug'
-import { PortalNetwork, getContentId } from 'portalnetwork'
+import { PortalNetwork, getContentId, SubNetworkIds } from 'portalnetwork'
 
 const log = debug('RPC')
 
@@ -15,6 +15,7 @@ export class RPCManager {
       log(
         `eth_getBlockByHash request received. blockHash: ${blockHash} includeTransactions: ${includeTransactions}`
       )
+      // lookup block header in DB and return if found
       const lookupKey = getContentId(1, blockHash, 0)
       const value = await this._client.db.get(lookupKey)
       if (value) {
@@ -24,6 +25,13 @@ export class RPCManager {
       // TODO: Add logic to compile block header and block body into one response since network
       // will return them separately
       return 'Block not found in DB'
+    },
+    portal_addBootNode: async (params: [string]) => {
+      const [enr] = params
+      log(`portal_addBootNode request received. enr: ${enr.slice(0, 15)}...`)
+      const res = await this._client.sendPing(enr, SubNetworkIds.HistoryNetwork)
+      log(res?.enrSeq)
+      return res?.enrSeq ? 'node added' : 'node not found'
     },
   }
 
