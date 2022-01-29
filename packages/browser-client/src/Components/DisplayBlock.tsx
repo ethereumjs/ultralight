b3b8348df656b3d36ca931796f6921658c01926d
ScottyPoi <66335769+ScottyPoi@users.noreply.github.com>
2022-01-28 21:40:52 -0700

    display block as tabbed table

--- packages/browser-client/src/Components/DisplayBlock.tsx
+++ packages/browser-client/src/Components/DisplayBlock.tsx
@@ -1,6 +1,21 @@
-import { Container } from '@chakra-ui/react'
+import {
+  Container,
+  Box,
+  TabPanels,
+  TabPanel,
+  Tab,
+  TabList,
+  Tabs,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Td,
+  Text,
+  Link,
+} from '@chakra-ui/react'
 // eslint-disable-next-line implicit-dependencies/no-implicit
-import { Block, BlockHeader } from '@ethereumjs/block'
+import { Block, BlockHeader, JsonHeader } from '@ethereumjs/block'
 // eslint-disable-next-line implicit-dependencies/no-implicit
 import Common from '@ethereumjs/common'
 // eslint-disable-next-line implicit-dependencies/no-implicit
@@ -8,6 +23,7 @@ import { toBuffer } from 'ethereumjs-util'
 
 interface DisplayBlockProps {
   rlpHeader: string
+  findContent: any
 }
 
 export default function DisplayBlock(props: DisplayBlockProps) {
@@ -15,10 +31,77 @@ export default function DisplayBlock(props: DisplayBlockProps) {
   const chain = 'mainnet'
   const common = new Common({ chain: chain })
   const blockHeader = BlockHeader.fromRLPSerializedHeader(toBuffer(rlpHeader), { common: common })
-
   const block = new Block(blockHeader)
+  const json = block.toJSON()
+  const header: JsonHeader = json.header!
+  const headerObj: Record<string, string | number> = {
+    baseFee: header.baseFee!,
+    baseFeePerGas: header.baseFeePerGas!,
+    bloom: parseInt(header.bloom!.toString()),
+    coinbase: header.coinbase!,
+    difficutly: header.difficulty!,
+    extraData: header.extraData!,
+    gasLimit: header.gasLimit!,
+    gasUsed: header.gasUsed!,
+    logsBloom: Number(header.logsBloom!),
+    mixHash: header.mixHash!,
+    nonce: header.nonce!,
+    number: Number(header.number!),
+    parentHash: header.parentHash!,
+    receiptTrie: header.receiptTrie!,
+    stateRoot: header.stateRoot!,
+    timestamp: new Date(parseInt(header.timestamp!)).toString(),
+    transactionsTrie: header.transactionsTrie!,
+    uncleHash: header.uncleHash!,
+  }
+  const tx = json.transactions
+  // const uncles = json.uncleHeaders
+  function handleClick(hash: string) {
+    props.findContent(hash)
+  }
 
-  //   const json = block.toJSON
-
-  return <Container>Block {JSON.stringify(block.toJSON())}</Container>
+  return (
+    <Container>
+      <Box>
+        <Tabs>
+          <TabList>
+            <Tab>Header</Tab>
+            <Tab>Transactions</Tab>
+            <Tab>Uncles</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Table size={'sm'} variant="simple">
+                <Thead>
+                  {Object.keys(headerObj).map((key, idx) => {
+                    return (
+                      <Tr key={idx}>
+                        <Th>{key}</Th>
+                        <Td>
+                          {key === 'parentHash' ? (
+                            <Link onClick={() => handleClick(headerObj[key] as string)}>
+                              {headerObj[key]}
+                            </Link>
+                          ) : (
+                            headerObj[key]
+                          )}
+                        </Td>
+                      </Tr>
+                    )
+                  })}
+                </Thead>
+              </Table>
+            </TabPanel>
+            <TabPanel>
+              TXs
+              {tx?.map((t, idx) => {
+                return <Text key={idx}>{t}</Text>
+              })}
+            </TabPanel>
+            <TabPanel>Uncles</TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  )
 }
