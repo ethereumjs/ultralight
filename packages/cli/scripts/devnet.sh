f721db4f1d848cc33f6fc63a3e148a959f44b415
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-02-17 14:29:42 -0500

    Move devent script to cli

--- packages/cli/scripts/devnet.sh
+++ packages/cli/scripts/devnet.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 set -e
 
-node packages/proxy/dist/index.js --nat=localhost &
+node ../proxy/dist/index.js --nat=localhost &
 sleep 3
 counter=1
 while [ $counter -le $1 ]
 do
   port=$((8545+$counter))
-  node packages/cli/dist/index.js --nat=localhost --proxy=false --rpc --rpcport=$port &
+  node dist/index.js --nat=localhost --proxy=false --rpc --rpcport=$port &
   counter=$(($counter+1))
 done
 
