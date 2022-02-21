9a1e41ccbffd61880113fe06fb3d04f1bd1c19a8
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-02-21 06:51:56 -0500

    Move content lookup metrics back to portal network (#123)

--- packages/cli/src/rpc.ts
+++ packages/cli/src/rpc.ts
@@ -17,7 +17,6 @@ export class RPCManager {
       log(
         `eth_getBlockByHash request received. blockHash: ${blockHash} includeTransactions: ${includeTransactions}`
       )
-      this._client.metrics?.totalContentLookups.inc()
       // lookup block header in DB and return if found
       const headerlookupKey = getContentId(1, blockHash, 0)
       const bodylookupKey = includeTransactions && getContentId(1, blockHash, 1)
@@ -44,10 +43,8 @@ export class RPCManager {
           : rlp.encode([[], []])
         // TODO: Figure out why block body isn't coming back as Uint8Array
         block = reassembleBlock(header as Uint8Array, Uint8Array.from(body as Uint8Array))
-        this._client.metrics?.successfulContentLookups.inc()
         return block
-      } catch {}
-      this._client.metrics?.failedContentLookups.inc()
+      } catch { }
       return 'Block not found'
     },
     portal_addBootNode: async (params: [string]) => {
