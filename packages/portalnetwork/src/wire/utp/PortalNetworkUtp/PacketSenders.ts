3135e142a698dbafcc38c453b6e3661861d4ba17
ScottyPoi <66335769+ScottyPoi@users.noreply.github.com>
2022-03-04 02:16:37 -0700

    move packet sender functions into separate file

---
+++ packages/portalnetwork/src/wire/utp/PortalNetworkUtp/PacketSenders.ts
@@ -0,0 +1,76 @@
+import {
+  ConnectionState,
+  createAckPacket,
+  createDataPacket,
+  createFinPacket,
+  createResetPacket,
+  createSelectiveAckPacket,
+  createSynPacket,
+  PacketType,
+  _UTPSocket,
+} from '..'
+
+export async function sendSynPacket(socket: _UTPSocket): Promise<void> {
+  // Initiates a uTP connection from a ConnectionId
+  const packet = createSynPacket(socket.rcvConnectionId, socket.seqNr++, socket.ackNr)
+  socket.state = ConnectionState.SynSent
+  await socket.sendPacket(packet, PacketType.ST_SYN)
+}
+
+export async function sendSynAckPacket(socket: _UTPSocket): Promise<void> {
+  const packet = createAckPacket(
+    socket.seqNr++,
+    socket.sndConnectionId,
+    socket.ackNr,
+    socket.rtt_var,
+    socket.cur_window
+  )
+  await socket.sendPacket(packet, PacketType.ST_STATE)
+}
+export async function sendDataPacket(socket: _UTPSocket, payload: Uint8Array): Promise<void> {
+  const packet = createDataPacket(
+    socket.seqNr++,
+    socket.sndConnectionId,
+    socket.ackNr,
+    socket.max_window,
+    payload,
+    socket.rtt_var
+  )
+  socket.seqNrs.push(packet.header.seqNr)
+  await socket.sendPacket(packet, PacketType.ST_DATA)
+}
+export async function sendAckPacket(socket: _UTPSocket): Promise<void> {
+  const packet = createAckPacket(
+    socket.seqNr++,
+    socket.sndConnectionId,
+    socket.ackNr,
+    socket.rtt_var,
+    socket.cur_window
+  )
+  await socket.sendPacket(packet, PacketType.ST_STATE)
+}
+export async function sendResetPacket(socket: _UTPSocket): Promise<void> {
+  const packet = createResetPacket(socket.seqNr++, socket.sndConnectionId, socket.ackNr)
+  await socket.sendPacket(packet, PacketType.ST_RESET)
+}
+export async function sendFinPacket(socket: _UTPSocket): Promise<void> {
+  const packet = createFinPacket(
+    socket.sndConnectionId,
+    socket.seqNr,
+    socket.ackNr,
+    socket.cur_window
+  )
+  socket.finNr = packet.header.seqNr
+  await socket.sendPacket(packet, PacketType.ST_FIN)
+}
+export async function sendSelectiveAck(socket: _UTPSocket) {
+  const received: number[] = socket.received
+  const _packet = createSelectiveAckPacket(
+    socket.seqNr++,
+    socket.sndConnectionId,
+    socket.ackNr,
+    socket.rtt_var,
+    socket.cur_window
+  )
+  await socket.sendPacket(_packet, PacketType.ST_STATE)
+}
