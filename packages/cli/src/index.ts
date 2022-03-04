0b5d45b23d75c8b5f4cb4066f7663a5b42b2154a
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-03-04 11:09:59 -0500

    Add leveldb option

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -10,6 +10,8 @@ import http from 'http'
 import * as PromClient from 'prom-client'
 import debug from 'debug'
 import { RPCManager } from './rpc'
+import { setupMetrics } from './metrics'
+const level = require('level')
 
 const args: any = yargs(hideBin(process.argv))
   .option('pk', {
@@ -52,6 +54,11 @@ const args: any = yargs(hideBin(process.argv))
     describe: 'Port exposed for metrics scraping',
     number: true,
     default: 18545,
+  })
+  .options('datadir', {
+    describe: 'data directory where content is stored',
+    string: true,
+    optional: true,
   }).argv
 
 let child: ChildProcessWithoutNullStreams
@@ -60,89 +67,6 @@ const register = new PromClient.Registry()
 const reportMetrics = async (req: http.IncomingMessage, res: http.ServerResponse) => {
   res.writeHead(200)
   res.end(await register.metrics())
-}
-
-const setupMetrics = () => {
-  return {
-    knownDiscv5Nodes: new PromClient.Gauge({
-      name: 'ultralight_known_discv5_peers',
-      help: 'how many peers are in discv5 routing table',
-      async collect() {},
-    }),
-    knownHistoryNodes: new PromClient.Gauge({
-      name: 'ultralight_known_history_peers',
-      help: 'how many peers are in discv5 routing table',
-      async collect() {},
-    }),
-    totalContentLookups: new PromClient.Gauge<string>({
-      name: 'ultralight_total_content_lookups',
-      help: 'total number of content lookups initiated',
-    }),
-    successfulContentLookups: new PromClient.Counter({
-      name: 'ultralight_successful_content_lookups',
-      help: 'how many content lookups successfully returned content',
-    }),
-    failedContentLookups: new PromClient.Counter({
-      name: 'ultralight_failed_content_lookups',
-      help: 'how many content lookups failed to return content',
-    }),
-    offerMessagesSent: new PromClient.Counter({
-      name: 'ultralight_offer_messages_sent',
-      help: 'how many offer messages have been sent',
-    }),
-    offerMessagesReceived: new PromClient.Counter({
-      name: 'ultralight_offer_messages_received',
-      help: 'how many offer messages have been received',
-    }),
-    acceptMessagesSent: new PromClient.Counter({
-      name: 'ultralight_accept_messages_sent',
-      help: 'how many accept messages have been sent',
-    }),
-    acceptMessagesReceived: new PromClient.Counter({
-      name: 'ultralight_accept_messages_received',
-      help: 'how many accept messages have been received',
-    }),
-    findContentMessagesSent: new PromClient.Counter({
-      name: 'ultralight_findContent_messages_sent',
-      help: 'how many findContent messages have been sent',
-    }),
-    findContentMessagesReceived: new PromClient.Counter({
-      name: 'ultralight_findContent_messages_received',
-      help: 'how many findContent messages have been received',
-    }),
-    contentMessagesSent: new PromClient.Counter({
-      name: 'ultralight_content_messages_sent',
-      help: 'how many content messages have been sent',
-    }),
-    contentMessagesReceived: new PromClient.Counter({
-      name: 'ultralight_content_messages_received',
-      help: 'how many content messages have been received',
-    }),
-    findNodesMessagesSent: new PromClient.Counter({
-      name: 'ultralight_findNodes_messages_sent',
-      help: 'how many findNodes messages have been sent',
-    }),
-    findNodesMessagesReceived: new PromClient.Counter({
-      name: 'ultralight_findNodes_messages_received',
-      help: 'how many findNodes messages have been received',
-    }),
-    nodesMessagesSent: new PromClient.Counter({
-      name: 'ultralight_nodes_messages_sent',
-      help: 'how many nodes messages have been sent',
-    }),
-    nodesMessagesReceived: new PromClient.Counter({
-      name: 'ultralight_nodes_messages_received',
-      help: 'how many nodes messages have been received',
-    }),
-    totalBytesReceived: new PromClient.Counter({
-      name: 'ultralight_total_bytes_received',
-      help: 'how many bytes have been received in Portal Network message payloads',
-    }),
-    totalBytesSent: new PromClient.Counter({
-      name: 'ultralight_total_bytes_sent',
-      help: 'how many bytes have been sent in Portal Network message payloads',
-    }),
-  }
 }
 
 const run = async () => {
@@ -156,6 +80,10 @@ const run = async () => {
   const log = debug(enr.nodeId.slice(0, 5)).extend('ultralight')
   enr.setLocationMultiaddr(new Multiaddr('/ip4/127.0.0.1/udp/0'))
   const metrics = setupMetrics()
+  let db
+  if (args.datadir) {
+    db = level(args.datadir)
+  }
   const portal = new PortalNetwork(
     {
       enr: enr,
@@ -165,7 +93,7 @@ const run = async () => {
       proxyAddress: 'ws://127.0.0.1:5050',
     },
     2n ** 256n,
-    undefined,
+    db,
     metrics
   )
   portal.enableLog('*ultralight*, *portalnetwork*, *<uTP>*')
