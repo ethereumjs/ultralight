8f20a592d57fe42a06c0ca6148aa070e6fa50341
ScottyPoi <scott.simpson@ethereum.org>
2022-03-25 02:18:31 -0600

    Update Block Display

--- packages/browser-client/src/Components/DisplayBlock.tsx
+++ packages/browser-client/src/Components/DisplayBlock.tsx
@@ -1,3 +1,4 @@
+import { CopyIcon, SearchIcon } from '@chakra-ui/icons'
 import {
   Box,
   TabPanels,
@@ -11,27 +12,60 @@ import {
   Th,
   Text,
   Heading,
+  Button,
+  Grid,
+  GridItem,
 } from '@chakra-ui/react'
 import { Block } from '@ethereumjs/block'
+import { getHistoryNetworkContentId } from 'portalnetwork'
+import { Dispatch, SetStateAction } from 'react'
 import SelectTx from './SelectTx'
-import { toHexString } from './ShowInfo'
 
 interface DisplayBlockProps {
   block: Block
+  findParent: (hash: string) => Promise<void>
+  setParentHash: Dispatch<SetStateAction<string>>
 }
 
 export default function DisplayBlock(props: DisplayBlockProps) {
   const header = Object.entries(props.block!.header!.toJSON())
   const txList = props.block.transactions
   const tx: string[] = props.block.transactions.map((tx) => '0x' + tx.hash().toString('hex'))
+  const headerlookupKey = getHistoryNetworkContentId(1, props.block.toJSON().header?.mixHash!, 0)
+  const bodylookupKey = getHistoryNetworkContentId(1, props.block.toJSON().header?.mixHash!, 1)
   return (
     <Box>
-      <Heading padding={4} size="md" textAlign={'center'}>
-        Block Explorer
-      </Heading>
       <Heading paddingBottom={4} size="sm" textAlign={'center'}>
-        {toHexString(props.block.hash())}
+        Block #{props.block.header.number.toNumber()}
       </Heading>
+      <Grid templateColumns={'repeat(16, 1fr'} columnGap={1}>
+        <GridItem colSpan={4}>
+          <Text fontSize="xs" textAlign={'start'}>
+            <span style={{ fontWeight: 'bold' }}>Header Key: </span>
+          </Text>
+        </GridItem>
+        <GridItem colStart={5} colSpan={1}>
+          <CopyIcon marginEnd={2} />
+        </GridItem>
+        <GridItem wordBreak={'break-all'} colSpan={10} colStart={6}>
+          <Text wordBreak={'break-all'} fontSize="xs" textAlign={'start'}>
+            {headerlookupKey}
+          </Text>
+        </GridItem>
+        <GridItem colSpan={4}>
+          <Text fontSize="xs" textAlign={'start'}>
+            <span style={{ fontWeight: 'bold' }}>Body Key: </span>
+          </Text>
+        </GridItem>
+        <GridItem colStart={5} colSpan={1}>
+          <CopyIcon marginEnd={2} />
+        </GridItem>
+        <GridItem wordBreak={'break-all'} colSpan={10} colStart={6}>
+          <Text wordBreak={'break-all'} fontSize="xs" textAlign={'start'}>
+            {bodylookupKey}
+          </Text>
+        </GridItem>
+      </Grid>
       <Tabs>
         <TabList>
           <Tab>Header</Tab>
@@ -41,14 +75,49 @@ export default function DisplayBlock(props: DisplayBlockProps) {
         </TabList>
         <TabPanels>
           <TabPanel>
+            <Grid templateColumns={'repeat(10, 1fr)'}>
+              {header &&
+                header.map((key, idx) => {
+                  return (
+                    <>
+                      <GridItem fontSize={'xs'} fontWeight="bold" colSpan={3}>
+                        {key[0]}
+                      </GridItem>
+                      <GridItem
+                        paddingBottom={3}
+                        fontSize={'xs'}
+                        wordBreak={'break-all'}
+                        colSpan={6}
+                      >
+                        {key[1]}
+                      </GridItem>
+                      <GridItem colSpan={1}>
+                        {' '}
+                        {idx === 0 && (
+                          <Button onClick={() => props.setParentHash(key[1])}>
+                            <SearchIcon />
+                          </Button>
+                        )}
+                      </GridItem>
+                    </>
+                  )
+                })}
+            </Grid>
             <Table size={'sm'} variant="simple">
               <Thead>
                 {header &&
-                  header.map((key) => {
+                  header.map((key, idx) => {
                     return (
                       <Tr key={key[0] + key[1]}>
                         <Th>{key[0]}</Th>
                         <Th wordBreak={'break-all'}>{key[1]}</Th>
+                        {idx === 0 && (
+                          <Th>
+                            <Button onClick={() => props.setParentHash(key[1])}>
+                              <SearchIcon />
+                            </Button>
+                          </Th>
+                        )}
                       </Tr>
                     )
                   })}
