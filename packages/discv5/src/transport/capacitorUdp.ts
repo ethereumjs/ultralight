b2550c56ac0cd9fd387e6841a22ddf9bedb034ce
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-03-31 14:21:30 -0400

    Add preliminary support for android-udp

---
+++ packages/discv5/src/transport/capacitorUdp.ts
@@ -0,0 +1,62 @@
+import { UDP } from "@frontall/capacitor-udp";
+import { EventEmitter } from "events";
+import { Multiaddr } from "multiaddr";
+
+import { decodePacket, encodePacket, IPacket, MAX_PACKET_SIZE } from "../packet";
+import { IRemoteInfo, ITransportService, TransportEventEmitter } from "./types";
+
+/**
+ * This class is responsible for encoding outgoing Packets and decoding incoming Packets over UDP
+ */
+export class CapacitorUDPTransportService
+  extends (EventEmitter as { new(): TransportEventEmitter })
+  implements ITransportService {
+  public multiaddr: Multiaddr;
+  private socket!: {
+    socketId: number,
+    ipv4: string,
+    ipv6: string
+  };
+  private srcId: string;
+
+  public constructor(multiaddr: Multiaddr, srcId: string) {
+    super();
+    const opts = multiaddr.toOptions();
+    if (opts.transport !== "udp") {
+      throw new Error("Local multiaddr must use UDP");
+    }
+    this.multiaddr = multiaddr;
+    this.srcId = srcId;
+  }
+
+  public async start(): Promise<void> {
+    const opts = this.multiaddr.toOptions();
+    this.socket = await UDP.create();
+    UDP.addListener("receive", ({ socketId, buffer }) => {
+      this.handleIncoming(Buffer.from(atob(buffer)), { family: "IPv4", address: "127.0.0.1", port: 9000, size: 12 });
+    })
+    return new Promise((resolve) => {
+      UDP.bind({ socketId: this.socket.socketId, address: this.socket.ipv4, port: 0 })
+      resolve()
+    });
+  }
+
+  public async stop(): Promise<void> {
+    await UDP.closeAllSockets()
+  }
+
+  public async send(to: Multiaddr, toId: string, packet: IPacket): Promise<void> {
+    const nodeAddr = to.toOptions();
+    await UDP.send({ socketId: this.socket.socketId, address: nodeAddr.host, port: nodeAddr.port, buffer: encodePacket(toId, packet).toString() })
+  }
+
+  public handleIncoming = (data: Buffer, rinfo: IRemoteInfo): void => {
+    const multiaddr = new Multiaddr(`/${rinfo.family === "IPv4" ? "ip4" : "ip6"}/${rinfo.address}/udp/${rinfo.port}`);
+    try {
+      const packet = decodePacket(this.srcId, data);
+      this.emit("packet", multiaddr, packet);
+    } catch (e) {
+      this.emit("decodeError", e, multiaddr);
+    }
+  };
+}
