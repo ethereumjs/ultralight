1a053bd365813bf046508e095044ba9050346406
ScottyPoi <scott.simpson@ethereum.org>
2022-05-04 00:28:45 -0600

    browser: add methods for indexeddb

--- packages/browser-client/src/Components/localStorage.ts
+++ packages/browser-client/src/Components/localStorage.ts
@@ -6,3 +6,24 @@ export function saveToLocalStorage(key: string, value: string) {
   }
 }
 
+export async function addToIndexedDB(type: string, key: string, value: any, db: IDBDatabase) {
+  const request = db.transaction(type, 'readwrite').objectStore(type).add(value, key)
+  request.onsuccess = () => {
+    console.log('added to indexeddb')
+    return request.result
+  }
+  request.onerror = () => {
+    return
+  }
+}
+
+export async function removeFromIndexedDB(type: string, key: string, db: IDBDatabase) {
+  const request = db.transaction(type, 'readwrite').objectStore(type).delete(key)
+  request.onsuccess = () => {
+    console.log('removed from indexeddb')
+    return request.result
+  }
+  request.onerror = () => {
+    return
+  }
+}
