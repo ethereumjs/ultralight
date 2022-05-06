3826d0b3b3c7a3d210cc9e19eb8939f5b40d63b9
ScottyPoi <scott.simpson@ethereum.org>
2022-05-06 00:06:57 -0600

    find content looks locally first

--- packages/browser-client/src/App.tsx
+++ packages/browser-client/src/App.tsx
@@ -335,6 +335,19 @@ export const App = () => {
             fromHexString(header),
             typeof body === 'string' ? fromHexString(body) : body
           )
+            const request = IDB!
+              .transaction('blocks', 'readwrite')
+              .objectStore('blocks')
+              .put(body, blockHash)
+            request!.onsuccess = () => {
+              const req = IDB!
+                .transaction('headers', 'readwrite')
+                .objectStore('headers')
+                .put(header, blockHash)
+              req.onsuccess = () => {}
+              req.onerror = () => {}
+            }
+            request!.onerror = () => {}
           setBlock(block)
           return block
         } catch (err) {
@@ -342,6 +355,7 @@ export const App = () => {
         }
       }
     }
+  }
   }
 
   async function findParent(hash: string) {
