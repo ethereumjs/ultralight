cd976af90c9591539e9a8b1a6bfbbb3961d74893
ScottyPoi <scott.simpson@ethereum.org>
2022-05-06 00:25:04 -0600

    remove logging

--- packages/browser-client/src/Components/localStorage.ts
+++ packages/browser-client/src/Components/localStorage.ts
@@ -9,7 +9,6 @@ export function saveToLocalStorage(key: string, value: string) {
 export async function addToIndexedDB(type: string, key: string, value: any, db: IDBDatabase) {
   const request = db.transaction(type, 'readwrite').objectStore(type).add(value, key)
   request.onsuccess = () => {
-    console.log('added to indexeddb')
     return request.result
   }
   request.onerror = () => {
@@ -20,7 +19,7 @@ export async function addToIndexedDB(type: string, key: string, value: any, db: 
 export async function removeFromIndexedDB(type: string, key: string, db: IDBDatabase) {
   const request = db.transaction(type, 'readwrite').objectStore(type).delete(key)
   request.onsuccess = () => {
-    console.log('removed from indexeddb')
+    console.log('removed entry from indexeddb', type)
     return request.result
   }
   request.onerror = () => {
