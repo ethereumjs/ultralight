e01c06319737b76f91a289b37777989e40217a30
ScottyPoi <scott.simpson@ethereum.org>
2022-06-28 16:08:26 -0600

    CanonicalIndices: Add logic for handling HashArrays

--- packages/portalnetwork/src/subprotocols/canonicalIndices/canonicalIndices.ts
+++ packages/portalnetwork/src/subprotocols/canonicalIndices/canonicalIndices.ts
@@ -3,8 +3,18 @@ import { Debugger } from 'debug'
 import { ProtocolId } from '..'
 import { PortalNetwork } from '../../client'
 import { toHexString } from '../../util/discv5'
+import { ContentLookup } from '../contentLookup'
 import { HistoryProtocol } from '../history/history'
 import { BaseProtocol } from '../protocol'
+import {
+  CanonicalIndicesNetworkContentKeyUnionType,
+  HashArrayContentSSZ,
+  HashArrayLookupKey,
+  HashArrayRequest,
+  HashArrayRequestKeys,
+  HashArrayWithProofSSZ,
+} from './types'
+import { fromHexString } from '@chainsafe/ssz'
 
 export class CanonicalIndicesProtocol extends BaseProtocol {
   logger: Debugger
@@ -12,12 +22,14 @@ export class CanonicalIndicesProtocol extends BaseProtocol {
   readonly protocolName: string
   sendFindContent: undefined
   public _blockIndex: (string | undefined)[]
+  public _rootsList: Uint8Array[]
   constructor(client: PortalNetwork) {
     super(client, 2n ** 256n)
     this.logger = client.logger.extend('canonicalIndices')
     this.protocolId = ProtocolId.CanonicalIndicesNetwork
     this.protocolName = 'Canonical Indices'
     this._blockIndex = []
+    this._rootsList = []
   }
 
   /**
@@ -74,7 +86,11 @@ export class CanonicalIndicesProtocol extends BaseProtocol {
   }
 
   public batchUpdate = (blockHashes: string[], newHeight: number) => {
+    if (newHeight <= 8192) {
+      return
+    }
     const newIndex = new Array(newHeight)
+    newIndex.fill('0')
     this._blockIndex.forEach((value, idx) => {
       newIndex[idx] = value
     })
@@ -84,21 +100,133 @@ export class CanonicalIndicesProtocol extends BaseProtocol {
     })
     this._blockIndex = newIndex
     this.logger('New Block-Index height:', this._blockIndex.length)
-    this.logger('Backfilling Block Index starting from', newHeight - blockHashes.length)
-    this.backFill(blockHashes[0])
+    this.logger('Searching network for hash lists')
+    this.backFillByEpoch()
+    // this.logger('Backfilling Block Index starting from', newHeight - blockHashes.length)
+    // this.backFill(newHeight - blockHashes.length, blockHashes[0])
   }
 
-  public backFill = async (blockHash: string) => {
+  public rootsUpdate = (newList: Uint8Array[]) => {
+    if (newList.length > this._rootsList.length) {
+      // this._rootsList.forEach((root, idx) => {
+      //   if (toHexString(root) !== toHexString(newList[idx])) {
+      //     throw new Error('Discrepency is Epoch Hash Lists')
+      //   }
+      // })
+      this._rootsList = newList.map((root) => {
+        return root
+      })
+      this.backFillByEpoch()
+    }
+  }
+
+  public storeEpoch = async (hashArray: Uint8Array[], proof: Uint8Array, slot: number) => {
+    const key: Uint8Array = HashArrayLookupKey(slot)
+    const value: Uint8Array = HashArrayContentSSZ.serialize({
+      chainId: 1,
+      content: {
+        array: hashArray,
+        proof: proof,
+      },
+    })
+    this.client.db.put(toHexString(key), toHexString(value))
+    this.logger('Storing HashArray with contentKey: ', toHexString(key))
+  }
+
+  updateIndexFromHashArray = (serialized: Uint8Array, slot: number): void => {
+    const hashArrayWith = HashArrayWithProofSSZ.deserialize(serialized)
+    const array = hashArrayWith.array
+    const proof = hashArrayWith.proof
+    const history: HistoryProtocol = this.client.protocols.get(
+      ProtocolId.HistoryNetwork
+    ) as HistoryProtocol
+    if (history) {
+      const historical = [...history.accumulator.historicalEpochs.values()]
+      if (historical.length > Math.floor(8192 / slot))
+        if (toHexString(historical[slot]) !== toHexString(proof)) {
+          return
+        }
+    }
+    array.forEach((value, idx) => {
+      const index = slot * 8192 + idx
+      this._blockIndex[index] = toHexString(value)
+    })
+  }
+
+  public retrieveHashArrayFromRequestKey = async (requestKey: Uint8Array) => {
+    const deserialized = CanonicalIndicesNetworkContentKeyUnionType.deserialize(requestKey)
+    const value: HashArrayRequest = deserialized.value as unknown as HashArrayRequest
+    const key = value.content
+    const hashArrayContent = HashArrayWithProofSSZ.deserialize(
+      fromHexString(await this.client.db.get(toHexString(key)))
+    )
+    return hashArrayContent
+  }
+
+  public backFillByEpoch = async () => {
+    const roots = []
+    for (let i = 0; i < this._rootsList.length; i++) {
+      if (this._blockIndex.slice(i * 8192, i * 8192 + 8191).includes('0')) {
+        roots.push(this._rootsList[i])
+      }
+    }
+
+    const indexHeight: number = this._blockIndex.indexOf('0')
+    const unindexed: number = indexHeight === 0 ? 0 : Math.floor(indexHeight / 8192)
+    const isoEpochs: Uint8Array[] = this._rootsList
+    isoEpochs.forEach(async (epoch, idx) => {
+      if (idx >= unindexed) {
+        this.logger(
+          `Beginning lookup for HashArray for Blocks ${idx * 8192} through ${idx * 8192 + 8191}`
+        )
+        const contentKey = HashArrayRequestKeys[idx]
+        const lookup = new ContentLookup(this, fromHexString(contentKey))
+        const _hashArray = (await lookup.startLookup()) as Uint8Array
+        if (_hashArray) {
+          const hashArrayContent = HashArrayContentSSZ.deserialize(_hashArray)
+          if (toHexString(epoch) !== toHexString(hashArrayContent.content.proof)) {
+            return
+          }
+          hashArrayContent.content.array.forEach((hash, i) => {
+            const index = idx * 8192 + i
+            if (this._blockIndex[index] === '0') {
+              this._blockIndex[index] = toHexString(hash)
+            } else if (this._blockIndex[index] !== toHexString(hash)) {
+              this.logger(
+                `Mismatched Block Hash for block: `,
+                index,
+                ' Had: ',
+                this._blockIndex[index],
+                'Received: ',
+                toHexString(hash)
+              )
+            }
+          })
+          const indexed = this._blockIndex.filter((n) => n !== '0')
+          this.logger(`${indexed.length} / ${this._blockIndex.length} blocks indexed.`)
+        } else {
+          this.logger('HashArray lookup failed')
+        }
+      }
+    })
+  }
+  public backFillGetBlock = async (blockNumber: number, blockHash: string) => {
     const history = this.client.protocols.get(ProtocolId.HistoryNetwork) as HistoryProtocol
     const block = await history.getBlockByHash(blockHash, false)
     if (block) {
       const number = block.header.number.toNumber()
+      if (number !== blockNumber) {
+        this.logger('Numbers do not match')
+      }
       const hash = toHexString(block.hash())
       this._blockIndex[number] = hash
       this.logger('Block', number, 'Indexed by Number')
       if (number > 1) {
-        await this.backFill(toHexString(block.header.parentHash))
+        await this.backFillGetBlock(number - 1, toHexString(block.header.parentHash))
+      } else {
+        return
       }
     }
+    return
   }
 }
