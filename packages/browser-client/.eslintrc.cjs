e0328ec70312304ef47b3050b148b9f155b96b2d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-06-28 06:52:41 -0400

    Update eslint for browser client

--- packages/browser-client/.eslintrc.cjs
+++ packages/browser-client/.eslintrc.cjs
@@ -2,10 +2,11 @@ module.exports = {
   extends: "../../config/eslint.cjs",
   ignorePatterns: ["*.js", "serviceWorker.ts", "index.ts"],
   rules: {
-    "node/file-extension-in-import": "off"
+    "node/file-extension-in-import": "off",
+    "node/no-unsupported-features/es-syntax": "off"
   },
   env: {
-    browser: true
+    browser: true,
   },
   parserOptions: {
     sourceType: 'module',
