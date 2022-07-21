b68f58c7e1463177ce44fe78696d0faddfb3978c
ScottyPoi <scott.simpson@ethereum.org>
2022-07-21 19:41:01 -0400

    browser-client: make changes to receipt helpers

--- packages/browser-client/src/receipts.ts
+++ packages/browser-client/src/receipts.ts
@@ -63,29 +63,29 @@ export type JsonRpcTx = {
 }
 
 export type JsonRpcReceipt = {
-  transactionHash: string
-  transactionIndex: string
-  blockHash: string
-  blockNumber: string
-  from: string
-  to: string | null
-  cumulativeGasUsed: string
-  effectiveGasPrice: string
+  transactionHash?: string
+  transactionIndex?: string
+  blockHash?: string
+  blockNumber?: string
+  from?: string
+  to?: string | null
+  cumulativeGasUsed?: string
+  effectiveGasPrice?: string
   gasUsed: string
-  contractAddress: string | null
+  contractAddress?: string | null
   logs: JsonRpcLog[]
   logsBloom: string
-  root?: string
+  stateRoot?: string
   status?: string
 }
 
 export type JsonRpcLog = {
-  removed: boolean
-  logIndex: string | null
-  transactionIndex: string | null
+  removed?: boolean
+  logIndex?: string | null
+  transactionIndex?: string | null
   transactionHash: string | null
-  blockHash: string | null
-  blockNumber: string | null
+  blockHash?: string | null
+  blockNumber?: string | null
   address: string
   data: string
   topics: string[]
@@ -124,65 +124,107 @@ export const jsonRpcTx = (tx: TypedTransaction, block?: Block, txIndex?: number)
  */
 export const jsonRpcLog = (
   log: Log,
+  txHash: string,
   block?: Block,
   tx?: TypedTransaction,
   txIndex?: number,
   logIndex?: number
-): JsonRpcLog => ({
-  removed: false, // TODO implement
-  logIndex: logIndex !== undefined ? intToHex(logIndex) : null,
-  transactionIndex: txIndex !== undefined ? intToHex(txIndex) : null,
-  transactionHash: tx ? bufferToHex(tx.hash()) : null,
-  blockHash: block ? bufferToHex(block.hash()) : null,
-  blockNumber: block ? bnToHex(block.header.number) : null,
-  address: bufferToHex(log[0]),
-  topics: log[1].map((t) => bufferToHex(t as Buffer)),
-  data: bufferToHex(log[2]),
-})
+): JsonRpcLog => {
+  let jsonLog: JsonRpcLog
+  if (block && tx) {
+    jsonLog = {
+      removed: false, // TODO implement
+      logIndex: logIndex !== undefined ? intToHex(logIndex) : null,
+      transactionIndex: txIndex !== undefined ? intToHex(txIndex) : null,
+      transactionHash: tx ? bufferToHex(tx.hash()) : null,
+      blockHash: block ? bufferToHex(block.hash()) : null,
+      blockNumber: block ? bnToHex(block.header.number) : null,
+      address: bufferToHex(log[0]),
+      topics: log[1].map((t) => bufferToHex(t as Buffer)),
+      data: bufferToHex(log[2]),
+    }
+  } else {
+    jsonLog = {
+      transactionHash: txHash,
+      address: bufferToHex(log[0]),
+      topics: log[1].map((t) => bufferToHex(t as Buffer)),
+      data: bufferToHex(log[2]),
+    }
+  }
+  return jsonLog
+}
 
 /**
  * Returns receipt formatted to the standard JSON-RPC fields
  */
-export const jsonRpcReceipt = (
+export function jsonRpcReceipt(
   receipt: TxReceipt,
-  gasUsed: bigint,
-  effectiveGasPrice: bigint,
-  block: Block,
-  tx: TypedTransaction,
-  txIndex: number,
-  logIndex: number,
+  txHash: string,
+  gasUsed?: bigint,
+  effectiveGasPrice?: bigint,
+  block?: Block,
+  tx?: TypedTransaction,
+  txIndex?: number,
+  logIndex?: number,
   contractAddress?: Address
-): JsonRpcReceipt => ({
-  transactionHash: bufferToHex(tx.hash()),
-  transactionIndex: intToHex(txIndex),
-  blockHash: bufferToHex(block.hash()),
-  blockNumber: bnToHex(block.header.number),
-  from: tx.getSenderAddress().toString(),
-  to: tx.to?.toString() ?? null,
-  cumulativeGasUsed: bufferToHex(receipt.gasUsed),
-  effectiveGasPrice: bigIntToHex(effectiveGasPrice),
-  gasUsed: bigIntToHex(gasUsed),
-  contractAddress: contractAddress?.toString() ?? null,
-  logs: receipt.logs.map((l, i) => jsonRpcLog(l, block, tx, txIndex, logIndex + i)),
-  logsBloom: bufferToHex(receipt.bitvector),
-  root: Buffer.isBuffer(receipt.stateRoot) ? bufferToHex(receipt.stateRoot) : undefined,
-  status: Buffer.isBuffer(receipt.status) ? intToHex(receipt.status) : undefined,
-})
+): JsonRpcReceipt {
+  let jsonReceipt: JsonRpcReceipt
+  try {
+    jsonReceipt = {
+      transactionHash: bufferToHex(tx!.hash()),
+      transactionIndex: intToHex(txIndex!),
+      blockHash: bufferToHex(block!.hash()),
+      blockNumber: bnToHex(block!.header.number),
+      from: tx!.getSenderAddress().toString(),
+      to: tx!.to?.toString() ?? null,
+      cumulativeGasUsed: bufferToHex(receipt.gasUsed),
+      effectiveGasPrice: bigIntToHex(effectiveGasPrice!),
+      gasUsed: bigIntToHex(gasUsed!),
+      contractAddress: contractAddress?.toString() ?? null,
+      logs: receipt.logs.map((l, i) => jsonRpcLog(l, txHash, block, tx, txIndex, logIndex! + i)),
+      logsBloom: bufferToHex(receipt.bitvector),
+      stateRoot: Buffer.isBuffer(receipt.stateRoot) ? bufferToHex(receipt.stateRoot) : undefined,
+      status: Buffer.isBuffer(receipt.status) ? intToHex(receipt.status) : undefined,
+    }
+  } catch {
+    const logs = receipt.logs.map((log) => {
+      return {
+        transactionHash: txHash,
+        address: bufferToHex(log[0]),
+        topics: log[1].map((t) => bufferToHex(t as Buffer)),
+        data: bufferToHex(log[2]),
+      }
+    })
+    jsonReceipt = {
+      gasUsed: toHexString(receipt.gasUsed),
+      logsBloom: toHexString(receipt.bitvector),
+      logs: logs,
+      status: Buffer.isBuffer(receipt.status) ? toHexString(receipt.status) : undefined,
+      stateRoot: Buffer.isBuffer(receipt.stateRoot) ? toHexString(receipt.stateRoot) : undefined,
+    }
+  }
+  return jsonReceipt
+}
+
+/**
+ * Returns receipt formatted to the standard JSON-RPC fields
+ */
 
 export function decodeReceipt(
   receipt: string,
-  transaction: TypedTransaction,
-  gasPrice: string,
-  block: Block,
-  txIdx: number
-) {
+  txHash: string,
+  transaction?: TypedTransaction,
+  gasPrice?: string,
+  block?: Block,
+  txIdx?: number
+): JsonRpcReceipt {
   const decoded = rlp.decode(receipt) as NestedBufferArray
   const status =
     Number((decoded[0] as Buffer).toString('hex')) === 0
       ? 0
-      : Number(decoded[0].toString('hex')) === 0
+      : Number(decoded[0].toString('hex')) === 1
       ? 1
-      : 0
+      : undefined
   const gasUsed = decoded[1] as Buffer
   const bitvector = decoded[2] as Buffer
   const logs: Log[] = (decoded[3] as NestedBufferArray[]).map((_log) => {
@@ -199,8 +241,21 @@ export function decodeReceipt(
     logs,
   }
   const gu = BigInt(toHexString(gasUsed))
-  const jsonReceipt = jsonRpcReceipt(rec, gu, BigInt(gasPrice), block, transaction, txIdx, txIdx)
-
+  let jsonReceipt: JsonRpcReceipt
+  try {
+    jsonReceipt = jsonRpcReceipt(
+      rec,
+      txHash,
+      gu,
+      BigInt(gasPrice!),
+      block!,
+      transaction!,
+      txIdx!,
+      0
+    )
+  } catch (e) {
+    jsonReceipt = jsonRpcReceipt(rec, txHash)
+  }
   return jsonReceipt
 }
 export {}
