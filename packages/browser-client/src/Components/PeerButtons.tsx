8026d4d74f32414fec5267343fb94c53d1c1407e
ScottyPoi <scott.simpson@ethereum.org>
2022-08-17 18:59:17 -0400

    browser-client: Refactor network tab with PeerTools

---
+++ packages/browser-client/src/Components/PeerButtons.tsx
@@ -0,0 +1,395 @@
+import { ArrowLeftIcon, ArrowRightIcon, CopyIcon } from '@chakra-ui/icons'
+import {
+  Box,
+  Button,
+  Divider,
+  GridItem,
+  Heading,
+  HStack,
+  IconButton,
+  Input,
+  Table,
+  Tbody,
+  Td,
+  Text,
+  Th,
+  Tooltip,
+  Tr,
+  VStack,
+} from '@chakra-ui/react'
+import {
+  ENR,
+  fromHexString,
+  HistoryNetworkContentKeyUnionType,
+  HistoryNetworkContentTypes,
+  reassembleBlock,
+  shortId,
+} from 'portalnetwork'
+import React, { Dispatch, SetStateAction, useContext, useState } from 'react'
+import { BlockContext, HistoryProtocolContext } from '../App'
+
+export interface PeerButtonsProps {
+  peerIdx: number
+  setPeerIdx: Dispatch<SetStateAction<number>>
+  sortedDistList: any
+  peer: ENR
+  peers: ENR[]
+}
+
+export default function PeerButtons(props: PeerButtonsProps) {
+  const { block, setBlock } = useContext(BlockContext)
+  const [epoch, setEpoch] = useState(0)
+  const { peerIdx, setPeerIdx, sortedDistList, peer } = props
+  const [offer, setOffer] = useState<Uint8Array[]>([])
+  const [pinging, setPinging] = useState(false)
+  const [ponged, setPonged] = useState<boolean | undefined>()
+  const [distance, setDistance] = useState('')
+  const [blockHash, setBlockHash] = useState('')
+  const historyProtocol = useContext(HistoryProtocolContext)
+  const addToOffer = (type: string) => {
+    const contentKeys: Uint8Array[] = []
+    switch (type) {
+      case 'header':
+        contentKeys.push(
+          HistoryNetworkContentKeyUnionType.serialize({
+            selector: HistoryNetworkContentTypes.BlockHeader,
+            value: {
+              chainId: 1,
+              blockHash: fromHexString(blockHash),
+            },
+          })
+        )
+        break
+      case 'body':
+        contentKeys.push(
+          HistoryNetworkContentKeyUnionType.serialize({
+            selector: HistoryNetworkContentTypes.BlockBody,
+            value: {
+              chainId: 1,
+              blockHash: fromHexString(blockHash),
+            },
+          })
+        )
+        break
+      case 'block':
+        contentKeys.push(
+          HistoryNetworkContentKeyUnionType.serialize({
+            selector: HistoryNetworkContentTypes.BlockHeader,
+            value: {
+              chainId: 1,
+              blockHash: fromHexString(blockHash),
+            },
+          })
+        )
+        contentKeys.push(
+          HistoryNetworkContentKeyUnionType.serialize({
+            selector: HistoryNetworkContentTypes.BlockBody,
+            value: {
+              chainId: 1,
+              blockHash: fromHexString(blockHash),
+            },
+          })
+        )
+
+        break
+    }
+    setOffer([...offer, ...contentKeys])
+  }
+  const handlePing = async () => {
+    setPinging(true)
+    setTimeout(async () => {
+      const pong = await historyProtocol.sendPing(peer)
+      if (pong) {
+        setPonged(true)
+        setTimeout(() => {
+          setPinging(false)
+          setPonged(undefined)
+        }, 1500)
+      } else {
+        setPonged(false)
+        setTimeout(() => {
+          setPinging(false)
+          setPonged(undefined)
+        }, 1000)
+      }
+    }, 500)
+  }
+  const handleFindNodes = (peer: ENR) => {
+    historyProtocol.sendFindNodes(peer.nodeId, [parseInt(distance)])
+  }
+  const handleRequestSnapshot = () => {
+    const accumulatorKey = HistoryNetworkContentKeyUnionType.serialize({
+      selector: 4,
+      value: { selector: 0, value: null },
+    })
+    historyProtocol.sendFindContent(peer.nodeId, accumulatorKey)
+  }
+
+  const handleOffer = (peer: ENR) => {
+    historyProtocol.sendOffer(peer.nodeId, offer)
+  }
+  const sendFindContent = async (type: string) => {
+    if (type === 'header') {
+      const headerKey = HistoryNetworkContentKeyUnionType.serialize({
+        selector: 0,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(blockHash),
+        },
+      })
+      historyProtocol.sendFindContent(peer.nodeId, headerKey)
+    } else if (type === 'body') {
+      const headerKey = HistoryNetworkContentKeyUnionType.serialize({
+        selector: 0,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(blockHash),
+        },
+      })
+      historyProtocol.sendFindContent(peer.nodeId, headerKey)
+      const bodyKey = HistoryNetworkContentKeyUnionType.serialize({
+        selector: 1,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(blockHash),
+        },
+      })
+      historyProtocol.sendFindContent(peer.nodeId, bodyKey)
+    } else if (type === 'block') {
+      const headerKey = HistoryNetworkContentKeyUnionType.serialize({
+        selector: 0,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(blockHash),
+        },
+      })
+      const bodyKey = HistoryNetworkContentKeyUnionType.serialize({
+        selector: 1,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(blockHash),
+        },
+      })
+      try {
+        const header = (await historyProtocol.sendFindContent(peer.nodeId, headerKey))
+          ?.value as Uint8Array
+        const _body = await historyProtocol.sendFindContent(peer.nodeId, bodyKey)
+        const body: Uint8Array | undefined =
+          _body !== undefined ? (_body.value as Uint8Array) : undefined
+        const block = reassembleBlock(header, body)
+        setBlock(block)
+      } catch {}
+    } else if (type === 'epoch') {
+      const epochKey = HistoryNetworkContentKeyUnionType.serialize({
+        selector: 3,
+        value: {
+          chainId: 1,
+          blockHash: historyProtocol.accumulator.historicalEpochs[epoch],
+        },
+      })
+    }
+  }
+  return (
+    <GridItem>
+      <Box border={'1px'}>
+        <VStack>
+          <HStack>
+            <VStack>
+              <HStack>
+                <IconButton
+                  disabled={peerIdx < 1}
+                  onClick={() => setPeerIdx(peerIdx - 1)}
+                  aria-label="prev peer"
+                  icon={<ArrowLeftIcon />}
+                />
+                <Heading size={'md'}>
+                  Peer {peerIdx + 1} / {props.peers.length}
+                </Heading>
+                <IconButton
+                  onClick={() => setPeerIdx(peerIdx + 1)}
+                  disabled={peerIdx === props.peers.length - 1}
+                  aria-label="prev peer"
+                  icon={<ArrowRightIcon />}
+                />
+              </HStack>
+              <Table size="xs">
+                <Tbody>
+                  <Tr>
+                    <Td>ENR:</Td>
+                    <Th>
+                      <Tooltip label={props.sortedDistList[peerIdx][1][3]}>
+                        <CopyIcon
+                          cursor={'pointer'}
+                          onClick={() =>
+                            navigator.clipboard.writeText(sortedDistList[peerIdx][1][3])
+                          }
+                        />
+                      </Tooltip>
+                    </Th>
+                  </Tr>
+                  <Tr>
+                    <Td>Addr: </Td>
+                    <Td>
+                      {props.sortedDistList[peerIdx][1][0]}: {sortedDistList[peerIdx][1][1]}
+                    </Td>
+                  </Tr>
+                  <Tr>
+                    <Td>NodeId: </Td>
+                    <Td>{shortId(peer.nodeId)}</Td>
+                  </Tr>
+                </Tbody>
+              </Table>
+            </VStack>
+            <Button
+              size="lg"
+              onClick={() => handlePing()}
+              bgColor={
+                ponged === true
+                  ? 'green.100'
+                  : ponged === false
+                  ? 'red.100'
+                  : pinging
+                  ? 'yellow.100'
+                  : 'blue.100'
+              }
+            >
+              {ponged === true
+                ? 'PONG RECEIVED!'
+                : ponged === false
+                ? 'PING/PONG FAILED'
+                : pinging
+                ? 'PINGING'
+                : 'Send PING'}{' '}
+            </Button>
+          </HStack>
+          <Button width="100%" onClick={() => handleRequestSnapshot()}>
+            Request Accumulator Snapshot
+          </Button>
+          <Divider />
+          <HStack width={'100%'}>
+            <Button
+              isDisabled={historyProtocol.accumulator.historicalEpochs.length < 1}
+              width="70%"
+              onClick={() => sendFindContent('epoch')}
+            >
+              Request Epoch Accumulator by Epoch
+            </Button>
+            <Input
+              type={'number'}
+              min={1}
+              max={historyProtocol.accumulator.historicalEpochs.length}
+              width={'30%'}
+              placeholder={'Epoch'}
+              onChange={(evt) => {
+                setEpoch(parseInt(evt.target.value))
+              }}
+            />
+          </HStack>
+          <Divider />
+          <HStack width={'100%'}>
+            <Button
+              isDisabled={historyProtocol.accumulator.historicalEpochs.length < 1}
+              width="70%"
+              onClick={() => sendFindContent('epoch')}
+            >
+              Request Epoch Accumulator by BlockNumber
+            </Button>
+            <Input
+              type={'number'}
+              min={1}
+              max={historyProtocol.accumulator.currentHeight()}
+              width={'30%'}
+              placeholder={`BlockNumber (Max: ${historyProtocol.accumulator.currentHeight()})`}
+              onChange={(evt) => {
+                setEpoch(Math.floor(parseInt(evt.target.value) / 8192))
+              }}
+            />
+          </HStack>
+          <Divider />
+          <HStack width={'100%'}>
+            <Button width="70%" onClick={() => handleFindNodes(peer)}>
+              FindNodes
+            </Button>
+            <Input
+              width={'30%'}
+              placeholder={'Distance'}
+              onChange={(evt) => {
+                setDistance(evt.target.value)
+              }}
+            />
+          </HStack>
+          <Divider />
+          <Input
+            value={blockHash}
+            placeholder="BlockHash"
+            onChange={(evt) => setBlockHash(evt.target.value)}
+          />
+          <HStack width={'100%'}>
+            <Button
+              width={'33%'}
+              title="Add content to offer"
+              onClick={() => {
+                sendFindContent('header')
+              }}
+            >
+              Find Header
+            </Button>
+            <Button
+              width={'33%'}
+              title="Add content to offer"
+              onClick={() => {
+                sendFindContent('body')
+              }}
+            >
+              Find Body
+            </Button>
+            <Button
+              width={'33%'}
+              title="Add content to offer"
+              onClick={() => {
+                sendFindContent('block')
+              }}
+            >
+              Find Block
+            </Button>
+          </HStack>
+          <HStack width={'100%'}>
+            <Button
+              width={'33%'}
+              title="Add content to offer"
+              onClick={() => {
+                addToOffer('header')
+              }}
+            >
+              Offer Header
+            </Button>
+            <Button
+              width={'33%'}
+              title="Add content to offer"
+              onClick={() => {
+                addToOffer('body')
+              }}
+            >
+              Offer Body
+            </Button>
+            <Button
+              width={'33%'}
+              title="Add content to offer"
+              onClick={() => {
+                addToOffer('block')
+              }}
+            >
+              Offer Block
+            </Button>
+          </HStack>
+          <Box width={'90%'} border={'1px'}>
+            <Text textAlign={'center'}>OFFER: {offer.length} / 26</Text>
+          </Box>
+          <Button width={'100%'} onClick={() => handleOffer(peer)}>
+            Send Offer
+          </Button>
+        </VStack>
+      </Box>
+    </GridItem>
+  )
+}
--- packages/browser-client/src/Components/RoutingTableView.tsx
+++ packages/browser-client/src/Components/RoutingTableView.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { ENR } from 'portalnetwork'
-import { CopyIcon } from '@chakra-ui/icons'
+import React, { useContext, useState } from 'react'
+import { ENR, fromHexString, HistoryNetworkContentKeyUnionType, shortId } from 'portalnetwork'
+import { ArrowLeftIcon, ArrowRightIcon, CopyIcon } from '@chakra-ui/icons'
 import {
   Table,
   Box,
@@ -12,52 +12,144 @@ import {
   Tr,
   Td,
   Tooltip,
+  VStack,
+  HStack,
+  Button,
+  Divider,
+  Input,
+  useToast,
+  Text,
+  GridItem,
+  SimpleGrid,
+  IconButton,
+  Heading,
 } from '@chakra-ui/react'
+import { HistoryProtocolContext } from '../App'
+import PeerButtons from './PeerButtons'
 
 interface RoutingTableViewProps {
-  peers: ENR[] | undefined
+  peers: ENR[]
   sortedDistList: [number, string[]][]
 }
 
 export default function RoutingTableView(props: RoutingTableViewProps) {
+  const [peerIdx, setPeerIdx] = useState(0)
+  const [hover, setHover] = useState<number>()
+  const [peer, _setPeer] = useState(props.peers[peerIdx])
+  const [distance, setDistance] = useState('')
+  const [blockHash, setBlockHash] = useState('')
+  const [offer, setOffer] = useState<string[]>([])
+  const [pinging, setPinging] = useState(false)
+  const [ponged, setPonged] = useState<boolean | undefined>()
+  const toast = useToast()
+
+  const protocol = useContext(HistoryProtocolContext)
+
+  const handlePing = async () => {
+    setPinging(true)
+    setTimeout(async () => {
+      const pong = await protocol.sendPing(peer)
+      if (pong) {
+        setPonged(true)
+        setTimeout(() => {
+          setPinging(false)
+          setPonged(undefined)
+        }, 1500)
+      } else {
+        setPonged(false)
+        setTimeout(() => {
+          setPinging(false)
+          setPonged(undefined)
+        }, 1000)
+      }
+    }, 500)
+  }
+  const handleFindNodes = (peer: ENR) => {
+    protocol.sendFindNodes(peer.nodeId, [parseInt(distance)])
+  }
+  const handleRequestSnapshot = () => {
+    const accumulatorKey = HistoryNetworkContentKeyUnionType.serialize({
+      selector: 4,
+      value: { selector: 0, value: null },
+    })
+    protocol.sendFindContent(peer.nodeId, accumulatorKey)
+  }
+
+  const handleOffer = (peer: ENR) => {
+    if (blockHash.slice(0, 2) !== '0x') {
+      setBlockHash('')
+      toast({
+        title: 'Invalid content key',
+        description: 'Key must be hex prefixed',
+        status: 'warning',
+      })
+      return
+    }
+    protocol.sendOffer(peer.nodeId, [fromHexString(blockHash)])
+  }
+
+  const addToOffer = () => {
+    setOffer([...offer, blockHash])
+  }
+
   return (
     <Center>
-      <Box width={'90%'} maxHeight="40%">
-        <Center>
-          <Table size="xs">
-            <TableCaption>Peers: {props.peers?.length}</TableCaption>
-            <Thead>
-              <Tr>
-                <Th>ENR</Th>
-                <Th>DIST</Th>
-                <Th>IP</Th>
-                <Th>PORT</Th>
-                <Th>NodeId</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {props.sortedDistList.map((peer) => {
-                return (
-                  <Tr key={peer[1][2]}>
-                    <Td key={peer[1][2] + 'abc'}>
-                      <Tooltip label={peer[1][3]}>
-                        <CopyIcon
-                          cursor={'pointer'}
-                          onClick={() => navigator.clipboard.writeText(peer[1][3])}
-                        />
-                      </Tooltip>
-                    </Td>
-                    <Th>{peer[0]}</Th>
-                    <Td>{peer[1][0]}</Td>
-                    <Td>{peer[1][1]}</Td>
-                    <Td>{peer[1][2].slice(0, 15) + '...'}</Td>
-                  </Tr>
-                )
-              })}
-            </Tbody>
-          </Table>
-        </Center>
-      </Box>
+      {/* <Box width={'90%'} maxHeight="10%"> */}
+      <SimpleGrid width={'100%'} columns={[1, 1, 1, 2]}>
+        <PeerButtons
+          peers={props.peers}
+          peer={peer}
+          setPeerIdx={setPeerIdx}
+          peerIdx={peerIdx}
+          sortedDistList={props.sortedDistList}
+        />
+        <GridItem>
+          <Center>
+            <Table size="xs">
+              <TableCaption>Peers: {props.peers?.length}</TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>ENR</Th>
+                  <Th>DIST</Th>
+                  <Th>IP</Th>
+                  <Th>PORT</Th>
+                  <Th>NodeId</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {props.sortedDistList.map((peer, idx) => {
+                  return (
+                    <Tr
+                      key={peer[1][2]}
+                      onMouseEnter={() => setHover(idx)}
+                      onMouseLeave={() => {
+                        setHover(undefined)
+                      }}
+                      onClick={() => setPeerIdx(idx)}
+                      backgroundColor={
+                        idx === hover ? 'blue.100' : peerIdx === idx ? 'red.100' : 'whiteAlpha.100'
+                      }
+                    >
+                      <Td key={peer[1][2] + 'abc'}>
+                        <Tooltip label={peer[1][3]}>
+                          <CopyIcon
+                            cursor={'pointer'}
+                            onClick={() => navigator.clipboard.writeText(peer[1][3])}
+                          />
+                        </Tooltip>
+                      </Td>
+                      <Th>{peer[0]}</Th>
+                      <Td>{peer[1][0]}</Td>
+                      <Td>{peer[1][1]}</Td>
+                      <Td>{peer[1][2].slice(0, 15) + '...'}</Td>
+                    </Tr>
+                  )
+                })}
+              </Tbody>
+            </Table>
+          </Center>
+        </GridItem>
+      </SimpleGrid>
     </Center>
   )
 }
