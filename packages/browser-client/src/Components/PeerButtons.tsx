1e88556038fb72487805cf504fd02058fa9f0526
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2022-08-19 12:54:55 -0400

    Remove broken left/right buttons

--- packages/browser-client/src/Components/PeerButtons.tsx
+++ packages/browser-client/src/Components/PeerButtons.tsx
@@ -1,4 +1,4 @@
-import { ArrowLeftIcon, ArrowRightIcon, CopyIcon } from '@chakra-ui/icons'
+import { CopyIcon } from '@chakra-ui/icons'
 import {
   Box,
   Button,
@@ -6,7 +6,6 @@ import {
   GridItem,
   Heading,
   HStack,
-  IconButton,
   Input,
   Table,
   Tbody,
@@ -40,7 +39,7 @@ export default function PeerButtons(props: PeerButtonsProps) {
   const { setBlock } = useContext(BlockContext)
   const peers = useContext(PeersContext)
   const [epoch, setEpoch] = useState(0)
-  const { peerIdx, setPeerIdx, sortedDistList, peer } = props
+  const { peerIdx, sortedDistList, peer } = props
   const [offer, setOffer] = useState<Uint8Array[]>([])
   const [pinging, setPinging] = useState(false)
   const [ponged, setPonged] = useState<boolean | undefined>()
@@ -189,7 +188,7 @@ export default function PeerButtons(props: PeerButtonsProps) {
         setBlock(block)
       } catch {}
     } else if (type === 'epoch') {
-      const epochKey = HistoryNetworkContentKeyUnionType.serialize({
+      const _epochKey = HistoryNetworkContentKeyUnionType.serialize({
         selector: 3,
         value: {
           chainId: 1,
@@ -205,21 +204,9 @@ export default function PeerButtons(props: PeerButtonsProps) {
           <HStack>
             <VStack>
               <HStack>
-                <IconButton
-                  disabled={peerIdx < 1}
-                  onClick={() => setPeerIdx(peerIdx - 1)}
-                  aria-label="prev peer"
-                  icon={<ArrowLeftIcon />}
-                />
                 <Heading size={'md'}>
                   Peer {peerIdx + 1} / {peers.length}
                 </Heading>
-                <IconButton
-                  onClick={() => setPeerIdx(peerIdx + 1)}
-                  disabled={peerIdx === peers.length - 1}
-                  aria-label="prev peer"
-                  icon={<ArrowRightIcon />}
-                />
               </HStack>
               <Table size="xs">
                 <Tbody>
