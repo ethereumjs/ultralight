4acb0680ee6e4c220dd01b784523d5c5d1d99c93
ScottyPoi <scott.simpson@ethereum.org>
2022-08-20 12:35:58 -0400

    portalnetwork: create, encode, and decode methods for receipt

--- packages/portalnetwork/src/subprotocols/history/receipt.ts
+++ packages/portalnetwork/src/subprotocols/history/receipt.ts
@@ -25,4 +25,63 @@ export class Receipt {
     this.status = opts.status
     this.txType = opts.txType
   }
+  encoded = (): Buffer => {
+    const rlpReceipt: rlpReceipt = [
+      this.stateRoot ?? intToBuffer(this.status!),
+      bigIntToBuffer(this.cumulativeBlockGasUsed),
+      this.logs,
+    ]
+    const receipt = Buffer.from(RLP.encode(rlpReceipt))
+    if (this.txType) {
+      const byte = Buffer.alloc(1)
+      byte[0] = this.txType
+      return Buffer.concat([byte, receipt])
+    } else {
+      return receipt
+    }
+  }
+
+  rlpDecode = (receipt: Buffer): TxReceiptType => {
+    const r = RLP.decode(Uint8Array.from(receipt)) as rlpReceipt
+    const cumulativeBlockGasUsed = bufferToBigInt(r[1])
+    const logs = RLP.decode(r[2])
+    if (r[0].length === 32) {
+      // Pre-Byzantium Receipt
+      return {
+        stateRoot: r[0],
+        cumulativeBlockGasUsed,
+        logs,
+      } as PreByzantiumTxReceipt
+    } else {
+      // Post-Byzantium Receipt
+      return {
+        status: bufferToInt(r[0]),
+        cumulativeBlockGasUsed,
+        logs,
+      } as PostByzantiumTxReceipt
+    }
+  }
+
+  fromEncoded = (encoded: Buffer, type: boolean): Receipt => {
+    const receipt = this.rlpDecode(encoded.subarray(1))
+    if (type) {
+      const type = encoded[0]
+      return new Receipt({
+        bitvector: receipt.bitvector,
+        cumulativeBlockGasUsed: receipt.cumulativeBlockGasUsed,
+        logs: receipt.logs,
+        stateRoot: (receipt as any).stateRoot,
+        status: (receipt as any).status,
+        txType: type,
+      })
+    } else {
+      return new Receipt({
+        bitvector: receipt.bitvector,
+        cumulativeBlockGasUsed: receipt.cumulativeBlockGasUsed,
+        logs: receipt.logs,
+        stateRoot: (receipt as any).stateRoot,
+        status: (receipt as any).status,
+      })
+    }
+  }
 }
