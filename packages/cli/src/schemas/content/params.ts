bd078015753d888aa3c2136936020c70b7e9aa11
ScottyPoi <scott.simpson@ethereum.org>
2022-09-13 17:18:46 -0600

    create ConnectionId validator

--- packages/cli/src/schemas/content/params.ts
+++ packages/cli/src/schemas/content/params.ts
@@ -1,2 +1,13 @@
+import { INVALID_PARAMS } from '../../error-code.js'
 export const content_params = {
+  get ConnectionId() {
+    return (params: any[], index: number) => {
+      if (typeof params[index] !== 'number') {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: argument is not a number`,
+        }
+      }
+    }
+  },
 }
