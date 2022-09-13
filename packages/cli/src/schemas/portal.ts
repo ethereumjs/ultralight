c62c47676ae535d558d7f5e2fb4be27b9fc34527
ScottyPoi <scott.simpson@ethereum.org>
2022-09-13 13:45:07 -0600

    add validator for Bucket

--- packages/cli/src/schemas/portal.ts
+++ packages/cli/src/schemas/portal.ts
@@ -7,4 +7,29 @@ import { baseTypes } from './baseTypes.js'
  * @memberof module:portal
  */
 export const portal = {
+  /**
+   * bucket validator to ensure has array of up to 16 bytes32
+   * @param params parameters of method
+   * @param index index of parameter
+   */
+  get Bucket() {
+    return (params: any[], index: number) => {
+      if (!Array.isArray(params[index])) {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: argument is not array`,
+        }
+      }
+      if (params[index].length > 16) {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: max length is 16`,
+        }
+      }
+      for (const value of params[index]) {
+        const result = baseTypes.bytes32([value], 0)
+        if (result !== undefined) return result
+      }
+    }
+  },
 }
