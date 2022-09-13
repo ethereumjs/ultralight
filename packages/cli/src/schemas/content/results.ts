1cd999a51cd1b8944c5d5365f9f35019bad06de0
ScottyPoi <scott.simpson@ethereum.org>
2022-09-13 17:23:46 -0600

    create AcceptResult validator

--- packages/cli/src/schemas/content/results.ts
+++ packages/cli/src/schemas/content/results.ts
@@ -1,2 +1,13 @@
+import { INVALID_PARAMS } from '../../error-code.js'
 export const results = {
+  get AcceptResult() {
+    return (params: any[], index: number) => {
+      if (typeof params[index] !== 'boolean') {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: argument is not a bool`,
+        }
+      }
+    }
+  },
 }
