d9028f4b58c8d7ada494e648e15cb5eca6653d89
ScottyPoi <scott.simpson@ethereum.org>
2022-09-13 17:26:57 -0600

    create *ContentResult validators

--- packages/cli/src/schemas/content/results.ts
+++ packages/cli/src/schemas/content/results.ts
@@ -55,4 +55,50 @@ export const results = {
       }
     }
   },
+  get ContentResult() {
+    return (params: any[], index: number) => {
+      if (typeof params[index] !== 'boolean') {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: argument is not a bool`,
+        }
+      }
+    }
+  },
+  get SendContentResult() {
+    return (params: any[], index: number) => {
+      return baseTypes.bytes8([params[index]], 0)
+    }
+  },
+  get FindContentResult() {
+    return (params: any[], index: number) => {
+      if (typeof params[index] !== 'object') {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: argument is not an object`,
+        }
+      }
+      const values = Object.values(params[index])
+      if (values.length !== 3) {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: expected 3 values`,
+        }
+      }
+      if (typeof values[0] !== 'number') {
+        return {
+          code: INVALID_PARAMS,
+          message: `invalid argument ${index}: argument is not a number`,
+        }
+      }
+      const content = baseTypes.hexString([values[1]], 0)
+      if (content) {
+        return content
+      }
+      const enrs = content_params.Enrs([values[2]], 0)
+      if (enrs) {
+        return enrs
+      }
+    }
+  },
 }
