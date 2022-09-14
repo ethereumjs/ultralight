15bd51ffcf2ec9c4eecde81bb4a078e5338a450b
ScottyPoi <scott.simpson@ethereum.org>
2022-09-14 14:04:11 -0600

    add typedocs for discv5 methods

--- packages/cli/src/modules/discv5.ts
+++ packages/cli/src/modules/discv5.ts
@@ -89,22 +89,129 @@ export class discv5 {
       [schema.content_params.NodeId],
     ])
   }
+  /**
+   * Returns ENR and nodeId information of the local discv5 node.
+   * @param params an empty array
+   */
   async discv5_nodeInfo(params: []) {}
+
+  /**
+   * Add, update, or remove a key-value pair from the local node record
+   * @param params An array of two parameters:
+   *  1. socketAddr: ENR socket address
+   *  2. *optional* isTcp: TCP or UDP socket
+   */
   async discv5_updateNodeInfo(params: [string, boolean | undefined]) {}
+
+  /**
+   * Returns meta information about discv5 routing table.
+   * @param params an empty array
+   */
   async discv5_routingTableInfo(params: []) {}
+
+  /**
+   * Write an ethereum node record to the routing table.
+   * @param params ENR string
+   */
   async discv5_addEnr(params: [string]) {}
+
+  /**
+   * Fetch the latest ENR associated with the given node ID
+   * @param params NodeId string
+   */
   async discv5_getEnr(params: [string]) {}
+
+  /**
+   * Delete a Node ID from the routing table
+   * @param params NodeId string
+   */
   async discv5_deleteEnr(params: [string]) {}
+
+  /**
+   * Fetch the ENR representation associated with the given Node ID and optional sequence number
+   * @param params An array of two parameters:
+   * 1. NodeId string
+   * 2. ENR Seq number
+   */
   async discv5_lookupEnr(params: [string, number]) {}
+
+  /**
+   * Send a PING message to the designated node and wait for a PONG response.
+   * @param params ENR string
+   */
   async discv5_ping(params: [string]) {}
+
+  /**
+   * Send a PING message to the specified node
+   * @param params An array of two parameters
+   * 1. ENR string
+   * 2. RequestId
+   */
   async discv5_sendPing(params: [string]) {}
+
+  /**
+   * Send a FINDNODE request to a peer, to search within the given set of distances
+   * @param params An arrray of two parameters
+   * 1. ENR string
+   * 2. Distances
+   */
   async discv5_sendPong(params: [string, number]) {}
-  async discv5_findNode(params: [string, number[]]) {}
+
+  /**
+   * Send a FINDNODE request for nodes that fall within the given set of distances, to the designated peer and wait for a response.
+   * @param params An arrray of two parameters
+   * 1. ENR string
+   * 2. Distances
+   */
+  async discv5_findNode(params: [string, number[], number]) {}
+
+  /**
+   * Lookup a target node within in the network
+   * @param params NodeId string
+   */
   async discv5_recursiveFindNode(params: [string]) {}
+
+  /**
+   * Send a FINDNODE request to a peer, to search within the given set of distances
+   * @param params An array of two parameters
+   * 1. ENR string
+   * 2. Array of distances
+   */
   async discv5_sendFindNode(params: [string, number[]]) {}
+
+  /**
+   * Respond to a specific FINDNODE request with a NODES response.
+   * @param params An array of three parameters
+   * 1. ENR string
+   * 2. Array of Nodes
+   * 3. RequestId
+   */
   async discv5_sendNodes(params: [string, string[], number]) {}
+
+  /**
+   * Send a TALKREQ request with a payload to the given peer.
+   * @param params An array of three parameters
+   * 1. ENR string
+   * 2. ProtocolId
+   * 3. Discv5Payload
+   */
   async discv5_sendTalkRequest(params: [string, number, string]) {}
+
+  /**
+   * Respond to a TALKREQ request by sending a TALKRESP response.
+   * @param params An array of parameters
+   * 1. ENR string
+   * 2. Discv5Payload
+   * 3. RequestId
+   */
   async discv5_sendTalkResponse(params: [string, string, number]) {}
+
+  /**
+   * Send a TALKREQ request with a payload to a given peer and wait for response.
+   * @param params An array of three parameters
+   * 1. ENR string
+   * 2. ENR string
+   * 3. Discv5Payload
+   */
   async discv5_talkReq(params: [string, string, string]) {}
-}
 }
