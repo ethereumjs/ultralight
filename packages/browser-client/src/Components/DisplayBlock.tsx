b5c8733f2ece143379d15c22aabc46bab17bf2be
ScottyPoi <scott.simpson@ethereum.org>
2022-09-23 20:59:39 -0600

    Edit BlockHeader display for ethers interface

--- packages/browser-client/src/Components/DisplayBlock.tsx
+++ packages/browser-client/src/Components/DisplayBlock.tsx
@@ -16,6 +16,7 @@ import {
   Text,
   VStack,
 } from '@chakra-ui/react'
+import { BlockWithTransactions } from '@ethersproject/abstract-provider'
 import {
   fromHexString,
   HistoryNetworkContentKeyUnionType,
@@ -46,48 +47,55 @@ const DisplayBlock = () => {
     }
     dispatch({ type: StateChange.TOGGLELOADING })
   }
-  function GridRow(props: any) {
+  interface IGridRow {
+    k: string
+    v: any
+    idx: number
+  }
+  function GridRow(props: IGridRow) {
+    const { k, v, idx } = props
     return (
       <>
         <GridItem fontSize={'xs'} fontWeight="bold" colSpan={3}>
-          {props.k[0]}
+          {k}
         </GridItem>
         <GridItem paddingBottom={3} fontSize={'xs'} wordBreak={'break-all'} colSpan={6}>
-          {props.k[0] === 'parentHash' ? (
+          {k === 'parentHash' ? (
             <Link color={'blue'} onClick={async () => await findParent()}>
-              {props.k[1]}
+              {v}
             </Link>
           ) : (
-            <>{props.k[1]}</>
+            <>{typeof v === 'string' ? v : typeof v}</>
           )}
         </GridItem>
         <GridItem colSpan={1}> </GridItem>
       </>
     )
   }
-  const block = state.block!
-  const header = Object.entries(block)
-    .map((entry) => {
-      let val
-      switch (typeof entry[1]) {
-        case 'string':
-          val = entry[1]
-          break
-        case 'object':
-          val = entry[1].toString()
-          break
-        case 'number':
-          val = entry[1].toString()
-          break
-      }
-      return [entry[0], val]
-    })
-    .filter((entry) => entry[0].slice(0, 1) !== '_')
-
-  async function init() {
+  // const block = state.block!
+  // const header = (block: Block | BlockWithTransactions) => {
+  //   Object.entries(block)
+  //     .map((entry) => {
+  //       let val
+  //       switch (typeof entry[1]) {
+  //         case 'string':
+  //           val = entry[1]
+  //           break
+  //         case 'object':
+  //           val = entry[1].toString()
+  //           break
+  //         case 'number':
+  //           val = entry[1].toString()
+  //           break
+  //       }
+  //       return [entry[0], val]
+  //     })
+  //     .filter((entry) => entry[0].slice(0, 1) !== '_')
+  // }
+  async function init(hash: string) {
     try {
       const receipts = await state.provider!.historyProtocol?.receiptManager.getReceipts(
-        Buffer.from(block.hash.slice(2), 'hex')
+        Buffer.from(hash.slice(2), 'hex')
       )
       if (receipts) {
         setReceipts(receipts)
@@ -98,108 +106,114 @@ const DisplayBlock = () => {
   }
 
   useEffect(() => {
-    if (state.block!.transactions.length > 0) {
-      init()
+    if (state.block && state.block.transactions.length > 0) {
+      init(state.block.hash)
     }
   }, [state.block])
 
-  const headerlookupKey = toHexString(
-    HistoryNetworkContentKeyUnionType.serialize({
-      selector: 0,
-      value: {
-        chainId: 1,
-        blockHash: fromHexString(block.hash),
-      },
-    })
-  )
+  const headerlookupKey = (hash: string) => {
+    return toHexString(
+      HistoryNetworkContentKeyUnionType.serialize({
+        selector: 0,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(hash),
+        },
+      })
+    )
+  }
 
-  const bodylookupKey = toHexString(
-    HistoryNetworkContentKeyUnionType.serialize({
-      selector: 1,
-      value: {
-        chainId: 1,
-        blockHash: fromHexString(block.hash),
-      },
-    })
-  )
-
+  const bodylookupKey = (hash: string) => {
+    return toHexString(
+      HistoryNetworkContentKeyUnionType.serialize({
+        selector: 1,
+        value: {
+          chainId: 1,
+          blockHash: fromHexString(hash),
+        },
+      })
+    )
+  }
   return (
     <Box>
-      <Heading paddingBottom={4} size="sm" textAlign={'center'}>
-        <HStack justifyContent={'center'}>
-          <span>Block #</span>
-          <Skeleton isLoaded={!state.isLoading}>{block.number}</Skeleton>
-          {validated && <CheckCircleIcon />}
-        </HStack>
-      </Heading>
-      <Grid templateColumns={'repeat(16, 1fr'} columnGap={1}>
-        <GridItem colSpan={4}>
-          <Text fontSize="xs" textAlign={'start'}>
-            <span style={{ fontWeight: 'bold' }}>Header Key: </span>
-          </Text>
-        </GridItem>
-        <GridItem colStart={5} colSpan={1}>
-          <CopyIcon
-            marginEnd={2}
-            cursor="pointer"
-            onClick={() => navigator.clipboard.writeText(headerlookupKey)}
-          />
-        </GridItem>
-        <GridItem wordBreak={'break-all'} colSpan={10} colStart={6}>
-          <Skeleton isLoaded={!state.isLoading}>
-            <Text wordBreak={'break-all'} fontSize="xs" textAlign={'start'}>
-              {headerlookupKey}
-            </Text>
-          </Skeleton>
-        </GridItem>
-        <GridItem colSpan={4}>
-          <Text fontSize="xs" textAlign={'start'}>
-            <span style={{ fontWeight: 'bold' }}>Body Key: </span>
-          </Text>
-        </GridItem>
-        <GridItem colStart={5} colSpan={1}>
-          <CopyIcon
-            marginEnd={2}
-            cursor="pointer"
-            onClick={() => navigator.clipboard.writeText(bodylookupKey)}
-          />
-        </GridItem>
-        <GridItem wordBreak={'break-all'} colSpan={10} colStart={6}>
-          <Skeleton isLoaded={!state.isLoading}>
-            <Text wordBreak={'break-all'} fontSize="xs" textAlign={'start'}>
-              {bodylookupKey}
-            </Text>
-          </Skeleton>
-        </GridItem>
-      </Grid>
-      <Tabs>
-        <Center>
-          <TabList>
-            <Tab>Header</Tab>
-            <Tab>Transactions</Tab>
-            <Tab>JSON</Tab>
-          </TabList>
-        </Center>
-        <TabPanels>
-          <TabPanel>
-            <VStack>
-              {validated || <GetHeaderProofByHash />}
-              <Grid templateColumns={'repeat(10, 1fr)'}>
-                {header &&
-                  header.map((key, idx) => {
-                    return <GridRow key={idx} k={key} idx={idx} />
-                  })}
-              </Grid>
-            </VStack>
-          </TabPanel>
-          <TabPanel>{state.block!.transactions.length > 0 && <SelectTx />}</TabPanel>
-          <TabPanel>
-            <Skeleton isLoaded={!state.isLoading}>
-              <Text wordBreak={'break-all'}>{JSON.stringify(block)}</Text>
-            </Skeleton>
-          </TabPanel>
-        </TabPanels>
-      </Tabs>
+      {state.block && (
+        <>
+          <Heading paddingBottom={4} size="sm" textAlign={'center'}>
+            <HStack justifyContent={'center'}>
+              <span>Block #</span>
+              <Skeleton isLoaded={!state.isLoading}>{state.block.number}</Skeleton>
+              {validated && <CheckCircleIcon />}
+            </HStack>
+          </Heading>
+          <Grid templateColumns={'repeat(16, 1fr'} columnGap={1}>
+            <GridItem colSpan={4}>
+              <Text fontSize="xs" textAlign={'start'}>
+                <span style={{ fontWeight: 'bold' }}>Header Key: </span>
+              </Text>
+            </GridItem>
+            <GridItem colStart={5} colSpan={1}>
+              <CopyIcon
+                marginEnd={2}
+                cursor="pointer"
+                onClick={() => navigator.clipboard.writeText(headerlookupKey(state.block!.hash))}
+              />
+            </GridItem>
+            <GridItem wordBreak={'break-all'} colSpan={10} colStart={6}>
+              <Skeleton isLoaded={!state.isLoading}>
+                <Text wordBreak={'break-all'} fontSize="xs" textAlign={'start'}>
+                  {headerlookupKey(state.block.hash)}
+                </Text>
+              </Skeleton>
+            </GridItem>
+            <GridItem colSpan={4}>
+              <Text fontSize="xs" textAlign={'start'}>
+                <span style={{ fontWeight: 'bold' }}>Body Key: </span>
+              </Text>
+            </GridItem>
+            <GridItem colStart={5} colSpan={1}>
+              <CopyIcon
+                marginEnd={2}
+                cursor="pointer"
+                onClick={() => navigator.clipboard.writeText(bodylookupKey(state.block!.hash))}
+              />
+            </GridItem>
+            <GridItem wordBreak={'break-all'} colSpan={10} colStart={6}>
+              <Skeleton isLoaded={!state.isLoading}>
+                <Text wordBreak={'break-all'} fontSize="xs" textAlign={'start'}>
+                  {bodylookupKey(state.block.hash)}
+                </Text>
+              </Skeleton>
+            </GridItem>
+          </Grid>
+          <Tabs>
+            <Center>
+              <TabList>
+                <Tab>Header</Tab>
+                <Tab>Transactions</Tab>
+                <Tab>JSON</Tab>
+              </TabList>
+            </Center>
+            <TabPanels>
+              <TabPanel>
+                <VStack>
+                  {validated || <GetHeaderProofByHash />}
+                  <Grid templateColumns={'repeat(10, 1fr)'}>
+                    {Object.entries(state.block).map(([key, value], idx) => {
+                      return <GridRow key={idx} k={key} v={value} idx={idx} />
+                    })}
+                  </Grid>
+                </VStack>
+              </TabPanel>
+              <TabPanel>{state.block!.transactions.length > 0 && <SelectTx />}</TabPanel>
+              <TabPanel>
+                <Skeleton isLoaded={!state.isLoading}>
+                  <Text wordBreak={'break-all'}>{JSON.stringify(state.block)}</Text>
+                </Skeleton>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </>
+      )}
     </Box>
   )
 }
