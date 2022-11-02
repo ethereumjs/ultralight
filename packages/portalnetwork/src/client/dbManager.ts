ad29c7b9df321f7b2eadfc74d657ac3f1e9804fa
ScottyPoi <scott.simpson@ethereum.org>
2022-11-02 17:08:38 -0600

    DBManager: refactor get / set / delete for sublevels

--- packages/portalnetwork/src/client/dbManager.ts
+++ packages/portalnetwork/src/client/dbManager.ts
@@ -1,9 +1,11 @@
 import { NodeId, distance } from '@chainsafe/discv5'
 import { fromHexString } from '@chainsafe/ssz'
+import { bigIntToHex, bufferToBigInt } from '@ethereumjs/util'
 import { AbstractBatchOperation, AbstractLevel } from 'abstract-level'
 import { Debugger } from 'debug'
 import { MemoryLevel } from 'memory-level'
 import { ProtocolId } from '../subprotocols/index.js'
+import { serializedContentKeyToContentId } from '../util/util.js'
 
 export class DBManager {
   nodeId: string
@@ -33,33 +35,15 @@ export class DBManager {
   }
 
   get(key: string) {
-    let db = this.db
-    switch (key.slice(0, 4)) {
-      case '0x00':
-      case '0x01':
-      case '0x02':
-      case '0x03':
-      case '0x04':
-      case '0x05': {
-        db = this.sublevels.get(ProtocolId.HistoryNetwork)!
-      }
-    }
-    return db.get(key)
+    const db = this.sublevel(key)
+    const databaseKey = this.databaseKey(key)
+    return db.get(databaseKey)
   }
 
   put(key: string, val: string) {
-    let db = this.db
-    switch (key.slice(0, 4)) {
-      case '0x00':
-      case '0x01':
-      case '0x02':
-      case '0x03':
-      case '0x04':
-      case '0x05': {
-        db = this.sublevels.get(ProtocolId.HistoryNetwork)!
-      }
-    }
-    return db.put(key, val, (err: any) => {
+    const db = this.sublevel(key)
+    const databaseKey = this.databaseKey(key)
+    return db.put(databaseKey, val, (err: any) => {
       if (err) this.logger(`Error putting content in history DB: ${err.toString()}`)
     })
   }
@@ -70,6 +54,17 @@ export class DBManager {
   }
 
   del(key: string) {
+    const db = this.sublevel(key)
+    const databaseKey = this.databaseKey(key)
+    return db.del(databaseKey)
+  }
+  databaseKey(key: string) {
+    const contentId = serializedContentKeyToContentId(fromHexString(key))
+    const d = BigInt.asUintN(32, distance(contentId.slice(2), this.nodeId))
+    return bigIntToHex(d)
+  }
+
+  sublevel(key: string) {
     let db = this.db
     switch (key.slice(0, 4)) {
       case '0x00':
@@ -81,6 +76,8 @@ export class DBManager {
         db = this.sublevels.get(ProtocolId.HistoryNetwork)!
       }
     }
+    return db
+  }
     return db.del(key)
   }
 
