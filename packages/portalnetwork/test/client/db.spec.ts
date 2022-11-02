84f11db7aa29fc81cce35c7cbde2c811f4bf4505
ScottyPoi <scott.simpson@ethereum.org>
2022-11-02 17:11:08 -0600

    add test for db functions

---
+++ packages/portalnetwork/test/client/db.spec.ts
@@ -0,0 +1,108 @@
+import tape from 'tape'
+import {
+  getHistoryNetworkContentId,
+  getHistoryNetworkContentKey,
+  HistoryNetworkContentTypes,
+  ProtocolId,
+  toHexString,
+  DBManager,
+  fromHexString,
+  serializedContentKeyToContentId,
+} from '../../src/index.js'
+import debug from 'debug'
+import { randomBytes } from 'ethers/lib/utils.js'
+import { bigIntToHex } from '@ethereumjs/util'
+import { distance } from '@chainsafe/discv5'
+
+tape('DBManager unit tests', async (t) => {
+  const size = async () => {
+    return 500
+  }
+  const _self = randomBytes(32)
+  const self = toHexString(_self)
+  const log = debug('db test')
+  const db = new DBManager(self.slice(2), log, size, [ProtocolId.HistoryNetwork])
+  await db.open()
+  const sublevel = db.sublevels.get(ProtocolId.HistoryNetwork)!
+  const state = db.sublevels.get(ProtocolId.StateNetwork)
+  t.equal(db.db.status, 'open', 'Main database is open')
+  t.equal(sublevel.status, 'open', 'History Sublevel open')
+  t.equal(state, undefined, 'Unsupported Network not open')
+  t.equal(db.nodeId, self, 'db nodeId set correctly')
+
+  const testHash = randomBytes(32)
+  const testVal = randomBytes(48)
+  const testKey = getHistoryNetworkContentKey(HistoryNetworkContentTypes.BlockHeader, testHash)
+  const testId = getHistoryNetworkContentId(
+    HistoryNetworkContentTypes.BlockHeader,
+    toHexString(testHash)
+  )
+  const _testId = serializedContentKeyToContentId(fromHexString(testKey))
+  t.equal(_testId, testId, 'testIds match')
+
+  db.put(testKey, toHexString(testVal))
+  const lookupD = BigInt.asUintN(32, distance(self.slice(2), testId.slice(2)))
+  const lookupKey = bigIntToHex(lookupD)
+  const historyPrefix = '!0x500b!'
+  const key0 = [...(await db.db.keys().all())][0]
+  t.equal(
+    historyPrefix + lookupKey,
+    key0,
+    'DBManager prefixed HistoryNetwork key with HistoryNetwork prefix'
+  )
+  t.equal(
+    BigInt.asUintN(32, BigInt(self)) ^ BigInt.asUintN(32, BigInt(testId)),
+    BigInt.asUintN(32, lookupD),
+    'XOR formula works'
+  )
+  t.equal(
+    BigInt.asUintN(32, lookupD) ^ BigInt.asUintN(32, BigInt(testId)),
+    BigInt.asUintN(32, BigInt(self)),
+    'XOR formula works'
+  )
+  t.equal(
+    BigInt.asUintN(32, BigInt(self)) ^ BigInt.asUintN(32, lookupD),
+    BigInt.asUintN(32, BigInt(testId)),
+    'XOR formula works'
+  )
+  const val = await sublevel.get(lookupKey)
+  t.equal(
+    val,
+    toHexString(testVal),
+    'HistoryNetwork content retrieved directly from history sublevel'
+  )
+  const res = await db.get(testKey)
+  t.equal(res, toHexString(testVal), 'DBManager retrieved History Content from a content key')
+
+  for (let i = 0; i < 10000; i++) {
+    let testKey = toHexString(randomBytes(33))
+    while (parseInt(testKey[3]) < 6) {
+      testKey = toHexString(randomBytes(33))
+    }
+    const testVal = randomBytes(48)
+    db.put(testKey, toHexString(testVal))
+  }
+  for (let i = 0; i < 9999; i++) {
+    const testHash = randomBytes(32)
+    const testKey = getHistoryNetworkContentKey(HistoryNetworkContentTypes.BlockHeader, testHash)
+    const testVal = randomBytes(48)
+    db.put(testKey, toHexString(testVal))
+  }
+
+  t.equal((await db.db.keys().all()).length, 20000, '20000 total keys')
+  t.equal((await sublevel.keys().all())?.length, 10000, '10000 history sublevel keys')
+
+  const oldRadius = 2n ** 256n
+  const newRadius = oldRadius / 2n
+  for await (const key of sublevel!.keys({ gte: bigIntToHex(newRadius) })) {
+    sublevel!.del(key)
+  }
+  const rest = (await sublevel?.keys().all())!.length
+  t.ok(
+    rest < 10000,
+    `pruning by 50% removed ${((10000 - rest) * 100) / 10000}% of sublevel content`
+  )
+  t.equals((await db.db.keys().all()).length, 10000 + rest, 'Keys deleted only from sublevel')
+  db.close()
+  t.end()
+})
