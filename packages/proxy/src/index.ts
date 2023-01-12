485e19bd2f80e3ca37dee9ba98766b13be6e994c
ScottyPoi <scott.simpson@ethereum.org>
2023-01-11 17:13:10 -0700

    update Proxy to use selected port number

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -64,65 +64,67 @@ if (args.metrics) {
   metricsServer.listen(5051)
 }
 
-const startServer = async (ws: WS.Server, externalIp: string, wssPort = 5050, udpPort = 9009) => {
+const startServer = async (ws: WS.Server, externalIp: string, wssPort = 5050) => {
   log(`websocket server listening on ${args.ip ?? '127.0.0.1'}:${wssPort}`)
   ws.on('connection', async (websocket, req) => {
-    if (args.persistentPort && ws.clients.size > 1) {
-      log(`Rejecting additional client connection`)
-      websocket.close()
-      return
-    }
-    const udpsocket = dgram.createSocket({
-      recvBufferSize: 16 * MAX_PACKET_SIZE,
-      sendBufferSize: MAX_PACKET_SIZE,
-      type: 'udp4',
-    })
-    udpsocket.on('message', (data, rinfo) => {
-      log('incoming message from', rinfo.address, rinfo.port)
-      const connInfo = Uint8Array.from(Buffer.from(JSON.stringify(rinfo)))
-      const connLength = Buffer.from(connInfo.length.toString())
-      const msg = new Uint8Array([...connLength, ...connInfo, ...Uint8Array.from(data)])
-      websocket.send(msg)
-    })
-    log(`incoming connection from ${req.socket.remoteAddress}:${req.socket.remotePort}`)
-    let foundPort = false
-    while (!foundPort) {
-      try {
-        await udpsocket.bind(udpPort).once('error', () => {
-          udpsocket.bind(udpPort + Math.ceil(Math.random() * 990))
+    websocket.addListener('message', async (data) => {
+      if (data.toString().startsWith('port:')) {
+        const port = parseInt(data.toString().slice(5))
+        console.log('port: ' + port)
+        if (args.persistentPort && ws.clients.size > 1) {
+          log(`Rejecting additional client connection`)
+          websocket.close()
+          return
+        }
+        const udpsocket = dgram.createSocket({
+          recvBufferSize: 16 * MAX_PACKET_SIZE,
+          sendBufferSize: MAX_PACKET_SIZE,
+          type: 'udp4',
         })
-        foundPort = true
-      } catch (err) {
-        log(err)
+        udpsocket.on('message', (data, rinfo) => {
+          log('incoming message from', rinfo.address, rinfo.port)
+          const connInfo = Uint8Array.from(Buffer.from(JSON.stringify(rinfo)))
+          const connLength = Buffer.from(connInfo.length.toString())
+          const msg = new Uint8Array([...connLength, ...connInfo, ...Uint8Array.from(data)])
+          websocket.send(msg)
+        })
+        log(`incoming connection from ${req.socket.remoteAddress}:${req.socket.remotePort}`)
+        await udpsocket.bind(port).once('error', (e) => {
+          return
+        })
+        // Send external IP address/port to websocket client to update ENR
+        const remoteAddrArray = externalIp.split('.')
+        const bAddress = Uint8Array.from([
+          parseInt(remoteAddrArray[0]),
+          parseInt(remoteAddrArray[1]),
+          parseInt(remoteAddrArray[2]),
+          parseInt(remoteAddrArray[3]),
+        ])
+        const bPort = Buffer.alloc(2)
+        bPort.writeUIntBE(port, 0, 2)
+        websocket.send(Buffer.concat([bAddress, bPort]))
+        log('UDP proxy listening on ', externalIp, udpsocket.address().port)
+        websocket.on('message', (data) => {
+          if (data.toString().startsWith('port:')) {
+            return
+          }
+          console.log('MESSAGE: ' + data)
+          try {
+            const bAddress = Buffer.from(data.slice(0, 4) as ArrayBuffer)
+            const address = `${bAddress[0]}.${bAddress[1]}.${bAddress[2]}.${bAddress[3]}`
+            const port = Buffer.from(data as ArrayBuffer).readUIntBE(4, 2)
+            const payload = Buffer.from(data.slice(6) as ArrayBuffer)
+            log('outbound message to', address, port)
+            udpsocket.send(payload, port, address)
+          } catch (err) {
+            log(err)
+          }
+        })
+        websocket.on('close', () => {
+          log('socket closed', req.socket.remotePort)
+          udpsocket.close()
+        })
       }
-    }
-    // Send external IP address/port to websocket client to update ENR
-    const remoteAddrArray = externalIp.split('.')
-    const bAddress = Uint8Array.from([
-      parseInt(remoteAddrArray[0]),
-      parseInt(remoteAddrArray[1]),
-      parseInt(remoteAddrArray[2]),
-      parseInt(remoteAddrArray[3]),
-    ])
-    const bPort = Buffer.alloc(2)
-    bPort.writeUIntBE(udpsocket.address().port, 0, 2)
-    websocket.send(Buffer.concat([bAddress, bPort]))
-    log('UDP proxy listening on ', externalIp, udpsocket.address().port)
-    websocket.on('message', (data) => {
-      try {
-        const bAddress = Buffer.from(data.slice(0, 4) as ArrayBuffer)
-        const address = `${bAddress[0]}.${bAddress[1]}.${bAddress[2]}.${bAddress[3]}`
-        const port = Buffer.from(data as ArrayBuffer).readUIntBE(4, 2)
-        const payload = Buffer.from(data.slice(6) as ArrayBuffer)
-        log('outbound message to', address, port)
-        udpsocket.send(payload, port, address)
-      } catch (err) {
-        log(err)
-      }
-    })
-    websocket.on('close', () => {
-      log('socket closed', req.socket.remotePort)
-      udpsocket.close()
     })
   })
 }
@@ -150,7 +152,7 @@ const main = (externalIp: string) => {
         port: wssPort,
         clientTracking: true,
       })
-      startServer(ws, externalIp, wssPort, wssPort + 1000)
+      startServer(ws, externalIp, wssPort)
       servers.push(ws)
     })
   } else {
