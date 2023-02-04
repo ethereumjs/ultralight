804a5a28c1eb4e3ed95084a1d974583bfa84eb63
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-02-03 20:39:48 -0500

    Cleanup deps and webpack config

--- packages/browser-client/webpack.config.js
+++ packages/browser-client/webpack.config.js
@@ -1,63 +1,70 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require('webpack');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
 module.exports = {
-    entry: "./src/index.tsx",
-    output: { path: path.join(__dirname, "dist"), filename: "index.bundle.js" },
-    mode: process.env.NODE_ENV || "development",
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"],
-        fallback: {
-            "dgram": false,
-            "stream": require.resolve('stream-browserify'),
-            "crypto": require.resolve('crypto-browserify'),
-            "buffer": require.resolve('buffer/'),
-            "assert": require.resolve('assert/'),
-            "util": require.resolve('util/'),
-            "fs/promises": false, 
-            "fs": false,
+  entry: './src/index.tsx',
+  output: { path: path.join(__dirname, 'dist'), filename: 'index.bundle.js' },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    fallback: {
+      dgram: false,
+      stream: require.resolve('stream-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+      assert: require.resolve('assert/'),
+      util: require.resolve('util/'),
+      'fs/promises': false,
+      fs: false,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        resolve: {
+          fullySpecified: false,
         },
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx|js|jsx)$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: "babel-loader",
-                    options: {
-                        cacheDirectory: true,
-                        plugins: ['@babel/plugin-transform-runtime']
-                    }
-                }
-                ],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              cacheDirectory: true,
+              plugins: ['@babel/plugin-transform-runtime'],
             },
-            {
-                test: /\.(css|scss)$/,
-                use: ["style-loader", "css-loader"],
-            },
-            {
-                test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-                use: ["file-loader"],
-            },
-            {
-                test: /\.js$/,
-                enforce: 'pre',
-                use: ['source-map-loader']
-            }
+          },
         ],
-    },
-    ignoreWarnings: [/Failed to parse source map/],
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.join(__dirname, "public", "index.html"),
-        }),
-        new webpack.ProvidePlugin({
-            Buffer: ['buffer', 'Buffer'],
-        }),
-        new webpack.ProvidePlugin({
-            process: 'process/browser',
-        }),
-        new webpack.EnvironmentPlugin({ BINDADDRESS : '' , NODE_BACKEND : 'js'})
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader'],
+      },
+      {
+        test: /\.js$/,
+        enforce: 'pre',
+        use: ['source-map-loader'],
+      },
     ],
-};
+  },
+  ignoreWarnings: [/Failed to parse source map/],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+    }),
+    new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
+    }),
+    new webpack.ProvidePlugin({
+      process: 'process/browser',
+    }),
+    new webpack.EnvironmentPlugin({ BINDADDRESS: '', NODE_BACKEND: 'js' }),
+  ],
+}
