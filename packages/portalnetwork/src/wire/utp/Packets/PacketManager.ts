2469aa2e6e054f8cdbd61558a86c1ce338996d4c
ScottyPoi <scott.simpson@ethereum.org>
2023-02-09 02:26:54 -0700

    uTP: create PacketManager class

---
+++ packages/portalnetwork/src/wire/utp/Packets/PacketManager.ts
@@ -0,0 +1,47 @@
+import {
+  Bytes32TimeStamp,
+  HeaderExtension,
+  HeaderInput,
+  ICreate,
+  Packet,
+  PacketOptions,
+  PacketType,
+  CongestionControl,
+} from '../index.js'
+import { Debugger } from 'debug'
+
+export class PacketManager {
+  logger: Debugger
+  rcvConnectionId: number
+  sndConnectionId: number
+  congestionControl: CongestionControl
+  updateWindow: () => void
+  constructor(rcvConnectionId: number, sndConnectionId: number, logger: Debugger) {
+    this.rcvConnectionId = rcvConnectionId
+    this.sndConnectionId = sndConnectionId
+    this.logger = logger.extend('PacketManager')
+    this.congestionControl = new CongestionControl()
+    this.updateWindow = () => this.congestionControl.updateWindow()
+  }
+  createPacket<T extends PacketType>(opts: ICreate<T>): Packet<T> {
+    if (opts.extension === HeaderExtension.selectiveAck && !opts.bitmask) {
+      throw new Error('Selective acks must have a bitmask')
+    }
+    const header: HeaderInput<T> = {
+      ...opts,
+      version: 1,
+      timestampMicroseconds: Bytes32TimeStamp(),
+      connectionId: this.rcvConnectionId,
+      timestampDifferenceMicroseconds: this.congestionControl.reply_micro,
+      wndSize: Math.min(
+        2 ** 32 - 1,
+        this.congestionControl.max_window - this.congestionControl.cur_window
+      ),
+    }
+    const options: PacketOptions<T> = {
+      header,
+      payload: opts.payload,
+    }
+    return Packet.fromOpts(options)
+  }
+}
