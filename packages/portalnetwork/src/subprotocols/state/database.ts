89bc08d032abb00edbe1e7151ed46b1b256d444f
ScottyPoi <scott.simpson@ethereum.org>
2023-03-30 22:30:58 -0600

    Implement TrieLevel DB

--- packages/portalnetwork/src/subprotocols/state/database.ts
+++ packages/portalnetwork/src/subprotocols/state/database.ts
@@ -13,3 +13,53 @@ export type Addr = string
 export type AddressRoots = Map<Addr, StateRootHex[]>
 export type AccountRecord = { address: Addr; accounts: Record<StateRootHex, Account> }
 export type AddressRecord = [Addr, AccountRecord]
+export class TrieLevel implements DB {
+  db: TrieDB
+
+  constructor(database?: TrieDB) {
+    this.db = database ?? new MemoryLevel({ createIfMissing: true })
+  }
+  async open(_callback?: NodeCallback<void>): Promise<void> {
+    this.db.open()
+  }
+  async close(_callback?: NodeCallback<void>): Promise<void> {
+    this.db.close()
+  }
+  get status(): TrieLevelStatus {
+    return this.db.status
+  }
+  async get(key: Buffer): Promise<Buffer | null> {
+    try {
+      return Buffer.from(fromHexString(await this.db.get(toHexString(key))))
+    } catch {
+      return null
+    }
+  }
+  async put(key: Buffer, val: Buffer): Promise<void> {
+    await this.db.put(toHexString(key), toHexString(val))
+  }
+  async del(key: Buffer): Promise<void> {
+    await this.db.del(toHexString(key))
+  }
+  async batch(opStack: BatchDBOp[]): Promise<void> {
+    for (const op of opStack) {
+      if (op.type === 'del') {
+        await this.del(op.key)
+      }
+
+      if (op.type === 'put') {
+        await this.put(op.key, op.value)
+      }
+    }
+  }
+  async size(): Promise<number> {
+    let size = 0
+    for await (const [k, v] of this.db.iterator()) {
+      size += fromHexString(k).length + fromHexString(v).length
+    }
+    return size
+  }
+  copy(): DB {
+    return new TrieLevel(this.db)
+  }
+}
