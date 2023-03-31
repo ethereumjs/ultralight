6f5714792a16e6eb5fc860363af3460985f6affc
ScottyPoi <scott.simpson@ethereum.org>
2023-03-30 21:48:55 -0600

    function to generate graph data structure

--- packages/portalnetwork/src/subprotocols/state/stateroots.ts
+++ packages/portalnetwork/src/subprotocols/state/stateroots.ts
@@ -6,3 +6,25 @@ export type GraphInput = {
   stateroots: Set<TStateRoot>
   dag: DAG
 } /** input to graph function */
+
+/**
+ *
+ * @param sequences arrays of state roots in known valid order
+ * @returns GraphInput { stateroots: TStateRoot[], dag: DAG }
+ */
+export function graph(sequences: Sequence[]): GraphInput {
+  const stateroots: Set<TStateRoot> = new Set()
+  const dag: DAG = []
+  for (const sequence of sequences) {
+    for (let i = 0; i < sequence.length; i++) {
+      const u = sequence[i]
+      stateroots.add(u)
+      if (i + 1 < sequence.length) {
+        const v = sequence[i + 1]
+        !dag.map((edge) => JSON.stringify(edge)).includes(JSON.stringify([u, v])) &&
+          dag.push([u, v])
+      }
+    }
+  }
+  return { stateroots, dag }
+}
