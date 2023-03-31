3f779db2198f3da30aa1c4fe54863f4ad842fd50
ScottyPoi <scott.simpson@ethereum.org>
2023-03-30 21:51:25 -0600

    implement graph sort methods for PathFinder

--- packages/portalnetwork/src/subprotocols/state/stateroots.ts
+++ packages/portalnetwork/src/subprotocols/state/stateroots.ts
@@ -50,4 +50,57 @@ export class PathFinder {
     this.maxLenth = -1
     this.maxPaths = []
   }
+  newPath(newPath: Sequence) {
+    if (newPath.length > this.maxLenth) {
+      this.maxLenth = newPath.length
+      this.maxPaths = [newPath]
+    } else if (newPath.length === this.maxLenth) {
+      this.maxPaths.push(newPath)
+    }
+    return newPath
+  }
+  async graphSort(
+    current: TStateRoot,
+    visited: Set<TStateRoot>,
+    path: TStateRoot[],
+    pathLength: number,
+    v: TStateRoot
+  ): Promise<void> {
+    const visitedRoot = current + Array.from(visited.values()).sort().join(',')
+    const currentIndex = [...this.stateroots].indexOf(current)
+    if (
+      this.visited.has(visitedRoot) &&
+      this.visited.get(visitedRoot)![currentIndex] >= pathLength
+    ) {
+      return
+    }
+    if (!this.visited.has(visitedRoot)) {
+      this.visited.set(visitedRoot, new Array(this.stateroots.size).fill(-1))
+    }
+    this.visited.get(visitedRoot)![currentIndex] = pathLength
+
+    for (const [u, _v] of this.graph) {
+      if (u === current && !visited.has(_v)) {
+        const newPath = this.newPath([...path, _v])
+        if (visited.size === this.stateroots.size) {
+          return
+        } else {
+          this.graphSort(_v, new Set([...visited, _v]), newPath, newPath.length, v)
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   * @param u source root
+   * @param v sink root
+   * @returns valid sequences from u to v
+   */
+  async findPaths(u: TStateRoot, v: TStateRoot): Promise<Sequence[]> {
+    for (const start of [u, v]) {
+      await this.graphSort(start, new Set([start]), [start], 1, v)
+    }
+    return this.maxPaths
+  }
 }
