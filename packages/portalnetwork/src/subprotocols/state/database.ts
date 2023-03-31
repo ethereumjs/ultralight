b1169b7f6d3981a030e8e3859834bddcbb147212
ScottyPoi <scott.simpson@ethereum.org>
2023-03-30 22:30:27 -0600

    Define StateDB type aliases

--- packages/portalnetwork/src/subprotocols/state/database.ts
+++ packages/portalnetwork/src/subprotocols/state/database.ts
@@ -7,3 +7,9 @@ import { MemoryLevel } from 'memory-level'
 import { StateRootIndex } from './stateroots.js'
 import { AccountTrieProof, AccountTrieProofKey, StateRoot, StateRootHex } from './types.js'
 
+export type TrieDB = MemoryLevel | AbstractLevel<string, string>
+export type TrieLevelStatus = 'opening' | 'open' | 'closing' | 'closed'
+export type Addr = string
+export type AddressRoots = Map<Addr, StateRootHex[]>
+export type AccountRecord = { address: Addr; accounts: Record<StateRootHex, Account> }
+export type AddressRecord = [Addr, AccountRecord]
