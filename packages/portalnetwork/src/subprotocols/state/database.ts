1a1b5b53f8293d3063b161172627948f663d3214
ScottyPoi <scott.simpson@ethereum.org>
2023-03-30 22:32:01 -0600

    Implement state db class

--- packages/portalnetwork/src/subprotocols/state/database.ts
+++ packages/portalnetwork/src/subprotocols/state/database.ts
@@ -63,3 +63,181 @@ export class TrieLevel implements DB {
     return new TrieLevel(this.db)
   }
 }
+
+export class StateDB {
+  sublevels: Map<StateRootHex, TrieLevel>
+  knownAddresses: Set<string>
+  stateRootIndex: StateRootIndex
+  constructor() {
+    this.sublevels = new Map()
+    this.knownAddresses = new Set()
+    this.stateRootIndex = StateRootIndex.from([])
+  }
+
+  async addRoot(stateRoot: StateRoot): Promise<TrieLevel> {
+    const sub = new TrieLevel()
+    this.putSublevel(stateRoot, sub)
+    await sub.open()
+    while (sub.status !== 'open') {
+      await new Promise((resolve) => setTimeout(resolve, 100))
+    }
+    return sub
+  }
+  putSublevel(stateRoot: StateRoot, sublevel: TrieLevel): TrieLevel {
+    this.sublevels.set(toHexString(stateRoot), sublevel)
+    return sublevel
+  }
+  async getSublevel(stateRoot: StateRoot): Promise<TrieLevel> {
+    return this.sublevels.get(toHexString(stateRoot)) ?? this.addRoot(stateRoot)
+  }
+  delSublevel(stateRoot: StateRoot): void {
+    this.sublevels.delete(toHexString(stateRoot))
+  }
+
+  async getStateTrie(stateRoot: StateRoot) {
+    const db = await this.getSublevel(stateRoot)
+    const trie = await Trie.create({ db: new TrieLevel(db.db), root: Buffer.from(stateRoot) })
+    return trie
+  }
+
+  async getAllTries() {
+    const tries: Trie[] = []
+    for (const root of this.sublevels.keys()) {
+      const db = await this.getSublevel(fromHexString(root))
+      const trie = await Trie.create({ db, root: Buffer.from(fromHexString(root)) })
+      tries.push(trie)
+    }
+    return tries
+  }
+
+  private async _getAddrRoots(addr: string): Promise<StateRoot[]> {
+    const tries = await this.getAllTries()
+    const accountPresent = []
+    for (const trie of tries) {
+      const state = new DefaultStateManager({ trie: trie })
+      if (await state.accountExists(Address.fromString(addr))) {
+        accountPresent.push(trie.root())
+      }
+    }
+    return accountPresent
+  }
+
+  async rootsByAddr(): Promise<AddressRoots> {
+    const accountRecords = await Promise.all(
+      [...this.knownAddresses].map(async (addr) => {
+        return [addr, await this._getAddrRoots(addr)]
+      })
+    )
+
+    return Object.fromEntries(accountRecords)
+  }
+
+  async getAccount(stateRoot: StateRoot, address: Address): Promise<Account> {
+    const trie = await this.getStateTrie(stateRoot)
+    const state = new DefaultStateManager({ trie })
+    return state.getAccount(address)
+  }
+
+  async getAccountRecord(address: string): Promise<AccountRecord> {
+    const roots: StateRoot[] = await Promise.all(await this._getAddrRoots(address))
+    const accounts: [StateRootHex, Account][] = await Promise.all(
+      roots.map(async (root) => {
+        return [toHexString(root), await this.getAccount(root, Address.fromString(address))]
+      })
+    )
+    return { address, accounts: Object.fromEntries(accounts) }
+  }
+
+  sortAccountRecord(accountRecord: AccountRecord): AccountRecord {
+    const roots = Object.keys(accountRecord.accounts)
+    const sortedRoots = roots.sort((root1, root2) => {
+      if (accountRecord.accounts[root1].nonce === accountRecord.accounts[root2].nonce) {
+        return Number(accountRecord.accounts[root1].balance - accountRecord.accounts[root2].balance)
+      } else {
+        return Number(accountRecord.accounts[root1].nonce - accountRecord.accounts[root2].nonce)
+      }
+    })
+    return {
+      address: accountRecord.address,
+      accounts: Object.fromEntries(
+        sortedRoots.map((root) => {
+          return [root, accountRecord.accounts[root]]
+        })
+      ),
+    }
+  }
+  async getAllAccountRecords(): Promise<AccountRecord[]> {
+    const rootsByAddr = await this.rootsByAddr()
+    const addrs = Object.keys(rootsByAddr)
+    const accountRecords: AccountRecord[] = await Promise.all(
+      addrs.map(async (addr) => {
+        return await this.getAccountRecord(addr)
+      })
+    )
+    return accountRecords
+  }
+
+  private _setStateRootIndex(index: StateRootIndex) {
+    this.stateRootIndex = index
+  }
+
+  async setStateRootIndex() {
+    const sortedChangesByAccount = Object.fromEntries(
+      Object.entries(await this.getAllAccountRecords()).map(([addr, accountRecord]) => {
+        return [addr, this.sortAccountRecord(accountRecord)]
+      })
+    )
+    const sortedRootsByAccount = Object.fromEntries(
+      Object.entries(sortedChangesByAccount).map(([addr, account]) => {
+        return [addr, Object.keys(account.accounts)]
+      })
+    )
+    this._setStateRootIndex(StateRootIndex.from(Object.values(sortedRootsByAccount)))
+  }
+
+  async getStateRootOrder() {
+    return this.stateRootIndex.allPaths()
+  }
+
+  async getStateRootOrderContaining(roots: Uint8Array[]) {
+    return this.stateRootIndex.pathThru(roots.map((r) => toHexString(r)))
+  }
+
+  async getStateRootOrderForAccount(addr: string) {
+    return this.stateRootIndex.pathThru(Object.keys(await this._getAddrRoots(addr)))
+  }
+  async updateAccount(contentKey: AccountTrieProofKey, content: AccountTrieProof) {
+    const { stateRoot, address } = contentKey
+
+    this.knownAddresses.add(toHexString(address))
+    const trie = await this.getStateTrie(stateRoot)
+    if (
+      !trie.verifyProof(
+        Buffer.from(stateRoot),
+        Buffer.from(address),
+        content.witnesses.map((w) => Buffer.from(w))
+      )
+    ) {
+      throw new Error('Invalid account trie proof')
+    }
+    await trie.fromProof(content.witnesses.map((w) => Buffer.from(w)))
+    this.delSublevel(stateRoot)
+    this.putSublevel(stateRoot, trie.database().db as TrieLevel)
+    const roots = Object.keys(
+      this.sortAccountRecord(await this.getAccountRecord(toHexString(address))).accounts
+    )
+    this.stateRootIndex.update([roots])
+    return trie
+  }
+
+  async size(): Promise<number> {
+    let size = 0
+    for (const sub of this.sublevels.keys()) {
+      size += fromHexString(sub).length
+    }
+    for (const sub of this.sublevels.values()) {
+      size += await sub.size()
+    }
+    return size
+  }
+}
