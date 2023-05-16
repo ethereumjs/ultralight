ba06e350b21599f5b1f423574f8e8ff0be794ce9
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-05-16 15:23:15 -0400

    Add additional scaffolding

--- packages/portalnetwork/src/subprotocols/beaconChain/beacon.ts
+++ packages/portalnetwork/src/subprotocols/beaconChain/beacon.ts
@@ -5,6 +5,14 @@ import { PortalNetwork } from '../../client/client.js'
 import debug from 'debug'
 import { Union } from '@chainsafe/ssz/lib/interface.js'
 import { fromHexString, toHexString } from '@chainsafe/ssz'
+import { shortId } from '../../util/util.js'
+import {
+  ContentMessageType,
+  FindContentMessage,
+  MessageCodes,
+  PortalWireMessageType,
+} from '../../wire/types.js'
+import { BeaconLightClientNetworkContentType, LightClientOptimisticUpdate } from './types.js'
 
 export class BeaconLightClientNetwork extends BaseProtocol {
   protocolId: ProtocolId.BeaconLightClientNetwork
@@ -33,14 +41,61 @@ export class BeaconLightClientNetwork extends BaseProtocol {
     }
   }
 
-  public sendFindContent = (
+  public sendFindContent = async (
     dstId: string,
     key: Uint8Array
   ): Promise<Union<Uint8Array | Uint8Array[]> | undefined> => {
-    return Promise.resolve(undefined)
+    const enr = this.routingTable.getValue(dstId)
+    if (!enr) {
+      this.logger(`No ENR found for ${shortId(dstId)}.  FINDCONTENT aborted.`)
+      return
+    }
+    this.metrics?.findContentMessagesSent.inc()
+    const findContentMsg: FindContentMessage = { contentKey: key }
+    const payload = PortalWireMessageType.serialize({
+      selector: MessageCodes.FINDCONTENT,
+      value: findContentMsg,
+    })
+    const res = await this.sendMessage(enr, Buffer.from(payload), this.protocolId)
+    if (res.length === 0) {
+      return undefined
+    }
+    try {
+      if (parseInt(res.subarray(0, 1).toString('hex')) === MessageCodes.CONTENT) {
+        this.metrics?.contentMessagesReceived.inc()
+        this.logger.extend('FOUNDCONTENT')(`Received from ${shortId(dstId)}`)
+        const decoded = ContentMessageType.deserialize(res.subarray(1))
+        const contentHash = toHexString(key)
+        switch (decoded.selector) {
+          case BeaconLightClientNetworkContentType.LightClientOptimisticUpdate:
+            try {
+              LightClientOptimisticUpdate.deserialize(decoded.value as Uint8Array)
+            } catch (err) {
+              this.logger(`received invalid content from ${shortId(dstId)}`)
+              break
+            }
+            this.logger(
+              `received ${
+                BeaconLightClientNetworkContentType[decoded.selector]
+              } content corresponding to ${contentHash}`
+            )
+            await this.store(decoded.selector, contentHash, decoded.value as Uint8Array)
+            break
+          default:
+            this.logger(
+              `received ${
+                BeaconLightClientNetworkContentType[decoded.selector]
+              } content corresponding to ${contentHash}`
+            )
+        }
+        return decoded
+      }
+    } catch (err: any) {
+      this.logger(`Error sending FINDCONTENT to ${shortId(dstId)} - ${err.message}`)
+    }
   }
 
-  public store = (contentType: any, hashKey: string, value: Uint8Array): Promise<void> => {
-    return Promise.resolve()
+  public store = async (contentType: any, hashKey: string, value: Uint8Array): Promise<void> => {
+    await this.put(this.protocolId, hashKey, toHexString(value))
   }
 }
--- packages/portalnetwork/test/subprotocols/beaconLightClient/beacon.spec.ts
+++ packages/portalnetwork/test/subprotocols/beaconLightClient/beacon.spec.ts
@@ -3,8 +3,14 @@ import { createRequire } from 'module'
 const require = createRequire(import.meta.url)
 import { lightClientOptimisticUpdateFromJson } from '../../../src/subprotocols/beaconChain/helpers.js'
 import { LightClientOptimisticUpdate } from '../../../src/subprotocols/beaconChain/types.js'
+import { fromHexString, getUint8ByteToBitBooleanArray } from '@chainsafe/ssz'
 tape('type tests', (t) => {
   const testdata = require('./testdata.json')
+  const bytes = fromHexString(testdata.optimistic_update.sync_aggregate.sync_committee_bits)
+  const bools = [] as boolean[][]
+  for (const byte of bytes) {
+    bools.push(getUint8ByteToBitBooleanArray(byte))
+  }
 
   const optimisticUpdate = lightClientOptimisticUpdateFromJson(testdata.optimistic_update)
   const serializedUpdate = LightClientOptimisticUpdate.serialize(optimisticUpdate)
