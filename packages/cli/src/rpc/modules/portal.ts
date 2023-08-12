8ddefef68b7670cf63253ff4b3f5ec5944969a34
ScottyPoi <scott.simpson@ethereum.org>
2023-08-12 02:55:19 -0600

    return empty array for null

--- packages/cli/src/rpc/modules/portal.ts
+++ packages/cli/src/rpc/modules/portal.ts
@@ -383,10 +383,7 @@ export class portal {
     const res = await this._history.sendFindContent(nodeId, fromHexString(contentKey))
     this.logger.extend('findContent')(`request returned type: ${res && res.selector}`)
     if (!res) {
-      return {
-        content: '0x',
-        utpTransfer: false,
-      }
+      return []
     }
     const content: Uint8Array | Uint8Array[] =
       res.selector === 2
