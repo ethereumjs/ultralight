b4576062124cc49978755c4280d9024f22e11101
Luke Schoen <ltfschoen@gmail.com>
2023-08-31 13:53:03 +1000

    use regex to check base64url chars of enr

--- packages/cli/src/util.ts
+++ packages/cli/src/util.ts
@@ -1,6 +1,24 @@
 import { Enr } from './rpc/schema/types.js'
 import { BaseProtocol } from 'portalnetwork'
 
+const hasValidEnrPrefix = (enr: Enr) => {
+  return enr.startsWith('enr:')
+}
+
+// remove ENR prefix 'enr:'
+const extractBase64URLCharsFromEnr = (enr: Enr) => {
+  return enr.substr(4, enr.length)
+}
+
+const hasValidBase64URLChars = (enr: Enr) => {
+  const str = extractBase64URLCharsFromEnr(enr)
+  const validBase64Chars = /^[A-Za-z0-9_-]+$/i
+  if (str && validBase64Chars.test(str)) {
+    return true;
+  }
+  return false;
+}
+
 /**
  * Check if an Ethereum Node Record (ENR) is valid as specified in EIP-778
  * https://eips.ethereum.org/EIPS/eip-778 by starting with `enr:` and encoded
@@ -9,10 +27,7 @@ import { BaseProtocol } from 'portalnetwork'
  * @param enr a base64 encoded string containing an Ethereum Node Record (ENR)
  */
 export const isValidEnr = (enr: Enr) => {
-  if (enr && enr.startsWith('enr:')) {
-    // TODO: Check remaining characters are encoded using valid characters of
-    // the Base64URL encoding scheme
-
+  if (enr && hasValidEnrPrefix(enr) && hasValidBase64URLChars(enr)) {
     return true
   }
   return false
