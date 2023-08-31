9856aad5cd00ce6e0762606ad24b5a345dfcf2b7
Luke Schoen <ltfschoen@gmail.com>
2023-08-31 15:36:36 +1000

    tests for util to validate enr

---
+++ packages/cli/test/util.spec.ts
@@ -0,0 +1,25 @@
+import { describe, it, assert } from 'vitest'
+import { extractBase64URLCharsFromEnr, hasValidBase64URLChars } from '../src/util.js'
+import { Enr } from '../src/rpc/schema/types.js'
+
+describe('portal network cli test utils', () => {
+  let enr: Enr = "enr:-IS4QFV_wTNknw7qiCGAbHf6LxB-xPQCktyrCEZX-b-7PikMOIKkBg-frHRBkfwhI3XaYo_T-HxBYmOOQGNwThkBBHYDgmlkgnY0gmlwhKRc9_OJc2VjcDI1NmsxoQKHPt5CQ0D66ueTtSUqwGjfhscU_LiwS28QvJ0GgJFd-YN1ZHCCE4k"
+  
+  let badEnrPrefix: Enr = "ren:-"
+  it('hasValidEnrPrefix', () => {
+    assert.equal(hasValidEnrPrefix(enr), true, 'detected valid ENR prefix correctly')
+    assert.equal(hasValidEnrPrefix(badEnrPrefix), false, 'detected invalid ENR prefix correctly')
+  })
+  
+  const expectedExtractedBase64URLChars = "-IS4QFV_wTNknw7qiCGAbHf6LxB-xPQCktyrCEZX-b-7PikMOIKkBg-frHRBkfwhI3XaYo_T-HxBYmOOQGNwThkBBHYDgmlkgnY0gmlwhKRc9_OJc2VjcDI1NmsxoQKHPt5CQ0D66ueTtSUqwGjfhscU_LiwS28QvJ0GgJFd-YN1ZHCCE4k"
+  it('extractBase64URLCharsFromEnr', () => {
+    assert.equal(extractBase64URLCharsFromEnr(enr),
+      expectedExtractedBase64URLChars, 'extracted base64URL correctly')
+  })
+
+  let badEnr: Enr = "enr:=+/"
+  it('hasValidBase64URLChars', () => {
+    assert.equal(hasValidBase64URLChars(enr), true, 'detected valid base64URL correctly')
+    assert.equal(hasValidBase64URLChars(badEnr), false, 'detected invalid base64URL correctly')
+  })
+})
