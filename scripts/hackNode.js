a8e5f38105cbc8c244dd7f2109547c4ad947e2f3
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-09-16 13:40:04 -0400

    various cleanup

--- scripts/hackNode.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs')
-const packageJson = require('../node_modules/@chainsafe/discv5/package.json')
-packageJson.exports['./packet'] = { import: './lib/packet/index.js'}
-fs.writeFileSync('node_modules/@chainsafe/discv5/package.json', JSON.stringify(packageJson))
