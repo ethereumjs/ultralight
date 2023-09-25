a7a112fb91ffb00e3f3b45b10f29ab71dfeacfea
ScottyPoi <scott.simpson@ethereum.org>
2023-09-25 01:12:05 -0600

    fix DOM nesting error

--- packages/client/src/Components/Tabs.tsx
+++ packages/client/src/Components/Tabs.tsx
@@ -31,7 +31,7 @@ function TabPanel(props: TabPanelProps) {
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
+          {children}
         </Box>
       )}
     </div>
