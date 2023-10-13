28c0f23d7a8c98e6b87a08447509d86da695875b
ScottyPoi <scott.simpson@ethereum.org>
2023-10-12 21:44:03 -0600

    fix websocket error

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -7,6 +7,9 @@ import ClientTabs from './Components/Tabs'
 export function App() {
   const wsClient = createWSClient({
     url: `ws://localhost:3001`,
+    retryDelayMs(attemptIndex) {
+      return Math.min(1000 * 2 ** attemptIndex, 30_000)
+    },
   })
   const [queryClient] = useState(() => new QueryClient())
   const [trpcClient] = useState(() =>
