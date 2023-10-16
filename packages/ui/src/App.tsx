f14350fafc639c1f51adad0e4a0fdf47150b9cdf
ScottyPoi <scott.simpson@ethereum.org>
2023-10-15 19:06:38 -0600

    fix websocket close error

--- packages/ui/src/App.tsx
+++ packages/ui/src/App.tsx
@@ -5,31 +5,33 @@ import { trpc } from './utils/trpc'
 import ClientTabs from './Components/Tabs'
 
 export function App() {
-  const wsClient = createWSClient({
-    url: `ws://localhost:3001`,
-    retryDelayMs(attemptIndex) {
-      console.log('ws retrying', attemptIndex)
-      return Math.min(1000 * 2 ** attemptIndex, 30_000)
-    },
-    onClose(cause) {
-      clearInterval('update')
-      clearInterval('updated')
-      console.log('ws closed', cause)
-    },
-    onOpen() {
-      console.log('ws opened: ws://localhost:3001')
-    },
-
-  })
-  const queryClient = new QueryClient()
-  const trpcClient = trpc.createClient({
-    links: [
-      wsLink({
-        client: wsClient,
-      }),
-    ],
-  })
-
+  const [wsClient] = useState(
+    createWSClient({
+      url: `ws://localhost:3001`,
+      retryDelayMs(attemptIndex) {
+        console.log('ws retrying', attemptIndex)
+        return Math.min(1000 * 2 ** attemptIndex, 30_000)
+      },
+      onClose(cause) {
+        clearInterval('update')
+        clearInterval('updated')
+        console.log('ws closed', cause)
+      },
+      onOpen() {
+        console.log('ws opened: ws://localhost:3001')
+      },
+    }),
+  )
+  const [queryClient] = useState(new QueryClient())
+  const [trpcClient] = useState(
+    trpc.createClient({
+      links: [
+        wsLink({
+          client: wsClient,
+        }),
+      ],
+    }),
+  )
 
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
