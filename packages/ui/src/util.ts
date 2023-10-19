298b69c8471e0f6792936fbc47a39c6869049648
ScottyPoi <scott.simpson@ethereum.org>
2023-10-18 18:50:21 -0600

    copy utils.ts over to server directory

---
+++ packages/ui/src/util.ts
@@ -0,0 +1,114 @@
+import { BlockHeader } from '@ethereumjs/block'
+import { RLP } from '@ethereumjs/rlp'
+import { TransactionFactory } from '@ethereumjs/tx'
+import {
+  BaseProtocol,
+  BlockBodyContentType,
+  BlockHeaderWithProof,
+  EpochAccumulator,
+  ProtocolId,
+  sszReceiptType,
+  sszUnclesType,
+  toHexString,
+} from 'portalnetwork'
+
+type Enr = string
+
+export const hasValidEnrPrefix = (enr: Enr) => {
+  return enr.startsWith('enr:')
+}
+
+// remove ENR prefix 'enr:'
+export const extractBase64URLCharsFromEnr = (enr: Enr) => {
+  return enr.substr(4, enr.length)
+}
+
+export const hasValidBase64URLChars = (enr: Enr) => {
+  const str = extractBase64URLCharsFromEnr(enr)
+  const validBase64Chars = /^[A-Za-z0-9_-]+$/i
+  if (str && validBase64Chars.test(str)) {
+    return true
+  }
+  return false
+}
+
+/**
+ * Check if an Ethereum Node Record (ENR) is valid as specified in EIP-778
+ * https://eips.ethereum.org/EIPS/eip-778 by starting with `enr:` and encoded
+ * using valid characters of the Base64URL encoding scheme
+ * https://base64.guru/standards/base64url
+ * @param enr a base64 encoded string containing an Ethereum Node Record (ENR)
+ */
+export const isValidEnr = (enr: Enr) => {
+  if (enr && hasValidEnrPrefix(enr) && hasValidBase64URLChars(enr)) {
+    return true
+  }
+  return false
+}
+
+/**
+ * Add a valid Ethereum Node Record (ENR) that is compliant with EIP-778
+ * https://eips.ethereum.org/EIPS/eip-778 as a bootnode to an instance of a Portal Network
+ * protocol
+ * @param protocolId the protocolId associated with an instance of a Portal Network protocol
+ * @param baseProtocol the methods of the protocolId instance of a Portal Network protocol
+ * @param enr a base64 encoded string containing an Ethereum Node Record (ENR)
+ * @throws {Error}
+ */
+export const addBootNode = async (protocolId: ProtocolId, baseProtocol: BaseProtocol, enr: Enr) => {
+  try {
+    await baseProtocol!.addBootNode(enr)
+    baseProtocol!.logger(`Added bootnode ${enr} to ${protocolId}`)
+  } catch (error: any) {
+    throw new Error(`Error adding bootnode ${enr} to protocol \
+      ${protocolId}: ${error.message ?? error}`)
+  }
+}
+
+export const toJSON = (contentKey: Uint8Array, res: Uint8Array) => {
+  const contentType = contentKey[0]
+  let content = {}
+  switch (contentType) {
+    case 0: {
+      const blockHeaderWithProof = BlockHeaderWithProof.deserialize(res)
+      const header = BlockHeader.fromRLPSerializedHeader(blockHeaderWithProof.header, {
+        setHardfork: true,
+      }).toJSON()
+      const proof =
+        blockHeaderWithProof.proof.selector === 0
+          ? []
+          : blockHeaderWithProof.proof.value?.map((p) => toHexString(p))
+      content = { header, proof }
+      break
+    }
+    case 1: {
+      const blockBody = BlockBodyContentType.deserialize(res)
+      const transactions = blockBody.allTransactions.map((tx) =>
+        TransactionFactory.fromSerializedData(tx).toJSON(),
+      )
+      const unclesRlp = toHexString(sszUnclesType.deserialize(blockBody.sszUncles))
+      content = {
+        transactions,
+        uncles: {
+          rlp: unclesRlp,
+          count: RLP.decode(unclesRlp).length.toString(),
+        },
+      }
+      break
+    }
+    case 2: {
+      const receipt = sszReceiptType.deserialize(res)
+      content = receipt
+      break
+    }
+    case 3: {
+      const epochAccumulator = EpochAccumulator.deserialize(res)
+      content = epochAccumulator
+      break
+    }
+    default: {
+      content = {}
+    }
+  }
+  return JSON.stringify(content)
+}
