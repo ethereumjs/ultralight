68255563d586a8b7abc36618660d58c179b75ac6
ScottyPoi <scott.simpson@ethereum.org>
2023-11-01 13:20:09 -0600

    state: stub out eth namespace methods

---
+++ packages/portalnetwork/src/subprotocols/state/eth.ts
@@ -0,0 +1,25 @@
+import { StateProtocol } from './state.js'
+
+export async function eth_getBalance(this: StateProtocol, ...args: any) {
+  return undefined
+}
+
+export async function eth_getStorageAt(this: StateProtocol, ...args: any) {
+  return undefined
+}
+
+export async function eth_getTransactionCount(this: StateProtocol, ...args: any) {
+  return undefined
+}
+
+export async function eth_getCode(this: StateProtocol, ...args: any) {
+  return undefined
+}
+
+export async function eth_call(this: StateProtocol, ...args: any) {
+  return undefined
+}
+
+export async function eth_estimateGas(this: StateProtocol, ...args: any) {
+  return undefined
+}
