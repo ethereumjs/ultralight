5ea0f31640d581a5944a88eb56affbb8baad7189
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2023-11-24 20:29:45 -0500

    Add working rpc test fraomework

--- packages/cli/test/rpc/beacon/getLightClientUpdate.spec.ts
+++ packages/cli/test/rpc/beacon/getLightClientUpdate.spec.ts
@@ -1,34 +1,43 @@
-import { spawn } from 'child_process'
-import jayson, { Client } from 'jayson/promise/index.js'
+import { computeSyncPeriodAtSlot } from '@lodestar/light-client/utils'
+import { ssz } from '@lodestar/types'
+import {
+  BeaconLightClientNetworkContentType,
+  computeLightClientKeyFromPeriod,
+  fromHexString,
+  getBeaconContentKey,
+  toHexString,
+} from 'portalnetwork'
 import { assert, describe, it } from 'vitest'
+
+import { startRpc } from '../util.js'
 const method = 'beacon_getLightClientUpdate'
 describe(`${method} tests`, () => {
   it('should retrieve a light client update', async () => {
-    const ip = '127.0.0.1'
-    const port = 9090
-    const rpcPort = 8545
-    const networks = ['beacon']
-    const child = spawn(
-      process.execPath,
-      [
-        '--loader',
-        'ts-node/esm',
-        '../../src/index.ts',
-        `--rpcAddr=${ip}`,
-        `--rpcPort=${rpcPort}`,
-        `--bindAddress=${ip}:${port}`,
-        ...networks,
-      ],
-      //    { stdio: ['pipe', 'pipe', process.stderr] },
+    const { ultralight, rpc } = await startRpc()
+    const rangeJson = require('./range.json')[0]
+    const rangeKey = getBeaconContentKey(
+      BeaconLightClientNetworkContentType.LightClientUpdate,
+      fromHexString(
+        computeLightClientKeyFromPeriod(
+          computeSyncPeriodAtSlot(Number(rangeJson.data.attested_header.beacon.slot)),
+        ),
+      ),
     )
-    await new Promise((resolve) =>
-      setTimeout(() => {
-        resolve(undefined)
-      }, 10000),
+    const rangeHex = toHexString(
+      ssz.allForksLightClient.capella.LightClientUpdate.serialize(
+        ssz.allForksLightClient.capella.LightClientUpdate.fromJson(rangeJson.data),
+      ),
     )
-    const ultralight = Client.http({ host: ip, port: 8545 })
-    const res = await ultralight.request(`discv5_nodeInfo`, [])
+    await rpc.request('portal_beaconStore', [rangeKey, rangeHex])
+
+    const period =
+      '0x' +
+      computeSyncPeriodAtSlot(Number(BigInt(rangeJson.data.attested_header.beacon.slot))).toString(
+        '16',
+      )
+
+    const res = await rpc.request('beacon_getLightClientUpdate', [period])
     console.log(res)
-    child.kill(9)
-  })
+    ultralight.kill(9)
+  }, 20000)
 })
---
+++ packages/cli/test/rpc/util.ts
@@ -0,0 +1,44 @@
+import { spawn } from 'child_process'
+import { Client } from 'jayson/promise'
+
+export interface cliConfig {
+  ip?: string
+  port?: number
+  rpcPort?: number
+  networks?: string[]
+}
+export const startRpc = async (opts?: cliConfig) => {
+  const ip = opts?.ip ?? '127.0.0.1'
+  const port = opts?.port ?? 9090
+  const rpcPort = opts?.rpcPort ?? 8545
+  const networks = opts?.networks ?? ['beacon']
+  const ultralight = spawn(
+    process.execPath,
+    [
+      '--loader',
+      'ts-node/esm',
+      'src/index.ts',
+      `--rpcAddr=${ip}`,
+      `--rpcPort=${rpcPort}`,
+      `--bindAddress=${ip}:${port}`,
+      `--networks=${networks.join(' ')}`,
+    ],
+    { stdio: ['pipe', 'pipe', 'inherit'] },
+  )
+  const rpc = Client.http({ host: ip, port: 8545 })
+  let done = false
+  while (!done) {
+    try {
+      await rpc.request(`discv5_nodeInfo`, [])
+      done = true
+    } catch {
+      /** Catch connection errors while waiting for node to start */
+    }
+    await new Promise((resolve) =>
+      setTimeout(() => {
+        resolve(undefined)
+      }, 1000),
+    )
+  }
+  return { ultralight, rpc }
+}
