083f547d7bbdc4004939d29a9a3672708d9ab2a2
ScottyPoi <scott.simpson@ethereum.org>
2024-01-30 17:28:42 -0700

    state: update state network types

--- packages/portalnetwork/src/networks/state/types.ts
+++ packages/portalnetwork/src/networks/state/types.ts
@@ -1,18 +1,125 @@
 import {
+  BooleanType,
   ByteListType,
   ByteVectorType,
   ContainerType,
-  ListBasicType,
   ListCompositeType,
   UintBigintType,
-  UintNumberType,
 } from '@chainsafe/ssz'
 
 export enum StateNetworkContentType {
-  AccountTrieProof = 16, // 0x10
-  ContractStorageTrieProof = 17, // 0x11
-  ContractByteCode = 18, // 0x12
+  AccountTrieNode = 16, // 0x20
+  ContractTrieNode = 17, // 0x21
+  ContractByteCode = 18, // 0x22
 }
+
+/* ----------------- Ping.custom_data & Pong.custom_data ----------- */
+/*
+ * The custom_payload field of the Ping and Pong messages
+ * is the serialization of an SSZ Container specified as custom_data:
+ */
+export type TPingPongPayload = { dataRadius: bigint }
+export const PingPongPayload = new ContainerType({ dataRadius: new UintBigintType(32) })
+
+/* ----------------- Paths (Nibbles) ----------- */
+export type TNibble = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'a' | 'b' | 'c' | 'd' | 'e' | 'f'
+export type TNibblePair = [TNibble, TNibble] // 2 nibbles tightly packed into a single byte
+export type TPackedNibbles = Array<TNibblePair>
+export type TNibbles = {
+  isOddLength: boolean
+  packedNibbles: Uint8Array
+}
+export const NibblePair = new UintBigintType(32)
+export const Nibbles = new ContainerType({
+  isOddLength: new BooleanType(),
+  packedNibbles: new ByteListType(32),
+})
+
+/* ----------------- Merkle Patricia Tire (MPT) Proofs ----------- */
+
+export type TTrieNode = Uint8Array
+export const TrieNode = new ByteListType(1024)
+export type TTrieProof = Array<TTrieNode>
+export const TrieProof = new ListCompositeType(TrieNode, 65)
+
+/* ----------------- Account Trie Node ----------- */
+
+export type TAccountTrieNodeKey = {
+  path: TNibbles
+  nodeHash: Bytes32
+}
+export const AccountTrieNodeKey = new ContainerType({
+  path: Nibbles,
+  nodeHash: new ByteVectorType(32),
+})
+export type TAccountTrieNodeOffer = {
+  witnesses: MPTWitnessNode[]
+  blockHash: Bytes32
+}
+export const AccountTrieNodeOffer = new ContainerType({
+  proof: TrieProof,
+  blockHash: new ByteVectorType(32),
+})
+export type TAccountTrieNodeRetrieval = {
+  node: MPTWitnessNode
+}
+export const AccountTrieNodeRetrieval = new ContainerType({
+  node: TrieNode,
+})
+
+/* ----------------- Contract Trie Node --------------------- */
+
+export type TStorageTrieNodeKey = {
+  address: Bytes20
+  path: TNibbles
+  nodeHash: Bytes32
+}
+export const StorageTrieNodeKey = new ContainerType({
+  address: new ByteVectorType(20),
+  path: Nibbles,
+  nodeHash: new ByteVectorType(32),
+})
+export type TStorageTrieNodeOffer = {
+  storageProof: TTrieProof
+  accountProof: TTrieProof
+  blockHash: Bytes32
+}
+export const StorageTrieNodeOffer = new ContainerType({
+  storageProof: TrieProof,
+  accountProof: TrieProof,
+  blockHash: new ByteVectorType(32),
+})
+export type TStorageTrieNodeRetrieval = {
+  node: MPTWitnessNode
+}
+export const StorageTrieNodeRetrieval = new ContainerType({
+  node: TrieNode,
+})
+
+/* ----------------- Contract Code --------------------- */
+
+export type TContractCodeKey = {
+  address: Bytes20
+  codeHash: Bytes32
+}
+export const ContractCodeKey = new ContainerType({
+  address: new ByteVectorType(20),
+  codeHash: new ByteVectorType(32),
+})
+export type TContractOffer = {
+  code: Uint8Array
+  accountProof: TTrieProof
+  blockHash: Bytes32
+}
+export const ContractCodeOffer = new ContainerType({
+  code: new ByteListType(32768),
+  accountProof: TrieProof,
+  blockHash: new ByteVectorType(32),
+})
+export type TContractRetrieval = {
+  code: Uint8Array
+}
+export const ContractRetrieval = new ContainerType({ code: new ByteListType(32768) })
 
 /* ----------------- Types ----------- */
 type Bytes32 = Uint8Array
@@ -22,23 +129,6 @@ export type Address = Bytes20
 export type StateRoot = Bytes32
 export type StateRootHex = string
 export type MPTWitnessNode = Uint8Array
-
-export type TNibble = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 'a' | 'b' | 'c' | 'd' | 'e' | 'f'
-export type TNibblePair = [TNibble, TNibble]
-export type TNibblesArray = Array<TNibblePair>
-
-export const NibblePair = new UintNumberType(1)
-export const Nibbles = new ListBasicType(NibblePair, 8)
-
-export type AccountTrieNodeKey = {
-  path: Uint8Array
-  nodeHash: Bytes32
-}
-
-export type AccountTrieProofKey = {
-  path: Address
-  stateRoot: StateRoot
-}
 export type Slot = Uint256
 export type StorageTrieProofKey = {
   address: Address
@@ -46,83 +136,3 @@ export type StorageTrieProofKey = {
   stateRoot: StateRoot
 }
 export type CodeHash = Bytes32
-
-export type ContractByteCodeKey = {
-  address: Address
-  codeHash: CodeHash
-}
-
-export type AccountTrieProof = {
-  witnesses: MPTWitnessNode[]
-}
-
-export type ContractStorageTrieProof = {
-  witness: MPTWitnessNode[]
-}
-
-export type ContractByteCode = Uint8Array
-
-/* ----------------- SSZ Type Aliases ----------- */
-const Bytes32Type = new ByteVectorType(32)
-export const Bytes20Type = new ByteVectorType(20)
-// MPT Witness nodes are RLP encoded arrays where each element is either empty or a 32 byte hash.
-// The largest node is a branch node that is an array of 17 elements that are each either null, a 32 byte hash, or the value of the node (i.e. an account - 4 32 byte elements)
-export const MPTWitnessNodeType = new ByteListType(667)
-export const MPTWitnessesType = new ListCompositeType(MPTWitnessNodeType, 17) // A list of `MPTWitnessNodes` that can be as long as the MPT can be deep (i.e. 16 layers deep)
-export const AddressType = Bytes20Type
-export const StateRootType = Bytes32Type
-export const SlotType = new UintBigintType(32)
-
-export const NibbleType = new UintNumberType(1)
-export const NibblesType = new ListBasicType(NibbleType, 8)
-
-export const AccountTrieNodeKeyType = new ContainerType({
-  path: Bytes32Type,
-  nodeHash: Bytes32Type,
-})
-
-export type AccountTrieNodeOffer = {
-  witnesses: MPTWitnessNode[]
-}
-export type AccountTrieNodeRequest = {
-  node: MPTWitnessNode
-}
-
-export type AccountTrieNodeContent<T extends 'OFFER' | 'REQUEST'> = T extends 'OFFER'
-  ? AccountTrieNodeOffer
-  : AccountTrieNodeRequest
-
-export const AccountTrieNodeOfferType = new ContainerType({
-  witnesses: MPTWitnessesType,
-})
-export const AccountTrieNodeRequestType = new ContainerType({
-  node: MPTWitnessNodeType,
-})
-export type AccountTrieNodeContentType<T extends 'OFFER' | 'REQUEST'> = T extends 'OFFER'
-  ? typeof AccountTrieNodeOfferType
-  : typeof AccountTrieNodeRequestType
-
-export const AccountTrieProofKeyType = new ContainerType({
-  address: Bytes20Type,
-  stateRoot: StateRootType,
-})
-
-export const AccountTrieProofType = new ContainerType({
-  witnesses: MPTWitnessesType,
-})
-
-export const ContractStorageTrieKeyType = new ContainerType({
-  address: AddressType,
-  slot: SlotType,
-  stateRoot: StateRootType,
-})
-
-export const ContractStorageTrieProofType = new ContainerType({
-  witnesses: MPTWitnessesType,
-})
-
-export const ContractByteCodeKeyType = new ContainerType({
-  address: AddressType,
-  codeHash: Bytes32Type,
-})
-export const ContractByteCodeType = new ByteListType(24576)
