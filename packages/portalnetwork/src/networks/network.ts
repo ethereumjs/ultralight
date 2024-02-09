96f736ab456dbe5a217285e9dc1f7366826b30a2
ScottyPoi <scott.simpson@ethereum.org>
2024-02-08 21:30:29 -0700

    network: make radius attribute public

--- packages/portalnetwork/src/networks/network.ts
+++ packages/portalnetwork/src/networks/network.ts
@@ -45,7 +45,7 @@ import type { Debugger } from 'debug'
 export abstract class BaseNetwork extends EventEmitter {
   public routingTable: PortalNetworkRoutingTable | StateNetworkRoutingTable
   public metrics: PortalNetworkMetrics | undefined
-  private nodeRadius: bigint
+  nodeRadius: bigint
   private checkIndex: number
   abstract logger: Debugger
   abstract networkId: NetworkId
