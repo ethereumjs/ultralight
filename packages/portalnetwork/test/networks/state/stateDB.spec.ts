e610504e22d2bba558397bda857da36a672e15fc
ScottyPoi <scott.simpson@ethereum.org>
2024-02-09 16:35:28 -0700

    state: build trie from stateDB

--- packages/portalnetwork/test/networks/state/stateDB.spec.ts
+++ packages/portalnetwork/test/networks/state/stateDB.spec.ts
@@ -1,16 +1,21 @@
+import { Trie } from '@ethereumjs/trie'
 import { MemoryLevel } from 'memory-level'
 import { describe, expect, it } from 'vitest'
 
 import { PortalTrieDB } from '../../../src/index.js'
 import { StateDB } from '../../../src/networks/state/statedb.js'
 
-describe('StateDB', async () => {
+describe('StateDB components', async () => {
+  const portalTrieDB = new PortalTrieDB(new MemoryLevel() as any)
   it('should create PortalTrieDB from constructor', () => {
-    const portalTrieDB = new PortalTrieDB(new MemoryLevel() as any)
     expect(portalTrieDB).exist
   })
+  const stateDB = new StateDB(new MemoryLevel() as any)
   it('should create StateDB from constructor', () => {
-    const stateDB = new StateDB(new MemoryLevel() as any)
     expect(stateDB).exist
+  })
+  const trie = new Trie({ useKeyHashing: true, db: stateDB.db })
+  it('should be able to instantiate Trie from StateDB', () => {
+    expect(trie).exist
   })
 })
