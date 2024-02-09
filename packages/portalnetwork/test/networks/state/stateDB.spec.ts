683d8928e2080e09f8570e546b21e1a43aa61eea
ScottyPoi <scott.simpson@ethereum.org>
2024-02-09 15:34:53 -0700

    state: test db constructors

--- packages/portalnetwork/test/networks/state/stateDB.spec.ts
+++ packages/portalnetwork/test/networks/state/stateDB.spec.ts
@@ -2,10 +2,15 @@ import { MemoryLevel } from 'memory-level'
 import { describe, expect, it } from 'vitest'
 
 import { PortalTrieDB } from '../../../src/index.js'
+import { StateDB } from '../../../src/networks/state/statedb.js'
 
-describe('PortalTrieDB', async () => {
+describe('StateDB', async () => {
   it('should create PortalTrieDB from constructor', () => {
     const portalTrieDB = new PortalTrieDB(new MemoryLevel() as any)
     expect(portalTrieDB).exist
+  })
+  it('should create StateDB from constructor', () => {
+    const stateDB = new StateDB(new MemoryLevel() as any)
+    expect(stateDB).exist
   })
 })
