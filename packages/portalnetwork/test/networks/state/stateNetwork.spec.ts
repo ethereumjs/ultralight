03694cc56578d3c76120eb47ca497799dca6a554
ScottyPoi <scott.simpson@ethereum.org>
2024-02-09 16:29:34 -0700

    state: test db connection

--- packages/portalnetwork/test/networks/state/stateNetwork.spec.ts
+++ packages/portalnetwork/test/networks/state/stateNetwork.spec.ts
@@ -4,7 +4,9 @@ import { describe, expect, it } from 'vitest'
 
 import { NetworkId, PortalNetwork } from '../../../src/index.js'
 
-describe.skip('StateNetwork', async () => {
+import type { StateNetwork } from '../../../src/index.js'
+
+describe('StateNetwork', async () => {
   const pk = await createSecp256k1PeerId()
   const enr1 = SignableENR.createFromPeerId(pk)
   const r = 4n
@@ -20,5 +22,17 @@ describe.skip('StateNetwork', async () => {
   const stateNetwork = ultralight.networks.get(NetworkId.StateNetwork)
   it('should start with state network', () => {
     expect(stateNetwork).toBeDefined()
+  })
+  it('should instantiate StateDB', () => {
+    expect((stateNetwork as StateNetwork).stateDB).exist
+    expect((stateNetwork as StateNetwork).stateDB.db).exist
+  })
+  it('should connect client db to stateDB', async () => {
+    ultralight.db.put(NetworkId.StateNetwork, '0x1234', 'testvalue')
+    const value = await stateNetwork!.get(NetworkId.StateNetwork, '0x1234')
+    expect(value).toEqual('testvalue')
+    stateNetwork!.put(NetworkId.StateNetwork, '0xabcd', 'testvalue2')
+    const value2 = await ultralight.db.get(NetworkId.StateNetwork, '0xabcd')
+    expect(value2).toEqual('testvalue2')
   })
 })
