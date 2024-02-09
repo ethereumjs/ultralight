d10cfb3f52907ed6940d29ca6f283e4c4d35a86a
ScottyPoi <scott.simpson@ethereum.org>
2024-02-08 21:41:43 -0700

    state: remove old distance, import regular distance

--- packages/portalnetwork/src/networks/state/util.ts
+++ packages/portalnetwork/src/networks/state/util.ts
@@ -1,4 +1,6 @@
 import { digest as sha256 } from '@chainsafe/as-sha256'
+import { distance } from '@chainsafe/discv5'
+import { equalsBytes } from '@ethereumjs/util'
 
 import {
   AccountTrieNodeKey,
@@ -23,15 +25,6 @@ const MID = 2n ** 255n
  * Calculates the distance between two ids using the distance function defined here
  * https://github.com/ethereum/portal-network-specs/blob/master/state-network.md#distance-function
  */
-export const distance = (id1: bigint, id2: bigint): bigint => {
-  if (id1 >= MODULO || id2 >= MODULO) {
-    throw new Error('numeric representation of node id cannot be greater than 2^256')
-  }
-  let diff: bigint
-  id1 > id2 ? (diff = id1 - id2) : (diff = id2 - id1)
-  diff > MID ? (diff = MODULO - diff) : diff
-  return diff
-}
 
 /* ContentKeys */
 
