2ff94972d5682ca124532b9c7757b448eac8188d
ScottyPoi <scott.simpson@ethereum.org>
2024-02-09 18:16:13 -0700

    stateDB: test put and get for account trie nodes

--- packages/portalnetwork/test/networks/state/stateDB.spec.ts
+++ packages/portalnetwork/test/networks/state/stateDB.spec.ts
@@ -2,14 +2,31 @@ import { Trie, decodeNode } from '@ethereumjs/trie'
 import { MemoryLevel } from 'memory-level'
 import { assert, describe, expect, it } from 'vitest'
 
-import { PortalTrieDB, fromHexString, toHexString } from '../../../src/index.js'
+import {
+  AccountTrieNodeOffer,
+  AccountTrieNodeRetrieval,
+  PortalTrieDB,
+  StateNetworkContentKey,
+  StateNetworkContentType,
+  fromHexString,
+  getDatabaseContent,
+  getDatabaseKey,
+  toHexString,
+} from '../../../src/index.js'
 import { StateDB } from '../../../src/networks/state/statedb.js'
 
+import testdata from './testdata/accountNodeSamples.json'
+
+import type { TAccountTrieNodeKey } from '../../../src/index.js'
 import type { AbstractLevel } from 'abstract-level'
 
 describe('StateDB components', async () => {
   it('should create StateDB from constructor', () => {
-    const stateDB = new StateDB(new MemoryLevel() as any)
+    const stateDB = new StateDB(
+      new MemoryLevel({
+        createIfMissing: true,
+      }) as AbstractLevel<string, string, string>,
+    )
     expect(stateDB).exist
   })
   const portalTrieDB = new PortalTrieDB(
@@ -53,5 +70,42 @@ describe('StateDB components', async () => {
     } catch {
       assert.ok('deleted value deleted')
     }
+  })
+})
+
+describe('database key / database contents', async () => {
+  const stateDB = new StateDB(
+    new MemoryLevel({
+      createIfMissing: true,
+    }) as AbstractLevel<string, string, string>,
+  )
+  const [sampleKey, sampleContent] = testdata[0] as [string, object]
+  const sampleContentKey = StateNetworkContentKey.decode(fromHexString(sampleKey))
+  const sampleContentBytes = Uint8Array.from(Object.values(sampleContent))
+  const content = AccountTrieNodeOffer.deserialize(sampleContentBytes)
+  const { nodeHash, path } = sampleContentKey as TAccountTrieNodeKey
+  const { blockHash, proof } = content
+  const nodeSample = proof.slice(-1)[0]
+  const contentNodeSample = AccountTrieNodeRetrieval.serialize({ node: nodeSample })
+  it('should correctly decode sample', () => {
+    expect(sampleContentKey).exist
+    expect(content).exist
+    expect(nodeHash).exist
+    expect(path).exist
+    expect(blockHash).exist
+    expect(proof).exist
+  })
+  const dbKey = getDatabaseKey(fromHexString(sampleKey))
+  it('should get dbKey from contentKey', () => {
+    expect(dbKey).toEqual(toHexString(nodeHash))
+  })
+  const dbContent = getDatabaseContent(StateNetworkContentType.AccountTrieNode, contentNodeSample)
+  it('should get dbContent from content', () => {
+    assert.deepEqual(fromHexString(dbContent), nodeSample)
+  })
+  await stateDB.storeContent(fromHexString(sampleKey), contentNodeSample)
+  const retrieved = await stateDB.getContent(fromHexString(sampleKey))
+  it('should put and get node using AccountTrieNode Content and Key', () => {
+    assert.equal(retrieved, toHexString(nodeSample))
   })
 })
