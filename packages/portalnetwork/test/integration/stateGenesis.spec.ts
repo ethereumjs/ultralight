ff9b1a302b0e1fec406cf9d964f08a78ddfa229a
ScottyPoi <scott.simpson@ethereum.org>
2024-02-20 22:36:22 -0700

    state/tests: genesis content devnet test

--- packages/portalnetwork/test/integration/stateGenesis.spec.ts
+++ packages/portalnetwork/test/integration/stateGenesis.spec.ts
@@ -245,3 +245,104 @@ describe('genesisContent', async () => {
   })
 })
 
+describe('genesisDevnet', async () => {
+  const protoBufs = [
+    '0x0a27002508021221024776a66a32c732ff71d6477fab2beb1e1b303ae157c3b5d95789aa52b1740b82122508021221024776a66a32c732ff71d6477fab2beb1e1b303ae157c3b5d95789aa52b1740b821a240802122091b5cbbc2bf054f913c3a344bf8ce6d19373142854eabeeffb5a3f159c44e610',
+    '0x0a2700250802122103d2a342da6a4fe1598f83df70bfcf9047e24eb7804799067c680870989e4ff0b412250802122103d2a342da6a4fe1598f83df70bfcf9047e24eb7804799067c680870989e4ff0b41a24080212202a63200954ac3c187131b79d39f16ef601d83d57f882b58acdad0dd346c06258',
+    '0x0a2700250802122103879ca6d3b9e51e746f90704496e3c36a0c473e0ec734dfa52bd8be50c7c4044c12250802122103879ca6d3b9e51e746f90704496e3c36a0c473e0ec734dfa52bd8be50c7c4044c1a24080212204d9ce45403c77746d795f6f01bafd81b5e4dfd9f7bfd6bd2edd9a06f32d86e36',
+    '0x0a270025080212210297b980a75593bc2c9f3ffc0d393a240b8d7b26465bcbc0b8a488f01202b962cd1225080212210297b980a75593bc2c9f3ffc0d393a240b8d7b26465bcbc0b8a488f01202b962cd1a24080212204c768f46d83b047fe5f7521f77b2feb3f182df96a39543f9b9b09f3c7e1a4e29',
+    '0x0a2700250802122102a80d91fa0da65157cf3e7d44cf5a070c01f5a37f5c77536c421813dbe3fe874a12250802122102a80d91fa0da65157cf3e7d44cf5a070c01f5a37f5c77536c421813dbe3fe874a1a24080212203676d8bd61041188b449f9517a51837d415f01caa10f81c7bd22febca0eadf3b',
+    '0x0a27002508021221030bc06a165852567cd1f47728741e44aa8c1445e2f64176866a42f658bb9f13fe122508021221030bc06a165852567cd1f47728741e44aa8c1445e2f64176866a42f658bb9f13fe1a24080212205be348796815dabfd5c89d2d4dba943f3314a59a47e4d21b2a1a1b66fff330da',
+  ]
+  const peerIds = await Promise.all(
+    protoBufs.map(async (protoBuf) => {
+      const peerId = await createFromProtobuf(fromHexString(protoBuf))
+      return peerId
+    }),
+  )
+  const clients = await Promise.all(
+    peerIds.map(async (peerId, i) => {
+      const enr = SignableENR.createFromPeerId(peerId)
+      const initMa: any = multiaddr(`/ip4/127.0.0.1/udp/${3022 + i}`)
+      enr.setLocationMultiaddr(initMa)
+      const node = await PortalNetwork.create({
+        transport: TransportLayer.NODE,
+        supportedNetworks: [NetworkId.StateNetwork],
+        config: {
+          enr,
+          bindAddrs: {
+            ip4: initMa,
+          },
+          peerId,
+        },
+        radius: 2n ** 254n,
+      })
+      await node.start()
+      return node
+    }),
+  )
+  const networks: StateNetwork[] = clients.map(
+    (client) => client.networks.get(NetworkId.StateNetwork) as StateNetwork,
+  )
+  const trie = await genesisStateTrie()
+  const sortedNodeHashByClient = await populateGenesisDB(trie, networks)
+  const hasRoot = Object.entries(sortedNodeHashByClient)
+    .filter(([_, nodeHashes]) =>
+      nodeHashes.map((h) => toHexString(h)).includes(toHexString(trie.root())),
+    )
+    .map(([nodeId, _]) => nodeId)
+  it('should store root in 2 clients', () => {
+    expect(hasRoot.length).toBe(2)
+  })
+
+  const storedTrieNodes = Object.entries(sortedNodeHashByClient)
+    .map(([_, trieNodes]) => {
+      return trieNodes.map((node) => toHexString(node))
+    })
+    .flat()
+  const uniqueStoredTrieNodes = Array.from(new Set(storedTrieNodes))
+  it('should distribute all nodes', () => {
+    expect(uniqueStoredTrieNodes.length).toEqual(12356)
+  })
+
+  await connectNetwork(networks, clients)
+  await new Promise((r) => setTimeout(r, 1000))
+  const storedKeys: Record<string, string[]> = {}
+  for (const network of networks) {
+    storedKeys[network.enr.nodeId] = []
+    const dbKeys = network.stateDB.db.db.keys()
+    for await (const key of dbKeys) {
+      storedKeys[network.enr.nodeId].push(key)
+    }
+  }
+  const allKeys = Object.values(storedKeys).flat()
+  const uniqueKeys = Array.from(new Set(allKeys))
+
+  it(`should store ${allKeys.length} keys in network`, () => {
+    expect(allKeys.length).toBeGreaterThan(0)
+  })
+  it(`Should store 12356 unique keys`, () => {
+    expect(uniqueKeys.length).toEqual(12356)
+  })
+  for (let i = 0; i < 20; i++) {
+    const testClient = networks[i % networks.length]
+    const testAddress = Object.keys(mainnet.alloc)[Math.floor(Math.random() * 8893)]
+    const found = await testClient.getAccount('0x' + testAddress, trie.root())
+    const foundAccount = Account.fromRlpSerializedAccount(found!)
+    it(`client {${i % networks.length}} should find account balance for addr: ${testAddress.slice(
+      0,
+      8,
+    )}...`, async () => {
+      expect(foundAccount.balance).toBeGreaterThan(0n)
+      assert.deepEqual(
+        foundAccount.balance,
+        BigInt(mainnet.alloc[testAddress].balance),
+        'account data found',
+      )
+    })
+    const emptyTemp = [...testClient.stateDB.db.temp.keys()]
+    it('should empty temp node map', () => {
+      expect(emptyTemp.length).toEqual(0)
+    })
+  }
+})
