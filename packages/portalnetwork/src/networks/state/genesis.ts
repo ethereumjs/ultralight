4714313433b21568ac0df418eecee8124bc34b8d
ScottyPoi <scott.simpson@ethereum.org>
2024-02-20 21:05:34 -0700

    state: genesis trie helper methods

--- packages/portalnetwork/src/networks/state/genesis.ts
+++ packages/portalnetwork/src/networks/state/genesis.ts
@@ -1,12 +1,10 @@
-import { distance } from '@chainsafe/discv5'
 import { fromHexString, toHexString } from '@chainsafe/ssz'
-import { Trie } from '@ethereumjs/trie'
-import { Account, parseGethGenesisState } from '@ethereumjs/util'
-import { sha256 } from 'ethereum-cryptography/sha256.js'
+import { LeafNode, Trie } from '@ethereumjs/trie'
+import { Account, equalsBytes, parseGethGenesisState } from '@ethereumjs/util'
 
 import genesis from './mainnet.json' assert { type: 'json' }
 
-import type { StateNetwork } from './state'
+import type { Proof, TrieNode } from '@ethereumjs/trie'
 import type { AccountState } from '@ethereumjs/util'
 
 const genesisAccounts = () => {
@@ -28,51 +26,45 @@ export const genesisStateTrie = async () => {
   for (const account of genesisAccounts()) {
     await trie.put(fromHexString(account[0]), account[1])
   }
+  if (!equalsBytes(trie.root(), fromHexString(genesis.genesisStateRoot))) {
+    throw new Error('Invalid genesis state root')
+  }
   return trie
 }
 
-export const inRadiusAccounts = async (nodeId: string, radius: bigint): Promise<string[]> => {
-  const distances: Map<bigint, string> = new Map()
-  const accounts: string[] = []
-  for (const account of genesisAccounts()) {
-    const d = distance(nodeId, toHexString(sha256(fromHexString(account[0]))).slice(2))
-    if (d <= radius) {
-      accounts.push(account[0])
-    }
-    distances.set(d, account[0])
-  }
-  return accounts
-}
-
-export const inRadiusGenesisTrie = async (nodeId: string, radius: bigint) => {
+export const generateAccountTrieProofs = async (): Promise<{
+  nonLeafProofs: Record<string, { path: string[]; proof: Proof }>
+  leafProofs: Record<string, { path: string[]; proof: Proof }>
+}> => {
   const trie = await genesisStateTrie()
-  const inRadius = await inRadiusAccounts(nodeId, radius)
-  const proofs = (
-    await Promise.all(
-      inRadius.map(async (address) => {
-        return trie.createProof(fromHexString(address))
-      }),
-    )
-  ).flat()
-
-  const partialTrie = new Trie({ useKeyHashing: true, root: trie.root() })
-  for (const node of proofs) {
-    const hash = trie['hash'](node)
-    await partialTrie.database().put(hash, node)
+  const nodes: [TrieNode, number[]][] = []
+  const leafNodes: [TrieNode, number[]][] = []
+  await trie.walkAllNodes(async (node, key) => {
+    node instanceof LeafNode ? leafNodes.push([node, key]) : nodes.push([node, key])
+  })
+  const leafProofs = await Promise.all(
+    leafNodes.map(async ([node, path]) => {
+      const nodeHash = trie['hash'](node.serialize())
+      const proof = [
+        ...(await trie.findPath(nodeHash)).stack.map((node) => node.serialize()),
+        node.serialize(),
+      ]
+      return [toHexString(nodeHash), { path, proof }]
+    }),
+  )
+  const proofs = await Promise.all(
+    nodes.map(async ([node, path]) => {
+      const nodeHash = trie['hash'](node.serialize())
+      const nodePath = await trie.findPath(nodeHash)
+      const proof = [...nodePath.stack.map((node) => node.serialize()), node.serialize()]
+      const content = [toHexString(nodeHash), { path, proof }]
+      return content
+    }),
+  )
+  return {
+    nonLeafProofs: Object.fromEntries(proofs),
+    leafProofs: Object.fromEntries(leafProofs),
   }
-  return partialTrie
 }
 
-export const genesisDBEntries = async (
-  nodeId: string = '0x' + '00'.repeat(32),
-  radius: bigint,
-): Promise<[string, Uint8Array][]> => {
-  const partialTrie = await inRadiusGenesisTrie(nodeId, radius)
-  const db = (partialTrie.database().db as any)._database
-  const entries = [...db.entries()] as [string, Uint8Array][]
-  return entries
-}
-
-export async function genesisTrie(state: StateNetwork) {
-  return genesisDBEntries(state.enr.nodeId, state['nodeRadius'])
-}
+export const mainnet = genesis
