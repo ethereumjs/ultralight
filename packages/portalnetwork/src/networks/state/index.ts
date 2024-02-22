0dc43b369072307ab555dc34e425e7fa6dfc15d9
ScottyPoi <scott.simpson@ethereum.org>
2024-02-21 21:35:51 -0700

    state: extract runBlock helpers from EthJS

--- packages/portalnetwork/src/networks/state/index.ts
+++ packages/portalnetwork/src/networks/state/index.ts
@@ -1,5 +1,6 @@
 export * from './nibbleEncoding.js'
 export * from './routingTable.js'
+export * from './runBlock.js'
 export * from './state.js'
 export * from './stateManager.js'
 export * from './types.js'
