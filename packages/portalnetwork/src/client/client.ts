26eaf85005d7fab238d0b12ebd05f3a1b71b457b
ScottyPoi <scott.simpson@ethereum.org>
2024-05-20 11:32:54 -0600

    portalnetwork: adjust discv5 timeout settings

--- packages/portalnetwork/src/client/client.ts
+++ packages/portalnetwork/src/client/client.ts
@@ -62,6 +62,9 @@ export class PortalNetwork extends (EventEmitter as { new (): PortalNetworkEvent
         allowUnverifiedSessions: true,
         requestTimeout: 3000,
         sessionEstablishTimeout: 3000,
+        lookupTimeout: 3000,
+        sessionTimeout: 3000,
+        requestRetries: 2,
       },
     }
     const config = { ...defaultConfig, ...opts.config }
