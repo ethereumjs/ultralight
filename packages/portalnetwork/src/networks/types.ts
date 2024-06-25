3ec4bbe29d319ef2312fe8ae8ff0461b1dc15214
ScottyPoi <scott.simpson@ethereum.org>
2024-06-25 15:49:31 -0600

    portalnetwork: update mainnet identifiers

--- packages/portalnetwork/src/networks/types.ts
+++ packages/portalnetwork/src/networks/types.ts
@@ -12,10 +12,10 @@ const BYTE_SIZE = 256
 export enum NetworkId {
   StateNetwork = '0x500a',
   HistoryNetwork = '0x500b',
-  TxGossipNetwork = '0x500c',
-  HeaderGossipNetwork = '0x500d',
-  CanonicalIndicesNetwork = '0x500e',
-  BeaconLightClientNetwork = '0x501a',
+  BeaconChainNetwork = '0x500c',
+  CanonicalTxIndexNetwork = '0x500d',
+  VerkleStateNetwork = '0x500e',
+  TransactionGossipNetwork = '0x500f',
   UTPNetwork = '0x757470',
   Rendezvous = '0x72656e',
 }
