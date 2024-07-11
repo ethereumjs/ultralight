9dbb8787fe4b2ab57d84f42a098bd20bc9f660e1
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2024-07-11 15:08:41 -0400

    Update bridge script and types

--- packages/cli/scripts/postMergeHistoryBridge.ts
+++ packages/cli/scripts/postMergeHistoryBridge.ts
@@ -1,44 +1,59 @@
 import { ProofType, createProof } from '@chainsafe/persistent-merkle-tree'
-import { hexToBytes } from '@ethereumjs/util'
-import { createBeaconConfig, defaultChainConfig } from '@lodestar/config'
-import { genesisData } from '@lodestar/config/networks'
-import { ForkName } from '@lodestar/params'
+import { bytesToHex, equalsBytes } from '@ethereumjs/util'
 import { ssz } from '@lodestar/types'
+import { readFileSync, writeFileSync } from 'fs'
 import jayson from 'jayson/promise/index.js'
+import { HistoricalRootsBlockProof } from 'portalnetwork'
+import { cwd } from 'process'
 
 import type { SingleProof } from '@chainsafe/persistent-merkle-tree'
 
 const { Client } = jayson
 
 const main = async () => {
-  const beaconConfig = createBeaconConfig(
-    defaultChainConfig,
-    hexToBytes(genesisData.mainnet.genesisValidatorsRoot),
-  )
-  const BellatrixForkDigest = beaconConfig.forkName2ForkDigest(ForkName.bellatrix)
   const beaconNode = 'https://lodestar-mainnet.chainsafe.io'
-  const ultralight = Client.http({ host: '127.0.0.1', port: 8545 })
+  const _ultralight = Client.http({ host: '127.0.0.1', port: 8545 })
 
-  const merge_slot = 4700013
+  const historicalBatch = readFileSync(
+    cwd() + '/scripts/historicalBatches/historical_batch-574-3c0da77d.ssz',
+  )
+  const postMergeBatch = ssz.phase0.HistoricalBatch.deserialize(Buffer.from(historicalBatch))
+  let roots = ''
+  for (const root of postMergeBatch.blockRoots) roots += bytesToHex(root) + '\n'
+  writeFileSync('./blockHex.txt', roots)
+  const merge_slot = 4700013 // The Beacon Chain slot number corresponding to the merge block
+  const merge_block_index = 5997 // The index of the merge block blockRoot in the historical batch for era 574 (where the merge occurred)
   console.log('Retrieving beacon block...')
   const res = (await (await fetch(beaconNode + `/eth/v2/beacon/blocks/${merge_slot}`)).json()).data
   const block = ssz.bellatrix.BeaconBlock.fromJson(res.message)
-  const elBlockHashPath = ssz.bellatrix.BeaconBlockBody.getPathInfo([
+  const elBlockHashPath = ssz.bellatrix.BeaconBlock.getPathInfo([
+    'body',
     'executionPayload',
     'blockHash',
   ])
-  const blockBodyRootHash = ssz.bellatrix.BeaconBlock.getPathInfo(['body'])
-  const beaconBlockBodyProof = createProof(ssz.bellatrix.BeaconBlockBody.toView(block.body).node, {
+  const beaconBlockProof = createProof(ssz.bellatrix.BeaconBlock.toView(block).node, {
     gindex: elBlockHashPath.gindex,
     type: ProofType.single,
   }) as SingleProof
-  const beaconBlockHeaderProof = createProof(ssz.bellatrix.BeaconBlock.toView(block).node, {
-    gindex: blockBodyRootHash.gindex,
+
+  const historicalRootsPath = ssz.phase0.HistoricalBatch.getPathInfo([
+    'blockRoots',
+    merge_block_index,
+  ])
+  console.log(
+    `Merge Block blockRoot: ${bytesToHex(ssz.bellatrix.BeaconBlock.value_toTree(block).root)} and found in era 574 historicalBatch ${equalsBytes(ssz.bellatrix.BeaconBlock.value_toTree(block).root, postMergeBatch.blockRoots[merge_block_index])}`,
+  )
+  const historicalRootsProof = createProof(ssz.phase0.HistoricalBatch.toView(postMergeBatch).node, {
+    gindex: historicalRootsPath.gindex,
     type: ProofType.single,
   }) as SingleProof
-
-  console.log(beaconBlockBodyProof.witnesses.length)
-  console.log(beaconBlockHeaderProof.witnesses.length)
+  const headerProof = HistoricalRootsBlockProof.fromJson({
+    slot: merge_slot,
+    historicalRootsProof: historicalRootsProof.witnesses.map((witness) => bytesToHex(witness)),
+    beaconBlockHeaderProof: beaconBlockProof.witnesses.map((witness) => bytesToHex(witness)),
+    beaconBlockHeaderRoot: bytesToHex(ssz.bellatrix.BeaconBlock.value_toTree(block).root),
+  })
+  console.log(headerProof.slot)
 }
 
 main().catch((err) => {
