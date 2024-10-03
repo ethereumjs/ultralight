d69dc77df4c769a58ed3dc670bb340b970c93531
ScottyPoi <scott.simpson@ethereum.org>
2024-10-03 13:59:27 -0600

    uTP: separate ContentRequest class for each type

--- packages/portalnetwork/src/wire/utp/PortalNetworkUtp/ContentRequest.ts
+++ packages/portalnetwork/src/wire/utp/PortalNetworkUtp/ContentRequest.ts
@@ -1,5 +1,3 @@
-import { ConnectionState } from '../index.js'
-
 import { RequestCode } from './types.js'
 
 import type { NetworkId } from '../../../index.js'
@@ -11,52 +9,101 @@ export interface ContentRequestOptions {
   socket: UtpSocket
   socketKey: string
   contentKeys: Uint8Array[]
-  content?: Uint8Array
+  content: Uint8Array
 }
-export class ContentRequest {
+export abstract class ContentRequest {
   networkId: NetworkId
   requestCode: RequestCode
-  contentKey?: Uint8Array
-  contentKeys: Uint8Array[]
   socket: UtpSocket
   socketKey: string
-  content?: Uint8Array
-
   constructor(options: ContentRequestOptions) {
     this.networkId = options.networkId
-    this.contentKeys = options.contentKeys
     this.requestCode = options.requestCode
-    this.content = options.content
     this.socketKey = options.socketKey
     this.socket = options.socket
   }
 
-  async init(): Promise<RequestCode> {
-    switch (this.requestCode) {
-      case RequestCode.FINDCONTENT_READ:
-        this.socket.logger.extend('ContentRequest')('init() - Sending a SYN')
-        await this.sendSyn()
-        break
-      case RequestCode.OFFER_WRITE:
-        if (this.content) {
-          this.socket.content = this.content
-          this.socket.logger.extend('ContentRequest')('init() - Sending a SYN')
-          await this.sendSyn()
-        }
-        break
-      default:
-        this.socket.logger.extend('ContentRequest')('init() - Waiting for a SYN')
-        break
-    }
-    return this.requestCode
-  }
+  abstract init(): Promise<void>
 
   close(): void {
     this.socket.close()
   }
+}
 
-  async sendSyn(): Promise<void> {
+export class FindContentReadRequest extends ContentRequest {
+  requestCode: RequestCode.FINDCONTENT_READ
+  contentKey: Uint8Array
+  constructor(options: ContentRequestOptions) {
+    super(options)
+    this.requestCode = RequestCode.FINDCONTENT_READ
+    this.contentKey = options.contentKeys[0]
+  }
+  async init(): Promise<void> {
+    this.socket.logger.extend('ContentRequest')('init() - Sending a SYN')
     await this.socket.sendSynPacket(this.socket.sndConnectionId)
-    this.socket.state = ConnectionState.SynSent
+  }
+}
+
+export class FoundContentWriteRequest extends ContentRequest {
+  requestCode: RequestCode.FOUNDCONTENT_WRITE
+  content: Uint8Array
+  contentKey: Uint8Array
+  constructor(options: ContentRequestOptions) {
+    super(options)
+    this.requestCode = RequestCode.FOUNDCONTENT_WRITE
+    this.content = options.content
+    this.contentKey = options.contentKeys[0]
+  }
+  async init(): Promise<void> {
+    this.socket.logger.extend('ContentRequest')('init() - awaiting a SYN')
+  }
+}
+
+export class AcceptReadRequest extends ContentRequest {
+  requestCode: RequestCode.ACCEPT_READ
+  contentKeys: Uint8Array[]
+  constructor(options: ContentRequestOptions) {
+    super(options)
+    this.requestCode = RequestCode.ACCEPT_READ
+    this.contentKeys = options.contentKeys
+  }
+  async init(): Promise<void> {
+    this.socket.logger.extend('ContentRequest')('init() - awaiting a SYN')
+  }
+}
+
+export class OfferWriteRequest extends ContentRequest {
+  requestCode: RequestCode.OFFER_WRITE
+  content: Uint8Array
+  contentKeys: Uint8Array[]
+  constructor(options: ContentRequestOptions) {
+    super(options)
+    this.requestCode = RequestCode.OFFER_WRITE
+    this.content = options.content
+    this.contentKeys = options.contentKeys
+  }
+
+  async init(): Promise<void> {
+    this.socket.logger.extend('ContentRequest')('init() - Sending a SYN')
+    await this.socket.sendSynPacket(this.socket.sndConnectionId)
+  }
+}
+
+export type ContentRequestType =
+  | FindContentReadRequest
+  | FoundContentWriteRequest
+  | OfferWriteRequest
+  | AcceptReadRequest
+
+export function createContentRequest(options: ContentRequestOptions): ContentRequestType {
+  switch (options.requestCode) {
+    case RequestCode.FINDCONTENT_READ:
+      return new FindContentReadRequest(options)
+    case RequestCode.FOUNDCONTENT_WRITE:
+      return new FoundContentWriteRequest(options)
+    case RequestCode.OFFER_WRITE:
+      return new OfferWriteRequest(options)
+    case RequestCode.ACCEPT_READ:
+      return new AcceptReadRequest(options)
   }
 }
