27fe1ef053a0ac1bb239d210e8d4dadaf460a62c
ScottyPoi <scott.simpson@ethereum.org>
2024-10-23 16:23:23 -0600

    expand test

--- packages/portalnetwork/test/util/util.spec.ts
+++ packages/portalnetwork/test/util/util.spec.ts
@@ -1,4 +1,5 @@
 import { log2Distance } from '@chainsafe/discv5'
+import { unprefixedHexToBytes } from '@ethereumjs/util'
 import { assert, describe, it } from 'vitest'
 
 import {
@@ -16,20 +17,18 @@ describe('utility method tests', async () => {
     assert.ok(short === '82418...b2c41', 'correctly shortened node id')
   })
 
-  let randomNodeId: string
-  it('should correctly generate random node id at distance', async () => {
-    randomNodeId = generateRandomNodeIdAtDistance(nodeId, 255)
-    assert.equal(
-      log2Distance(nodeId, randomNodeId),
-      255,
-      'calculated random node ID at distance 255',
-    )
-  })
-
-  it('should correctly generate random node id at distance', async () => {
-    randomNodeId = generateRandomNodeIdAtDistance(nodeId, 25)
-    assert.equal(log2Distance(nodeId, randomNodeId), 25, 'calculated random node id at distance 25')
-  })
+  for (let i = 1; i < 256; i++) {
+    it(`should correctly generate random node id at distance ${i}`, async () => {
+      const randomNodeId = generateRandomNodeIdAtDistance(nodeId, i)
+      const array = unprefixedHexToBytes(randomNodeId)
+      assert.equal(array.length, 32, 'generated random node ID has correct length')
+      assert.equal(
+        log2Distance(nodeId, randomNodeId),
+        i,
+        `calculated random node ID at distance ${i}`,
+      )
+    })
+  }
 
   const arrayOfUint8Arrays = [Uint8Array.from([1, 2, 3]), Uint8Array.from([1, 2])]
   it('should correctly compute byte length of array of Uint8Arrays', async () => {
