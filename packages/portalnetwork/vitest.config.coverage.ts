8b86c257bd9646b76a327d83b22f23690bdd881d
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-17 21:13:25 -0500

    split coverage from test scripts

--- packages/portalnetwork/vitest.config.coverage.ts
+++ packages/portalnetwork/vitest.config.coverage.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     silent: true,
+    exclude: [
+      'test/integration'
+    ],
     testTimeout: 180000,
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      all: true,
+    },
+
   },
 })
