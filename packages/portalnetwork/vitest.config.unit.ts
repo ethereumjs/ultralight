2404daf8b2ebd0c7ce063c520789c94dbd2bb283
cjustinobi <cjustinobi@gmail.com>
2025-01-18 20:20:01 +0100

    update vitest to v3.0.2

--- packages/portalnetwork/vitest.config.unit.ts
+++ packages/portalnetwork/vitest.config.unit.ts
@@ -3,12 +3,7 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     silent: true,
-    exclude: [
-      'test/integration',
-      'test/networks/history/portalSpecTests.spec.ts',
-      'test/networks/state/content.spec.ts',
-      'test/networks/beacon/types.spec.ts',
-    ],
+    exclude: ['test/integration'],
     testTimeout: 180000,
     coverage: {
       provider: 'v8',
