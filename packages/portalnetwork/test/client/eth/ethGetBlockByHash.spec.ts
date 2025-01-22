afe3fa67b7812a9aa00ab432986f2bc06bd031e2
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-01-22 08:38:52 -0500

    test that demonstrates infinite loop

--- packages/portalnetwork/test/client/eth/ethGetBlockByHash.spec.ts
+++ packages/portalnetwork/test/client/eth/ethGetBlockByHash.spec.ts
@@ -1,15 +1,18 @@
 import { assert, describe, it } from 'vitest'
-import { randomBytes } from '@ethereumjs/util'
+import { hexToBytes } from '@ethereumjs/util'
 import { PortalNetwork } from '../../../src/client/client.js'
 import { NetworkId } from '../../../src/networks/types.js'
 
 import type { HistoryNetwork } from '../../../src/networks/history/history.js'
-describe('ETH class base level API checks', async () => {
+describe('getBlockByHash', async () => {
     const ultralight = await PortalNetwork.create({
         bindAddress: '127.0.0.1',
     })
-    it('should get stuck in an infinite loop', async () => {
-        const block = await ultralight.ETH.getBlockByHash(randomBytes(32), false)
-        console.log('Block', block)
-    })
+    it('should not find a block by hash', async () => {
+        const blockHash = '0x1e98ea9bdf6e44eaed730041682e7db748812d5baef84a38435c8ad5f6c5d1e2'
+        const history = ultralight.networks.get(NetworkId.HistoryNetwork) as HistoryNetwork
+        await history.indexBlockHash(21591997n, blockHash)
+        const block = await ultralight.ETH.getBlockByHash(hexToBytes(blockHash), false)
+        assert.equal(block, undefined)
+    }, 3000)
 })
