5ac22b67af88e2c1bc535ef486e03c58b1609d11
ScottyPoi <scott.simpson@ethereum.org>
2025-02-27 17:53:58 -0700

    RPC: add method ultralight_getPingPayload

--- packages/cli/src/rpc/modules/ultralight.ts
+++ packages/cli/src/rpc/modules/ultralight.ts
@@ -1,5 +1,5 @@
 import { hexToBytes } from '@ethereumjs/util'
-import { HistoryNetworkContentType, NetworkId, addRLPSerializedBlock } from 'portalnetwork'
+import { HistoryNetworkContentType, NetworkId } from 'portalnetwork'
 
 import { INTERNAL_ERROR } from '../error-code.js'
 import { middleware, validators } from '../validators.js'
@@ -22,6 +22,7 @@ const methods = [
   'ultralight_getNetworkDBSize',
   'ultralight_pruneNetworkDB',
   'ultralight_setNetworkStorage',
+  'ultralight_getPingPayload'
 ]
 
 export class ultralight {
@@ -63,6 +64,10 @@ export class ultralight {
     this.setNetworkStorage = middleware(this.setNetworkStorage.bind(this), 2, [
       [validators.networkId],
       [validators.megabytes],
+    ])
+    this.getPingPayload = middleware(this.getPingPayload.bind(this), 1, [
+      [validators.networkId],
+      [validators.extension]
     ])
   }
   async methods() {
@@ -196,4 +201,17 @@ export class ultralight {
       radius: '0x' + network.nodeRadius.toString(16),
     }
   }
+  async getPingPayload(params: [NetworkId, number]) {
+    const [networkId, extension] = params
+    const network = this._client.networks.get(networkId)
+    if (!network) {
+      throw {
+        code: INTERNAL_ERROR, 
+        message: `Invalid network id ${networkId}`,
+      }
+    }
+    const payload = network.pingPongPayload(extension)
+    return payload
+  }
+  
 }
