5060276385266bb957eec935d05a6e79a50a0406
acolytec3 <17355484+acolytec3@users.noreply.github.com>
2025-02-28 12:40:07 -0500

    fix unit tests

--- packages/portalnetwork/test/networks/history/ephemeralHeader.spec.ts
+++ packages/portalnetwork/test/networks/history/ephemeralHeader.spec.ts
@@ -1,7 +1,7 @@
-import { assert, beforeAll, describe, it } from 'vitest'
-import { Block } from '@ethereumjs/block'
 import type { BlockHeader, JsonRpcBlock } from '@ethereumjs/block'
-import latestBlocks from './testData/latest3Blocks.json'
+import { Block } from '@ethereumjs/block'
+import { hexToBytes } from '@ethereumjs/util'
+import { assert, beforeAll, describe, it } from 'vitest'
 import {
   EphemeralHeaderPayload,
   HistoryNetworkContentType,
@@ -11,7 +11,8 @@ import {
   getContentKey,
   getEphemeralHeaderDbKey,
 } from '../../../src/index.js'
-import { bytesToHex, hexToBytes } from '@ethereumjs/util'
+import latestBlocks from './testData/latest3Blocks.json'
+
 describe('ephemeral header handling', () => {
   let headers: BlockHeader[]
   let headerPayload: Uint8Array
@@ -24,20 +25,17 @@ describe('ephemeral header handling', () => {
     headerPayload = EphemeralHeaderPayload.serialize(headers.map((h) => h.serialize()))
     contentKey = getContentKey(HistoryNetworkContentType.EphemeralHeader, {
       blockHash: headers[0].hash(),
-      ancestorCount: headers.length,
+      ancestorCount: headers.length - 1,
     })
   })
   it('should be able to store a valid ephemeral header payload', async () => {
     const node = await PortalNetwork.create({})
     const network = node.network()['0x500b']
 
-    await network?.store(contentKey, headerPayload)
+    await network!.store(contentKey, headerPayload)
     const storedHeaderPayload = await network?.get(getEphemeralHeaderDbKey(headers[0].hash()))
     assert.deepEqual(hexToBytes(storedHeaderPayload!), headers[0].serialize())
-    assert.equal(
-      network?.ephemeralHeaderIndex.getByKey(headers[1].number),
-      bytesToHex(headers[1].hash()),
-    )
+    assert.deepEqual(network!.ephemeralHeaderIndex.getByKey(headers[1].number), headers[1].hash())
   })
   it('should produce the correct HISTORY_RADIUS ping payload', async () => {
     const node = await PortalNetwork.create({})
