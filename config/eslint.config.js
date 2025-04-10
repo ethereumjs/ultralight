4d437c9292ae1b0286801c3b03e82f49b2cdd6e0
Justin Obi <cjustinobi@gmail.com>
2025-04-10 19:36:24 +0100

    New eslint config done

---
+++ config/eslint.config.js
@@ -0,0 +1,54 @@
+import typescriptParser from '@typescript-eslint/parser'
+import typescriptEslint from '@typescript-eslint/eslint-plugin'
+
+export default [
+	{
+		files: ["**/*.ts"],
+		languageOptions: {
+			parser: typescriptParser,
+			parserOptions: {
+				project: './config/tsconfig.lint.json',
+				extraFileExtensions: ['.json'],
+				tsconfigRootDir: import.meta.dirname,
+			},
+			ecmaVersion: 2020,
+			sourceType: "module",
+		
+		},
+		plugins: {
+			'@typescript-eslint': typescriptEslint,
+		},
+		ignores: [
+			'node_modules/',
+			'dist/',
+			'dist.browser/',
+			'coverage/',
+			'prettier.config.cjs',
+			'typedoc.js',
+			'docs',
+			'.eslintrc.cjs',
+			'vitest.config.*.ts',
+			'archived-browser-client/',
+			'browser-client/',
+			'ui/',
+			'scripts/',
+			'proxy/',
+		],
+		rules: {
+			'@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': 'error',
+      '@typescript-eslint/await-thenable': 'error',
+      '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'error',
+      '@typescript-eslint/no-unsafe-call': 'error',
+      '@typescript-eslint/no-unsafe-member-access': 'error',
+      '@typescript-eslint/no-unsafe-return': 'error',
+      '@typescript-eslint/restrict-template-expressions': 'error',
+      '@typescript-eslint/unbound-method': 'error',
+      '@typescript-eslint/strict-boolean-expressions': ['error'],
+      '@typescript-eslint/prefer-nullish-coalescing': 'error',
+      '@typescript-eslint/return-await': 'error',
+		}
+	},
+]
