54f5ba32bf2c581f875a5a05b29408c25f7f3c61
cjustinobi <cjustinobi@gmail.com>
2025-04-22 21:02:03 +0100

    Format useJsonRpc.ts

--- packages/portal-client/src/hooks/useJsonRpc.ts
+++ packages/portal-client/src/hooks/useJsonRpc.ts
@@ -15,121 +15,117 @@ export const useJsonRpc = () => {
         throw new Error('Portal Network client is not initialized')
       }
 
-    try {
-      setIsLoading(true)
-      let result
-      let responseType = 'generic' as RPCResponse['responseType']
+      try {
+        setIsLoading(true)
+        let result
+        let responseType = 'generic' as RPCResponse['responseType']
 
-      switch (method) { 
-        case 'eth_getBlockByNumber':
-          result = await client.ETH.getBlockByNumber(params[0], params[1])
-          responseType = 'block'
-          break
-        case 'eth_getBlockByHash':
-          result = await client.ETH.getBlockByHash(params[0], params[1])
-          responseType = 'block'
-          break
-        case 'eth_getTransactionCount':
-          result = await client.ETH.getTransactionCount(params[0], params[1])
-          responseType = 'bigNumber'
-          break
-        case 'eth_getCode':
-          result = await client.ETH.getCode(params[0], params[1])
-          responseType = 'code'
-          break
-        case 'eth_getStorageAt':
-          result = await client.ETH.getStorageAt(params[0], params[1], params[2])
-          responseType = 'storage'
-          break
-        case 'eth_call':
-          result = await client.ETH.call(params[0], params[1])
-          responseType = 'callResult'
-          break
-        case 'eth_getBalance':
-          result = await client.ETH.getBalance(params[0], params[1])
-          responseType = 'ether'
-          break
-        case 'portal_historyPing':
-          const res = await historyNetwork?.sendPing(params[0])
-          if (!res) {
-            throw new Error('Pong not received')
-          }
-          const { customPayload, ...rest } = res
-          result = {
-            ...rest,
-            extensionPayload: decodeExtensionPayloadToJson(
-              res.payloadType, 
-              res.customPayload
-            )
-          }
+        switch (method) {
+          case 'eth_getBlockByNumber':
+            result = await client.ETH.getBlockByNumber(params[0], params[1])
+            responseType = 'block'
+            break
+          case 'eth_getBlockByHash':
+            result = await client.ETH.getBlockByHash(params[0], params[1])
+            responseType = 'block'
+            break
+          case 'eth_getTransactionCount':
+            result = await client.ETH.getTransactionCount(params[0], params[1])
+            responseType = 'bigNumber'
+            break
+          case 'eth_getCode':
+            result = await client.ETH.getCode(params[0], params[1])
+            responseType = 'code'
+            break
+          case 'eth_getStorageAt':
+            result = await client.ETH.getStorageAt(params[0], params[1], params[2])
+            responseType = 'storage'
+            break
+          case 'eth_call':
+            result = await client.ETH.call(params[0], params[1])
+            responseType = 'callResult'
+            break
+          case 'eth_getBalance':
+            result = await client.ETH.getBalance(params[0], params[1])
+            responseType = 'ether'
+            break
+          case 'portal_historyPing':
+            const res = await historyNetwork?.sendPing(params[0])
+            if (!res) {
+              throw new Error('Pong not received')
+            }
+            const { customPayload, ...rest } = res
+            result = {
+              ...rest,
+              extensionPayload: decodeExtensionPayloadToJson(res.payloadType, res.customPayload),
+            }
 
-          responseType = 'generic'
-          break
-        case 'portal_historyFindContent':
-          result = await historyNetwork?.sendFindNodes(params[0], params[1])
-          responseType = 'generic'
-          break
-        default:
-          throw new Error(`Unsupported method: ${method}`)
+            responseType = 'generic'
+            break
+          case 'portal_historyFindContent':
+            result = await historyNetwork?.sendFindNodes(params[0], params[1])
+            responseType = 'generic'
+            break
+          default:
+            throw new Error(`Unsupported method: ${method}`)
+        }
+
+        if (result === undefined) {
+          throw new Error('No result returned from the request')
+        }
+        switch (responseType) {
+          case 'block':
+            setResult({
+              result: formatBlockResponse(result, params[1]),
+              responseType,
+            })
+            break
+          case 'bigNumber':
+            setResult({
+              result: Number(result),
+              responseType,
+            })
+            break
+          case 'ether':
+            setResult({
+              result: Number(BigInt(result) / BigInt(1e18)),
+              responseType,
+            })
+            break
+          case 'code':
+          case 'storage':
+          case 'callResult':
+            setResult({
+              result,
+              responseType,
+            })
+            break
+          default:
+            setResult({
+              result,
+              responseType: 'generic',
+            })
+        }
+        return result
+      } catch (err) {
+        const message = err instanceof Error ? err.message : 'An unknown error occurred'
+
+        setIsLoading(false)
+        setResult({
+          error: {
+            code: -32000,
+            message,
+          },
+          responseType: 'generic',
+        })
+
+        throw new Error(message)
+      } finally {
+        setIsLoading(false)
       }
-
-      if (result === undefined) {
-        throw new Error('No result returned from the request')
-      }
-      switch (responseType) {
-        case 'block':
-          setResult({ 
-            result: formatBlockResponse(result, params[1]),
-            responseType 
-          })
-          break
-        case 'bigNumber':
-          setResult({ 
-            result: Number(result),
-            responseType 
-          })
-          break
-        case 'ether':
-          setResult({
-            result: Number(BigInt(result) / BigInt(1e18)),
-            responseType 
-          })
-          break
-        case 'code':
-        case 'storage':
-        case 'callResult':
-          setResult({ 
-            result,
-            responseType 
-          })
-          break
-        default:
-          setResult({ 
-            result,
-            responseType: 'generic'
-          })
-      }
-      return result
-    } catch (err) {
-
-      const message = err instanceof Error 
-        ? err.message 
-        : 'An unknown error occurred'
-      
-      setIsLoading(false)
-      setResult({ 
-        error: { 
-          code: -32000, 
-          message 
-        },
-        responseType: 'generic'
-      })
-      
-      throw new Error(message)
-    } finally {
-      setIsLoading(false)
-    }
-  }, [client])
+    },
+    [client],
+  )
 
   return {
     result,
