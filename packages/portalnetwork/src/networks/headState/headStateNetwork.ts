7d52dc8b70c348076108e3b07e826053645522df
ScottyPoi <scott.simpson@ethereum.org>
2025-05-06 17:45:43 -0600

    stub out class methods and constructor

--- packages/portalnetwork/src/networks/headState/headStateNetwork.ts
+++ packages/portalnetwork/src/networks/headState/headStateNetwork.ts
@@ -1,10 +1,29 @@
-import { BaseNetwork, NetworkId, type NetworkConfig } from '../../index.js'
+import type { ENR } from '@chainsafe/enr'
+import {
+  BaseNetwork,
+  type ContentLookupResponse,
+  NetworkId,
+  type BaseNetworkConfig,
+} from '../../index.js'
 
 export class HeadStateNetwork extends BaseNetwork {
   networkName = 'HeadStateNetwork'
   networkId = NetworkId.HeadStateNetwork
 
-  constructor(config: NetworkConfig) {
-    super(config)
+  constructor({ client, db, radius, maxStorage }: BaseNetworkConfig) {
+    super({ client, networkId: NetworkId.HeadStateNetwork, db, radius, maxStorage })
+  }
+
+  store(contentKey: Uint8Array, value: Uint8Array): Promise<void> {
+    throw new Error('Method not implemented.')
+  }
+  public findContentLocally = async (contentKey: Uint8Array): Promise<Uint8Array | undefined> => {
+    throw new Error('Method not implemented')
+  }
+  public sendFindContent = async (
+    enr: ENR,
+    key: Uint8Array,
+  ): Promise<ContentLookupResponse | undefined> => {
+    throw new Error('Method not implemented')
   }
 }
